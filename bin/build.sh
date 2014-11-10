53c87e95d16a6057c197b99244befc6498052a91
Michael Parrish <michael@zooniverse.org>
2014-11-10 16:03:41 -0600

    Tweaking build process and removing gulp

--- bin/build.sh
+++ bin/build.sh
@@ -8,15 +8,7 @@ cp -av "$DEV_DIR" "$BUILD_DIR"
 [[ -f "$BUILD_DIR/$OUT_JS" ]] && rm "$BUILD_DIR/$OUT_JS"
 [[ -f "$BUILD_DIR/$OUT_CSS" ]] && rm "$BUILD_DIR/$OUT_CSS"
 
-./node_modules/.bin/webpack --production --progress --config webpack.build.config.js
-
-./node_modules/.bin/uglifyjs \
-  --verbose \
-  --screw-ie8 \
-  --mangle \
-  --compress drop_console \
-  --output "$BUILD_DIR/$OUT_JS" \
-  "$BUILD_DIR/$OUT_JS"
+./node_modules/.bin/webpack --progress --config webpack.build.config.js
 
 ./node_modules/.bin/stylus \
   --use nib \
--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,3 +1,11 @@
 var webpackConfig = require('./webpack.config');
-webpackConfig.output.path = __dirname + '/build';
+delete webpackConfig.module.noParse;
+delete webpackConfig.devtool;
+
+webpackConfig.output.path = __dirname + '/' + process.env.BUILD_DIR;
+webpackConfig.optimize = {
+  occurenceOrder: true,
+  minimize: true
+};
+
 module.exports = webpackConfig;
