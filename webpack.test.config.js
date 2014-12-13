1bf97828660c9ae67ab37eeddda3d07c3cf75742
Brian Carstensen <brian.carstensen@gmail.com>
2014-12-13 18:23:48 +0000

    Clean up test scripts

--- webpack.test.config.js
+++
@@ -1,5 +0,0 @@
-var webpackConfig = require('./webpack.config');
-webpackConfig.entry.main = '../test/runner.coffee';
-webpackConfig.output.path = __dirname + '/tmp';
-webpackConfig.output.filename = 'test.js';
-module.exports = webpackConfig;
