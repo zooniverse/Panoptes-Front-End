de4a954a5225b8b6694b3ca469faccae980ffc60
Brian Carstensen <brian.carstensen@gmail.com>
2014-12-14 10:49:29 +0000

    Rename jobs var

--- bin/serve.sh
+++ bin/serve.sh
@@ -2,7 +2,7 @@
 
 source "$(dirname "$0")/config.sh"
 
-jobs=""
+pids=""
 
 [[ -f "$DEV_DIR/$OUT_JS" ]] && rm "$DEV_DIR/$OUT_JS"
 
@@ -10,7 +10,7 @@ jobs=""
   --watch \
   "$SRC_JS" \
   "$DEV_DIR/$OUT_JS" \
-  & jobs="$jobs $!"
+  & pids="$pids $!"
 
 [[ -f "$DEV_DIR/$OUT_CSS" ]] && rm "$DEV_DIR/$OUT_CSS"
 
@@ -21,14 +21,14 @@ jobs=""
   --import nib \
   --out "$DEV_DIR" \
   "$SRC_CSS" \
-  & jobs="$jobs $!"
+  & pids="$pids $!"
 
 ./node_modules/.bin/static \
   --port "$PORT" \
   --cache "no-cache, must-revalidate" \
   "$DEV_DIR"
-  & jobs="$jobs $!"
+  & pids="$pids $!"
 
-trap 'kill -HUP $jobs' INT TERM HUP
+trap 'kill -HUP $pids' INT TERM HUP
 
 wait
