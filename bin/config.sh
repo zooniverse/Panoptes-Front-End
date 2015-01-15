eb52174a56f9e60bb2382a6edba1d33aca107e19
Brian Carstensen <brian.carstensen@gmail.com>
2015-01-15 13:27:58 +0000

    Back to browserify

--- bin/config.sh
+++ bin/config.sh
@@ -5,6 +5,20 @@ export BUILD_DIR="build"
 
 export SRC_JS="app/main.cjsx"
 export OUT_JS="main.js"
+export VENDOR_JS="vendor.js"
 
 export SRC_CSS="css/main.styl"
 export OUT_CSS="main.css"
+
+# NOTE: All non-dev dependencies are assumed to be front-end modules.
+externals=$(node -p "Object.keys(require('./package').dependencies).join('\n');")
+
+function flag_externals {
+  out=""
+  for module in $externals; do
+    out="$out --$1 $module"
+  done
+  echo $out
+}
+
+export flag_externals
