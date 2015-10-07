5a65e4aafd33a075b47a992ec399ca6676af2c0b
Edward Paget <edward@cassetteta.pe>
2015-10-06 23:27:02 -0500

    Skip unneeded dependencies from vendor file
    That were causing an error from uglify in Safari.

--- bin/config.sh
+++ bin/config.sh
@@ -21,7 +21,9 @@ function flag_externals {
   out=""
   for module in $externals; do
     # Symlinked modules are assumed to be in development and aren't externalized.
-    [[ -L "node_modules/$module" ]] || out="$out --$1 $module"
+    if [[ "$module" != "express" && "$module" != "ejs" && "$module" != "babel" && "$module" != "coffee-react" ]]; then
+      [[ -L "node_modules/$module" ]] || out="$out --$1 $module"
+    fi
   done
   echo $out
 }
