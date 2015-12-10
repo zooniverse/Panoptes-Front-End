01e84ffd464e005e28f317da3bcdd82f058baed0
Brian Carstensen <brian.carstensen@gmail.com>
2015-12-10 14:58:00 -0600

    Break out postinstall script

---
+++ bin/postinstall.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+FALLBACK_POLYFILLS_URL="https://cdn.polyfill.io/v1/polyfill.min.js?features=default,es6,Promise,fetch&flags=gated&ua=(MSIE%209.0)"
+FONT_AWESOME_URL="https://github.com/FortAwesome/Font-Awesome/blob/8027c940b6/assets/font-awesome-4.2.0.zip?raw=true"
+
+curl "$FALLBACK_POLYFILLS_URL" > ./public/fallback-polyfills.js
+
+rm -rf ./public/font-awesome
+curl --location "$FONT_AWESOME_URL" --output ./.font-awesome.zip
+unzip -q ./.font-awesome.zip -d ./.font-awesome.tmp
+mkdir ./public/font-awesome
+mv ./.font-awesome.tmp/*/{css,fonts} ./public/.font-awesome
+rm -rf ./.font-awesome.{zip,tmp}
