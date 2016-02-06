2908965852d14148b1bbd3f37e3e0d8f2e15eaf5
Brian Carstensen <brian.carstensen@gmail.com>
2016-02-06 11:21:08 -0600

    Separate out dev dependencies

--- bin/config.sh
+++ bin/config.sh
@@ -23,7 +23,8 @@ function flag_externals {
   out=""
   for module in $externals; do
     # Symlinked modules are assumed to be in development and aren't externalized.
-    if [[ "$module" != "express" && "$module" != "ejs" && "$module" != "babel" && "$module" != "coffee-react" && "$module" != "jsplumb" ]]; then
+    # JSPlumb is broken.
+    if [[ "$module" != "jsplumb" ]]; then
       [[ -L "node_modules/$module" ]] || out="$out --$1 $module"
     fi
   done
