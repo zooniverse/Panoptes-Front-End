41b17f9659b5a4fa7d6abde849597eaebdc1ad1c
Brian Carstensen <brian.carstensen@gmail.com>
2016-02-06 15:13:11 -0600

    Allow serving from a DEPLOY_SUBDIR

--- bin/serve.js
+++ bin/serve.js
@@ -5,15 +5,21 @@ var express = require('express');
 var port = process.env.PORT || 3735;
 var host = process.env.HOST || 'localhost';
 
+var root = '/' + process.env.DEPLOY_SUBDIR || '';
+
 var app = express();
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+var router = express.Router();
 
-app.use(function(req, res, next) {
+router.use(express.static('public'));
+
+router.use(function(req, res, next) {
   res.render('index');
 });
 
+app.use(root, router);
+
 app.listen(port, host, function() {
-  console.log('Panoptes-Front-End listening at http://' + host + ':' + port + '/');
+  console.log('Panoptes-Front-End listening at http://' + host + ':' + port + root);
 });
