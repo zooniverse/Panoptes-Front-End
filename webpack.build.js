d80d1a3aa44a4752f13bb814746b710ef1ceebdf
Amy Boyer <amy@zooniverse.org>
2016-04-28 15:09:49 -0500

    don't use hot module reloading when doing builds

---
+++ webpack.build.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var deploySubdir = '/' + (process.env.DEPLOY_SUBDIR || '');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    path.join(__dirname, 'app/main.cjsx')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: deploySubdir
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'public', to: '.' }
+    ]),
+    new HtmlWebpackPlugin({
+      baseUrl: deploySubdir,
+      template: 'views/index.ejs',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new ExtractTextPlugin("style.css", {
+        allChunks: true
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.cjsx', '.coffee', '.styl', '.css'],
+    modulesDirectories: ['.', 'node_modules']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      // explicitly include markdownz (and dependencies) to be transformed because it's es6
+      test: /\.jsx?$/,
+      include: /markdown/,
+      loader: 'babel'
+    }, {
+      test: /\.cjsx$/,
+      exclude: /node_modules/,
+      loaders: ['coffee', 'cjsx']
+    }, {
+      test: /\.coffee$/,
+      loader: 'coffee'
+    }, {
+      test: /\.json?$/,
+      loader: 'json'
+    }, {
+      test   : /\.css$/,
+      loaders: ['style', 'css']
+    }, {
+      test: /\.styl$/,
+      loader: ExtractTextPlugin.extract('style-loader','css-loader!stylus-loader')
+    }, {
+      test: /\.(jpg|png|gif|otf|eot|svg|ttf|woff\d?)$/,
+      loader: 'file-loader'
+    }],
+    // suppress warning about the fact that sugar-client is precompiled
+    noParse: [/sugar-client/]
+  }
+};
