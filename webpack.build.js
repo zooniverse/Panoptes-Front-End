ab3fd29513c4d17f0f792001a017a6e5cfde94d3
Amy Boyer <amy@zooniverse.org>
2016-05-06 12:32:06 -0500

    do not build .htaccess file, it won't help

--- webpack.build.js
+++ webpack.build.js
@@ -38,12 +38,6 @@ module.exports = {
       inject: 'body',
       filename: 'index.html'
     }),
-    new HtmlWebpackPlugin({
-      baseUrl: deploySubdir,
-      template: 'views/htaccess.ejs',
-      inject: false,
-      filename: '.htaccess'
-    }),
     new webpack.NoErrorsPlugin(),
     new ExtractTextPlugin("[name]-[contenthash].css", {
         allChunks: true
