fa799e5bdc4c2bc4bc2d00f24172573b32a33b53
Brian Carstensen <brian.carstensen@gmail.com>
2016-06-29 16:15:04 -0500

    Start adding sections

---
+++ app/pages/home-for-user/recent-projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RecentProjectsSection = React.createClass({
+  render() {
+    return (
+      <div>
+        Recent projects...
+      </div>
+    );
+  }
+});
+
+export default RecentProjectsSection;
