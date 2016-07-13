d85a26a89b0c4c32e26d0a880e8aaf99c620d841
Brian Carstensen <brian.carstensen@gmail.com>
2016-07-13 16:56:18 -0500

    Get the right data for the messages panel

--- app/pages/home-for-user/recent-messages.jsx
+++ app/pages/home-for-user/recent-messages.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePageSection from './generic-section';
+import apiClient from 'panoptes-client/lib/api-client';
 import talkClient from 'panoptes-client/lib/talk-client';
 
 const LOADER_BULLETS = '• • •';
@@ -18,9 +19,8 @@ const RecentCollectionsSection = React.createClass({
       loading: false,
       error: null,
       conversations: [],
-      converationPartners: {},
-      lastMessages: {},
       messageAuthors: {},
+      lastMessages: {},
     };
   },
 
@@ -38,8 +38,9 @@ const RecentCollectionsSection = React.createClass({
     this.setState({
       loading: true,
       error: null,
+      converationPartners: {},
       lastMessages: {},
-      users: {},
+      messageAuthors: {},
     });
 
     talkClient.type('conversations').get({
@@ -71,44 +72,52 @@ const RecentCollectionsSection = React.createClass({
     });
   },
 
-  fetchConversationPartner(conversation, notThisOne) {
-    return conversation.get('users')
+  fetchConversationPartner(conversation, currentUser) {
+    return apiClient.type('users').get(conversation.links.users) // Talk's user links are broken.
     .catch(() => {
       return [];
     })
     .then((users) => {
-      const partner = users.find((potentialPartner) => {
-        return potentialPartner !== notThisOne;
+      let partner = users.find((potentialPartner) => {
+        return potentialPartner !== currentUser;
       });
+      if (partner === undefined) {
+        // Why're you talking to yourself?
+        partner = currentUser;
+      }
       this.state.converationPartners[conversation.id] = partner;
       this.forceUpdate();
-    })
+      return partner;
+    });
   },
 
   fetchLastMessage(conversation) {
     return conversation.get('messages', {
       page_size: 1,
+      sort: '-created_at',
     })
     .catch(() => {
       return [];
     })
-    .then((messages) => {
-      const message = messages[0];
+    .then(([message]) => {
       this.state.lastMessages[conversation.id] = message;
       this.forceUpdate();
-
-      return this.fetchMessageAuthor(message);
+      return this.fetchMessageAuthor(message)
+      .then(() => {
+        return message;
+      });
     });
   },
 
   fetchMessageAuthor(message) {
-    message.get('user')
+    return apiClient.type('users').get(message.links.user) // Talk's user links are broken.
     .catch(() => {
       return null;
     })
     .then((author) => {
       this.state.messageAuthors[message.id] = author;
       this.forceUpdate();
+      return author;
     });
   },
 
@@ -123,15 +132,20 @@ const RecentCollectionsSection = React.createClass({
         {this.state.conversations.map((conversation) => {
           const partner = this.state.converationPartners[conversation.id];
           const message = this.state.lastMessages[conversation.id];
-          const messageAuthor = !!message && this.state.messageAuthors[message.id];
+          const sentLastMessage = !!message && (this.state.messageAuthors[message.id] === this.context.user);
 
           return (
             <div key={conversation.id}>
               <b>TITLE</b>: {conversation.title}{' '}
-              <b>WITH</b>: {!!partner ? partner.display_name : LOADER_BULLETS}
+              <b>
+                {sentLastMessage ? (
+                  'TO'
+                ) : (
+                  'FROM'
+                )}
+              </b>: {!!partner ? partner.display_name : LOADER_BULLETS}
               <br />
-              <b>LAST MESSAGE</b>: {!!message ? message.body : LOADER_BULLETS}{' '}
-              <b>BY</b>: {!!messageAuthor ? messageAuthor.display_name : LOADER_BULLETS}
+              <b>LAST MESSAGE</b>: {!!message ? message.body : LOADER_BULLETS}
               <hr />
             </div>
           );
