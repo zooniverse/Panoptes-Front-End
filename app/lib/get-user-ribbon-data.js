64b65e35629c737d512c5e994d5704f7bde61a82
Will Granger <wgranger@C02QVL7VFVH5.local>
2016-08-12 10:02:21 -0500

    Condense API Calls for Ribbon

--- app/lib/get-user-ribbon-data.js
+++ app/lib/get-user-ribbon-data.js
@@ -1,34 +1,23 @@
 import getAllProjectPreferences from './get-all-project-preferences';
 import getColorFromString from './get-color-from-string';
+import apiClient from 'panoptes-client/lib/api-client'
 
 function getUserRibbonData(user) {
   return getAllProjectPreferences(user).then((prefsResources) => {
     const awaitProjects = Promise.all(prefsResources.map((prefsResource) => {
-      return prefsResource.get('project').catch(() => {
+      return apiClient.type('projects').get(prefsResource.links.project).catch(() => {
         return null;
       });
     }));
 
-    const awaitOwners = awaitProjects.then((projects) => {
-      return Promise.all(projects.map((project) => {
-        if (project === null) {
-          return null;
-        } else {
-          return project.get('owner').catch(() => {
-            return null;
-          });
-        }
-      }));
-    });
-
-    return Promise.all([awaitProjects, awaitOwners]).then(([projects, owners]) => {
+    return Promise.all([awaitProjects]).then(([projects]) => {
       return prefsResources.map((prefsResource, i) => {
-        if (projects[i] !== null && owners[i] !== null) {
+        if (projects[i] !== null) {
           return {
             id: projects[i].id,
             slug: projects[i].slug,
             name: projects[i].display_name,
-            owner: owners[i].display_name,
+            owner: projects[i].links.owner.display_name,
             color: getColorFromString(projects[i].slug),
             classifications: prefsResource.activity_count,
           };
