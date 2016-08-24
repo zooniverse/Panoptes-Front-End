f29a3c4e510cda1b65b52370d028c44de2fda79a
Sarah Allen <sarah@zooniverse.org>
2016-08-24 15:10:05 -0500

    Styling and cancel button

--- app/components/workflow-assignment-dialog.jsx
+++ app/components/workflow-assignment-dialog.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dialog from 'modal-form/dialog';
+import ReactDOM from 'react-dom';
 
 const WorkflowAssignmentDialog = React.createClass({
   statics: {
@@ -19,6 +20,14 @@ const WorkflowAssignmentDialog = React.createClass({
     },
   },
 
+  handlePromotionDecline() {
+    // Hacky way to get the dialog to close.
+    // Need to rework Modal Form to allow custom cancel events on child DOM elements
+    const closeButton =
+      ReactDOM.findDOMNode(this).parentNode.querySelector('.modal-dialog-close-button');
+    closeButton.click();
+  },
+
   render() {
     return (
       <div className="content-container">
@@ -26,10 +35,14 @@ const WorkflowAssignmentDialog = React.createClass({
           Congratulations! Because you're doing so well, you can level up and
           access more types of glitches, have more options for classifying them,
           and see glitches that our computer algorithms are even less confident in.
-          If you prefer to stay at this level, you can choose to stay. You can switch
-          back to a previous workflow from the project home page.
+          If you prefer to stay at this level, you can choose to stay.
         </p>
-        <button type="submit">Try the new workflow</button>
+        <div className="workflow-assignment-dialog__buttons">
+          <button className="standard-button" type="submit">Try the new workflow</button>
+          <button className="minor-button" onClick={this.handlePromotionDecline} type="button">
+            No, thanks
+          </button>
+        </div>
       </div>
     );
   },
