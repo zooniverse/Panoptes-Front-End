c279a22897de5c1c596dfd8c081cd59389e32d84
Michael Parrish <michael@zooniverse.org>
2016-10-31 10:28:42 -0500

    Update seven-ten config

--- app/lib/split-config.js
+++ app/lib/split-config.js
@@ -1,7 +1,16 @@
 import {Client} from 'seven-ten';
 import apiClient from 'panoptes-client/lib/api-client';
 
-export default Client.config = {
-  host: 'http://localhost:3000',
-  headers: apiClient.headers
-};
+if (process.env.NODE_ENV === 'production') {
+  Client.config = {
+    host: 'https://seven-ten.zooniverse.org',
+    headers: apiClient.headers
+  };
+} else {
+  Client.config = {
+    host: 'https://seven-ten-staging.zooniverse.org',
+    headers: apiClient.headers
+  };
+}
+
+export default Client.config;
