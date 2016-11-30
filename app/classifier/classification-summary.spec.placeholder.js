f0278590cab192dbe265df9e58f6d73357ccafc1
CKrawczyk <coleman.krawczyk@gmail.com>
2016-11-30 12:54:07 +0000

    add non-working test for classification-summary

---
+++ app/classifier/classification-summary.spec.placeholder.js
@@ -0,0 +1,53 @@
+/* eslint prefer-arrow-callback: 0, func-names: 0, 'react/jsx-boolean-value': ['error', 'always'] */
+/* global describe, it, beforeEach */
+import React from 'react';
+import ClassificationSummary from './classification-summary';
+import assert from 'assert';
+import { shallow } from 'enzyme';
+
+const workflow = {
+  tasks: {
+    T0: {
+      type: 'single',
+      question: 'question one',
+      help: '',
+      answers: [
+        { label: 'answer one' },
+        { label: 'answer two' },
+      ],
+    },
+  },
+};
+
+const classification = {
+  annotations: [
+    {
+      task: 'T0',
+      value: 0,
+    },
+  ],
+};
+
+describe('ClassificationSummary', function () {
+  describe('if classification is provided', function () {
+    let wrapper;
+
+    beforeEach(function () {
+      wrapper = shallow(<ClassificationSummary workflow={workflow} classification={classification} />);
+    });
+
+    it('should render without crashing', function () {
+    });
+
+    it('should have a summary component for each annotation', function () {
+      const summaries = wrapper.find('div.classification-task-summary');
+      assert.equal(summaries.length, classification.annotations.length);
+    });
+  });
+
+  describe('if classification is not provided', function () {
+    it('should render without crashing', function () {
+      shallow(<ClassificationSummary />);
+    });
+  });
+});
