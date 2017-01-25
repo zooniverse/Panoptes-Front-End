2fa0ceddd8ef1ac8c4ce35f31e3eebf13236e676
Jim O'Donnell <jim@zooniverse.org>
2017-01-25 14:02:04 +0000

    Move discussion reply box (and convert to ES6)

---
+++ app/talk/discussion-comment.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Link } from 'react-router';
+import talkClient from 'panoptes-client/lib/talk-client';
+import Avatar from '../partials/avatar';
+import DisplayRoles from './lib/display-roles';
+import CommentBox from './comment-box';
+import SignInPrompt from '../partials/sign-in-prompt';
+import commentValidations from './lib/comment-validations';
+import alert from '../lib/alert';
+import { getErrors } from './lib/validations';
+
+class DiscussionComment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmitComment = this.onSubmitComment.bind(this);
+    this.commentValidations = this.commentValidations.bind(this);
+    this.state = {
+      commentValidationErrors: []
+    };
+  }
+
+  onSubmitComment(e, textContent, subject, reply) {
+    let comment = {
+      user_id: this.props.user.id,
+      discussion_id: +this.props.params.discussion,
+      body: textContent
+    };
+    if (!!subject && subject.id) {
+      comment = Object.assign(comment, {
+        focus_id: +subject.id,
+        focus_type: 'Subject'
+      });
+    }
+    if (reply) {
+      comment = Object.assign(comment, {
+        reply_id: reply.comment.id
+      });
+    }
+
+    return talkClient.type('comments')
+    .create(comment)
+    .save()
+    .then(this.props.onSubmitComment);
+  }
+
+  commentValidations(commentBody) {
+    // TODO: return true if any additional validations fail
+    const commentValidationErrors = getErrors(commentBody, commentValidations);
+    this.setState({ commentValidationErrors });
+    return !!commentValidationErrors.length;
+  }
+
+  promptToSignIn() {
+    alert(resolve => <SignInPrompt onChoose={resolve} />);
+  }
+
+  render() {
+    if (this.props.user) {
+      const baseLink = this.props.project ? `projects/${this.props.project.slug}` : '/';
+      return (
+        <div>
+          <div className="talk-comment-author">
+            <Avatar user={this.props.user} />
+            <p>
+              <Link to={`${baseLink}users/${this.props.user.login}`}>{this.props.user.display_name}</Link>
+            </p>
+            <div className="user-mention-name">@{this.props.user.login}</div>
+            <DisplayRoles roles={this.props.roles} section={this.props.discussion.section} />
+          </div>
+
+          <CommentBox
+            user={this.props.user}
+            project={this.props.project}
+            validationCheck={this.commentValidations}
+            validationErrors={this.state.commentValidationErrors}
+            onSubmitComment={this.onSubmitComment}
+            reply={this.props.reply}
+            logSubmit={true}
+            onClickClearReply={this.props.onClearReply}
+            header={null}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <p>Please <button className="link-style" type="button" onClick={this.promptToSignIn}>sign in</button> to contribute to the discussion</p>
+      );
+    }
+  }
+}
+
+DiscussionComment.propTypes = {
+  params: React.PropTypes.object,
+  project: React.PropTypes.object,
+  user: React.PropTypes.object,
+  roles: React.PropTypes.array,
+  discussion: React.PropTypes.object,
+  reply: React.PropTypes.object,
+  onSubmitComment: React.PropTypes.func,
+  onClearReply: React.PropTypes.func
+};
+
+export default DiscussionComment;
