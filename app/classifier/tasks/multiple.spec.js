d25d534a828ace87c478a89eecc4a791691686e6
Jim O'Donnell <jim@zooniverse.org>
2017-01-31 14:28:17 +0000

    Multiple choice rendering tests Also tests for static methods on SingleAnswerTask and MultipleAnswerTask.

--- app/classifier/tasks/multiple.spec.js
+++ app/classifier/tasks/multiple.spec.js
@@ -1,60 +1,61 @@
 import React from 'react';
 import assert from 'assert';
 import { mount } from 'enzyme';
-import SingleTask from './single';
+import MultipleTask from './multiple';
 
 const task = {
   question: 'Is there something here?',
   answers: [
     { label: 'Yes', value: 'yes' },
-    { label: 'No', value: 'no' }
+    { label: 'No', value: 'no' },
+    { label: 'Maybe', value: 'maybe' }
   ],
-  required: true
+  required: 3
 };
 
 const annotation = {
-  value: 1
+  value: [0,1]
 };
 
-describe('SingleChoiceTask', function () {
+describe('MultipleChoiceTask', function () {
   let wrapper;
 
   beforeEach(function () {
-    wrapper = mount(<SingleTask task={task} annotation={annotation} />);
+    wrapper = mount(<MultipleTask task={task} annotation={annotation} />);
   });
 
   it('should render without crashing', function () {
   });
-  
+
   it('should have a question', function () {
     const question = wrapper.find('.question');
     assert.equal(question.length, 1);
   });
-  
+
   it('should have answers', function () {
     const answers = wrapper.find('.answer');
     assert.equal(answers.length, task.answers.length);
   });
-  
+
   it('should have the supplied annotation checked', function(){
-    assert.equal(wrapper.find('input[type="radio"]').find({checked: true, value: 1}).length, 1);
+    assert.equal(wrapper.find('input[type="checkbox"]').find({checked: true}).length, 2);
   });
-  
+
   it('other answers should not be checked', function(){
-    assert.equal(wrapper.find('input[type="radio"]').find({checked: false, value: 0}).length, 1);
+    assert.equal(wrapper.find('input[type="checkbox"]').find({checked: false}).length, 1);
   });
 
   describe('static methods', function(){
-    it('should be complete', function(){
-      assert.equal(SingleTask.isAnnotationComplete(task, annotation), true);
+    it('should be incomplete', function(){
+      assert.equal(MultipleTask.isAnnotationComplete(task, annotation), false);
     });
 
     it('should have the correct question text', function(){
-      assert.equal(SingleTask.getTaskText(task), task.question);
+      assert.equal(MultipleTask.getTaskText(task), task.question);
     });
 
-    it('the default annotation should be null', function(){
-      assert.equal(SingleTask.getDefaultAnnotation().value, null);
+    it('the default annotation should be an empty array', function(){
+      assert.equal(MultipleTask.getDefaultAnnotation().value.length, 0);
     });
   });
 
