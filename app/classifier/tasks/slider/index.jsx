6ddd7c0cfefbddaf2c3cf0fe086c816e228bac96
CKrawczyk <coleman.krawczyk@gmail.com>
2017-02-23 14:41:46 +0000

    refactored handleChange and fixed eslint errors

--- app/classifier/tasks/slider/index.jsx
+++ app/classifier/tasks/slider/index.jsx
@@ -1,7 +1,7 @@
+import { Markdown } from 'markdownz';
 import React from 'react';
 import GenericTask from '../generic';
 import SliderTaskEditor from './editor';
-import { Markdown } from 'markdownz';
 
 const NOOP = Function.prototype;
 
@@ -14,7 +14,9 @@ class Summary extends React.Component {
     return (
       <div>
         <div className="question">
-          {this.props.task.instruction}
+          <Markdown tag="span" inline={true}>
+            {this.props.task.instruction}
+          </Markdown>
         </div>
         <div className="answer">
           {answer}
@@ -41,45 +43,26 @@ Summary.propTypes = {
 class SliderTask extends React.Component {
   constructor(props) {
     super(props);
-    this.task = Object.assign(
-      {
-        help: '',
-        instruction: '',
-        required: false,
-        max: 1,
-        min: 0,
-        step: 0.1,
-        defaultValue: 0
-      },
-      this.props.task
-    );
-    let value = this.task.defaultValue;
+    let value = this.props.task.defaultValue;
     if (this.props.annotation.value !== null) {
       value = this.props.annotation.value;
     }
     this.state = { value };
     this.handleChange = this.handleChange.bind(this);
   }
-  handleChange() {
-    let value = 0;
-    if (parseFloat(this.numeric.value) !== this.state.value) {
-      if (this.numeric.value !== '') {
-        value = Math.max(
-          Math.min(parseFloat(this.numeric.value), this.task.max),
-          this.task.min
-        );
-      } else return;
-    } else {
-      value = Math.max(
-        Math.min(parseFloat(this.slider.value), this.task.max),
-        this.task.min
-      );
+
+  handleChange(e) {
+    let value = e.target.value;
+    if (value > this.props.task.max) {
+      value = this.props.task.max;
+    } else if (value < this.props.task.min) {
+      value = this.props.task.min;
     }
-    value = String(value);
-    const newAnnotation = Object.assign(this.props.annotation, { value });
-    this.props.onChange(newAnnotation);
     this.setState({ value });
+    const newAnnotation = Object.assign({}, this.props.annotation, { value });
+    this.props.onChange(newAnnotation);
   }
+
   render() {
     return (
       <div>
@@ -89,29 +72,29 @@ class SliderTask extends React.Component {
           required={this.props.task.required}
         >
           <div className="standard-input full">
-            <label className="answer">
+            <label htmlFor="sliderInput" className="answer">
               <input
                 type="range"
                 autoFocus={this.props.autoFocus}
-                ref={ r => { this.slider = r; }}
                 onChange={this.handleChange}
-                max={this.task.max}
-                min={this.task.min}
-                step={this.task.step}
+                max={this.props.task.max}
+                min={this.props.task.min}
+                step={this.props.task.step}
                 value={this.state.value}
+                id="sliderInput"
               />
             </label>
-            <label className="answer">
+            <label htmlFor="textInput" className="answer">
               <input
                 type="number"
                 autoFocus={this.props.autoFocus}
-                ref={ r => { this.numeric = r; }}
                 onChange={this.handleChange}
-                max={this.task.max}
-                min={this.task.min}
+                max={this.props.task.max}
+                min={this.props.task.min}
                 step="any"
                 value={this.state.value}
                 style={{ minWidth: '30px' }}
+                id="textInput"
               />
             </label>
           </div>
@@ -159,7 +142,8 @@ SliderTask.propTypes = {
   annotation: React.PropTypes.shape(
     { value: React.PropTypes.string }
   ),
-  onChange: React.PropTypes.func
+  onChange: React.PropTypes.func,
+  autoFocus: React.PropTypes.bool
 };
 
 SliderTask.defaultProps = {
