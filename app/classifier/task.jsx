8101a4b6157a67164ab486ae3fc2eec62731c11f
Jim O'Donnell <jim@zooniverse.org>
2017-02-27 16:44:38 +0000

    Convert callbacks to component methods Task component is now responsible for updating annotations and classifications.

--- app/classifier/task.jsx
+++ app/classifier/task.jsx
@@ -9,11 +9,32 @@ import GridTool from './drawing-tools/grid';
 import Intervention from '../lib/intervention';
 import Shortcut from './tasks/shortcut';
 
-const Task = (props) => {
-  const { annotation, classification, task, workflow } = props;
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAnnotationChange = this.handleAnnotationChange.bind(this);
+    this.addAnnotationForTask = this.addAnnotationForTask.bind(this);
+    this.completeClassification = this.completeClassification.bind(this);
+    this.destroyCurrentAnnotation = this.destroyCurrentAnnotation.bind(this);
+  }
+  
+  componentWillMount() {
+    const {workflow, classification} = this.props;
+    classification.annotations = classification.annotations ? classification.annotations : [];
+    if (classification.annotations.length === 0) {
+      this.addAnnotationForTask(workflow.first_task);
+    }
+  }
+
+  handleAnnotationChange(newAnnotation) {
+    const {workflow, classification} = this.props;
+    classification.annotations[classification.annotations.length - 1] = newAnnotation;
+    classification.update('annotations');
+  }
 
   // Next (or first question)
-  function addAnnotationForTask(taskKey) {
+  addAnnotationForTask(taskKey) {
+    const {workflow, classification} = this.props;
     const taskDescription = workflow.tasks[taskKey];
     let annotation = tasks[taskDescription.type].getDefaultAnnotation(taskDescription, workflow, tasks);
     annotation.task = taskKey;
@@ -29,34 +50,9 @@ const Task = (props) => {
     classification.update('annotations');
   }
 
-  classification.annotations = classification.annotations ? classification.annotations : [];
-  if (classification.annotations.length === 0) {
-    addAnnotationForTask(workflow.first_task);
-  }
-
-  if (!task) {
-    return null;
-  }
-
-  function handleAnnotationChange(newAnnotation) {
-    classification.annotations[classification.annotations.length - 1] = newAnnotation;
-    classification.update('annotations');
-  }
-
-  // Back
-  function destroyCurrentAnnotation() {
-    const lastAnnotation = classification.annotations[classification.annotations.length - 1];
-
-    classification.annotations.pop();
-    classification.update('annotations');
-
-    if (workflow.configuration.persist_annotations) {
-      CacheClassification.update(lastAnnotation);
-    }
-  }
-
   // Done
-  function completeClassification() {
+  completeClassification() {
+    const {workflow, classification} = this.props;
     if (workflow.configuration.persist_annotations) {
       CacheClassification.delete();
     }
@@ -77,7 +73,6 @@ const Task = (props) => {
         height: innerHeight
       }
     });
-      
 
     if (currentAnnotation.shortcut) {
       addAnnotationForTask(currentTask.unlinkedTask);
@@ -85,148 +80,177 @@ const Task = (props) => {
       newAnnotation.value = currentAnnotation.shortcut.index;
       delete currentAnnotation.shortcut;
     }
-    props.completeClassification();
+    this. props.completeClassification();
   }
 
-  const disableTalk = classification.metadata.subject_flagged;
-  const visibleTasks = Object.keys(workflow.tasks).filter(key => workflow.tasks[key].type !== 'shortcut');
-  const TaskComponent = tasks[task.type];
+  // Back
+  destroyCurrentAnnotation() {
+    const {workflow, classification} = this.props;
+    const lastAnnotation = classification.annotations[classification.annotations.length - 1];
 
-  // Should we disable the "Back" button?
-  const onFirstAnnotation = (classification.annotations.indexOf(annotation) === 0);
+    classification.annotations.pop();
+    classification.update('annotations');
 
-  // Should we disable the "Next" or "Done" buttons?
-  let waitingForAnswer;
-  if (TaskComponent && TaskComponent.isAnnotationComplete) {
-    waitingForAnswer = !props.annotation.shortcut && !TaskComponent.isAnnotationComplete(task, annotation, workflow);
-  }
-
-  // Each answer of a single-answer task can have its own `next` key to override the task's.
-  let nextTaskKey;
-  if (TaskComponent === tasks.single) {
-    const currentAnswer = task.answers[annotation.value];
-    nextTaskKey = currentAnswer ? currentAnswer.next : '';
-  } else {
-    nextTaskKey = task.next;
-  }
-
-  if (!!nextTaskKey && !props.workflow.tasks[nextTaskKey]) {
-    nextTaskKey = '';
-  }
-
-  // TODO: Actually disable things that should be.
-  // For now we'll just make them non-mousable.
-  const disabledStyle = {
-    opacity: 0.5,
-    pointerEvents: 'none'
-  };
-
-  // Run through the existing annotations to build up sets of persistent hooks in the order of the associated annotations. Skip duplicates.
-  const persistentHooksBeforeTask = [];
-  const persistentHooksAfterTask = [];
-  props.classification.annotations.map(annotation => {
-    const taskDescription = props.workflow.tasks[annotation.task];
-    const { PersistBeforeTask, PersistAfterTask } = tasks[taskDescription.type];
-    if (PersistBeforeTask && !persistentHooksBeforeTask.includes(PersistBeforeTask)) {
-      persistentHooksBeforeTask.push(PersistBeforeTask);
+    if (workflow.configuration.persist_annotations) {
+      CacheClassification.update(lastAnnotation);
     }
-    if (PersistAfterTask && !persistentHooksAfterTask.includes(PersistAfterTask)) {
-      persistentHooksAfterTask.push(PersistAfterTask);
+  }
+
+  render() {
+    const { annotation, classification, workflow } = this.props;
+    if (!annotation) {
+      return null;
     }
-  });
+    const task = this.props.task ? this.props.task : workflow.tasks[workflow.first_task];
 
-  // These props will be passed into the hooks. Append as necessary when creating hooks.
-  const taskHookProps = {
-    taskTypes: tasks,
-    workflow: props.workflow,
-    classification: props.classification,
-    onChange: props.classification.update
-  };
+    const disableTalk = classification.metadata.subject_flagged;
+    const visibleTasks = Object.keys(workflow.tasks).filter(key => workflow.tasks[key].type !== 'shortcut');
+    const TaskComponent = tasks[task.type];
 
-  return (
-    <div className="task-container" style={disabledStyle ? props.subjectLoading : ''}>
-      {!!props.renderIntervention &&
-        <Intervention
-          user={props.user}
-          experimentName={interventionMonitor.latestFromSugar['experiment_name']}
-          sessionID={getSessionID()}
-          interventionID={interventionMonitor.latestFromSugar['next_event']}
-          interventionDetails={experimentsClient.constructInterventionFromSugarData(interventionMonitor.latestFromSugar)}
-          disableInterventionFunction={props.disableIntervention}
-        />
+    // Should we disable the "Back" button?
+    const onFirstAnnotation = (classification.annotations.indexOf(annotation) === 0);
+
+    // Should we disable the "Next" or "Done" buttons?
+    let waitingForAnswer;
+    if (TaskComponent && TaskComponent.isAnnotationComplete) {
+      waitingForAnswer = !annotation.shortcut && !TaskComponent.isAnnotationComplete(task, annotation, workflow);
+    }
+
+    // Each answer of a single-answer task can have its own `next` key to override the task's.
+    let nextTaskKey;
+    if (TaskComponent === tasks.single) {
+      const currentAnswer = task.answers[annotation.value];
+      nextTaskKey = currentAnswer ? currentAnswer.next : '';
+    } else {
+      nextTaskKey = task.next;
+    }
+
+    if (nextTaskKey && !workflow.tasks[nextTaskKey]) {
+      nextTaskKey = '';
+    }
+
+    // TODO: Actually disable things that should be.
+    // For now we'll just make them non-mousable.
+    const disabledStyle = {
+      opacity: 0.5,
+      pointerEvents: 'none'
+    };
+
+    // Run through the existing annotations to build up sets of persistent hooks in the order of the associated annotations. Skip duplicates.
+    const persistentHooksBeforeTask = [];
+    const persistentHooksAfterTask = [];
+    classification.annotations.map(annotation => {
+      const taskDescription = workflow.tasks[annotation.task];
+      const { PersistBeforeTask, PersistAfterTask } = tasks[taskDescription.type];
+      if (PersistBeforeTask && !persistentHooksBeforeTask.includes(PersistBeforeTask)) {
+        persistentHooksBeforeTask.push(PersistBeforeTask);
       }
-      <div className="coverable-task-container">
-        {persistentHooksBeforeTask.map((HookComponent, i) => {
-          const key = i + Math.random();
-          <HookComponent key={key} {...taskHookProps} />})
+      if (PersistAfterTask && !persistentHooksAfterTask.includes(PersistAfterTask)) {
+        persistentHooksAfterTask.push(PersistAfterTask);
+      }
+    });
+
+    // These props will be passed into the hooks. Append as necessary when creating hooks.
+    const taskHookProps = {
+      taskTypes: tasks,
+      workflow: workflow,
+      classification: classification,
+      onChange: classification.update
+    };
+
+    return (
+      <div className="task-container" style={disabledStyle ? this.props.subjectLoading : ''}>
+        {!!this.props.renderIntervention &&
+          <Intervention
+            user={props.user}
+            experimentName={interventionMonitor.latestFromSugar['experiment_name']}
+            sessionID={getSessionID()}
+            interventionID={interventionMonitor.latestFromSugar['next_event']}
+            interventionDetails={experimentsClient.constructInterventionFromSugarData(interventionMonitor.latestFromSugar)}
+            disableInterventionFunction={this.props.disableIntervention}
+          />
         }
+        <div className="coverable-task-container">
+          {persistentHooksBeforeTask.map((HookComponent, i) => {
+            const key = i + Math.random();
+            <HookComponent key={key} {...taskHookProps} />})
+          }
 
-        <TaskComponent autoFocus={true} taskTypes={tasks} workflow={props.workflow} task={task} preferences={props.preferences} annotation={annotation} onChange={handleAnnotationChange} />
-
-        {persistentHooksAfterTask.map((HookComponent, i) => {
-          const key = i + Math.random();
-          <HookComponent key={key} {...taskHookProps} />})
-        }
-
-        <hr />
-
-        {!!task.unlinkedTask && 
-          <Shortcut
-            task={task}
+          <TaskComponent
+            autoFocus={true}
+            taskTypes={tasks}
             workflow={workflow}
+            task={task}
+            preferences={this.props.preferences}
             annotation={annotation}
-            classification={classification}
-          />}
+            onChange={this.handleAnnotationChange}
+          />
 
-        <nav className="task-nav">
-          {(visibleTasks.length > 1) && 
-            <button type="button"
-              className="back minor-button"
-              disabled={onFirstAnnotation}
-              onClick={destroyCurrentAnnotation}
-              onMouseEnter={props.warningToggleOn}
-              onFocus={props.warningToggleOn}
-              onMouseLeave={props.warningToggleOff}
-              onBlur={props.warningToggleOff}
-            >
-              Back
-            </button>}
-          {(!nextTaskKey && props.workflow.configuration.hide_classification_summaries && props.project && !disableTalk) && 
-            <Link
-              onClick={props.completeClassification}
-              to={`/projects/${props.project.slug}/talk/subjects/${props.subject.id}`}
-              className="talk standard-button"
-              style={waitingForAnswer ? disabledStyle : {}}
-            >
-              Done &amp; Talk
-            </Link>}
-          {(nextTaskKey && !annotation.shortcut) ? 
-            <button
-              type="button"
-              className="continue major-button"
-              disabled={waitingForAnswer}
-              onClick={addAnnotationForTask.bind(this, nextTaskKey)}
-            >
-              Next
-            </button> :
-            <button
-              type="button"
-              className="continue major-button"
-              disabled={waitingForAnswer}
-              onClick={completeClassification}
-            >
-              {props.demoMode && <i className="fa fa-trash fa-fw"></i>}
-              {props.classification.gold_standard && <i className="fa fa-star fa-fw"></i>}
-              {' '}Done
-            </button>}
-          {props.renderExpertOptions()}
-        </nav>
-        { props.backButtonWarning && props.renderBackButtonWarning()}
+          {persistentHooksAfterTask.map((HookComponent, i) => {
+            const key = i + Math.random();
+            <HookComponent key={key} {...taskHookProps} />})
+          }
 
-        {props.children}
+          <hr />
+
+          {!!task.unlinkedTask && 
+            <Shortcut
+              task={task}
+              workflow={workflow}
+              annotation={annotation}
+              classification={classification}
+            />}
+
+          <nav className="task-nav">
+            {(visibleTasks.length > 1) && 
+              <button type="button"
+                className="back minor-button"
+                disabled={onFirstAnnotation}
+                onClick={this.destroyCurrentAnnotation}
+                onMouseEnter={this.props.warningToggleOn}
+                onFocus={this.props.warningToggleOn}
+                onMouseLeave={this.props.warningToggleOff}
+                onBlur={this.props.warningToggleOff}
+              >
+                Back
+              </button>}
+            {(!nextTaskKey && workflow.configuration.hide_classification_summaries && this.props.project && !disableTalk) && 
+              <Link
+                onClick={this.completeClassification}
+                to={`/projects/${this.props.project.slug}/talk/subjects/${this.props.subject.id}`}
+                className="talk standard-button"
+                style={waitingForAnswer ? disabledStyle : {}}
+              >
+                Done &amp; Talk
+              </Link>}
+            {(nextTaskKey && !annotation.shortcut) ? 
+              <button
+                type="button"
+                className="continue major-button"
+                disabled={waitingForAnswer}
+                onClick={this.addAnnotationForTask.bind(this, nextTaskKey)}
+              >
+                Next
+              </button> :
+              <button
+                type="button"
+                className="continue major-button"
+                disabled={waitingForAnswer}
+                onClick={this.completeClassification}
+              >
+                {this.props.demoMode && <i className="fa fa-trash fa-fw"></i>}
+                {classification.gold_standard && <i className="fa fa-star fa-fw"></i>}
+                {' '}Done
+              </button>}
+            {this.props.renderExpertOptions()}
+          </nav>
+          {this.props.backButtonWarning && this.props.renderBackButtonWarning()}
+
+          {this.props.children}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 Task.propTypes ={
