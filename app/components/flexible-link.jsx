428eb8b6228976c36218f33b61613df8849eeb23
Jim O'Donnell <jim@eatyourgreens.org.uk>
2017-03-08 15:12:38 +0000

    Don't use Link for external project URLs on the home page (#3559)
    Fixes #3557

--- app/components/flexible-link.jsx
+++ app/components/flexible-link.jsx
@@ -10,27 +10,28 @@ export default class FlexibleLink extends React.Component {
     }
 
     if (this.props.to.indexOf('http') > -1) {
-      return (<a href={this.props.to}>{this.props.children}</a>);
+      return (<a className={this.props.className} href={this.props.to}>{this.props.children}</a>);
     }
 
-    return (<Link to={this.props.to} onClick={clickHandler.bind(this, this.props.logText)}>{this.props.children}</Link>);
+    return (<Link className={this.props.className} to={this.props.to} onClick={clickHandler.bind(this, this.props.logText)}>{this.props.children}</Link>);
   }
 }
 
 FlexibleLink.contextTypes = {
-  geordi: React.PropTypes.object,
+  geordi: React.PropTypes.object
 };
 
 FlexibleLink.propTypes = {
   children: React.PropTypes.node,
+  className: React.PropTypes.string,
   geordiHandler: React.PropTypes.string,
   logText: React.PropTypes.string,
-  to: React.PropTypes.string.isRequired,
+  to: React.PropTypes.string.isRequired
 };
 
 FlexibleLink.defaultProps = {
   children: null,
   geordiHandler: null,
   logText: null,
-  to: '',
+  to: ''
 };
