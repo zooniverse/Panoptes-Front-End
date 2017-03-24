d57507423a131683660fd42a3c1b72715a799335
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2017-03-24 14:11:11 +0000

    Fix App Status component - better colours on Project Page and no more 'empty' text (#3641)
    * Fix colours on project pages, ignore text if it's just whitespace
    
    * Check for null text properly
    
    * Different URLs for staging/development and production

--- app/partials/app-status.jsx
+++ app/partials/app-status.jsx
@@ -28,7 +28,9 @@ feature specs.
 
 import React from 'react';
 
-const APP_STATUS_URL = 'https://static.zooniverse.org/zooniverse.org-status.txt';
+const APP_STATUS_URL = (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'development')
+  ? 'https://static.zooniverse.org/zooniverse.org-status-TEST.txt'
+  : 'https://static.zooniverse.org/zooniverse.org-status.txt';
 
 export default class AppStatus extends React.Component {
   constructor(props) {
@@ -53,12 +55,13 @@ export default class AppStatus extends React.Component {
     })
     .then((text) => {
       console.log('AppStatus: Received status data from ' + APP_STATUS_URL + '.');
-      if (!text || text === '') {
+      const cleanedText = (text) ? text.trim() : '';  //If text is just white space or newlines...
+      if (cleanedText === '') {  //...ignore it.
         console.log('AppStatus: Nothing to report.');
       } else {
         this.setState({
           show: true,
-          message: text,
+          message: cleanedText,
         });
       }
     })
