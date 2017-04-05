b55e36ad3806f79df89100003c1a6d8a3f925e81
Jim O'Donnell <jim@eatyourgreens.org.uk>
2017-04-05 17:05:22 +0100

    Bugfix: check for workflow (#3696)

--- app/components/frame-viewer.jsx
+++ app/components/frame-viewer.jsx
@@ -40,7 +40,7 @@ export default class FrameViewer extends React.Component {
   }
 
   render() {
-    const zoomEnabled = this.props.workflow.configuration.pan_and_zoom;
+    const zoomEnabled = this.props.workflow && this.props.workflow.configuration.pan_and_zoom;
     const FrameWrapper = this.props.frameWrapper;
     const { type, format, src } = getSubjectLocation(this.props.subject, this.props.frame);
 
