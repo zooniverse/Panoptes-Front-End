d3673a3c2f3af00e538f276b406c8a6dbc16f0b8
srallen <srallen@users.noreply.github.com>
2017-04-12 09:33:50 -0500

    Fav button into ES6 (#3717)
    * Convert favorites button to ES6. Add unit tests
    
    * Fix TypeError when favorites collection doesnt have subjects

---
+++ app/collections/favorites-button.spec.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import assert from 'assert';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+import FavoritesButton from './favorites-button';
+
+const subject = { id: '4' };
+
+describe('<Favorites Button />', function() {
+  let wrapper;
+  let toggleFavoriteSpy;
+  let button;
+  let icon;
+  before(function() {
+    toggleFavoriteSpy = sinon.spy(FavoritesButton.prototype, 'toggleFavorite');
+    wrapper = shallow(<FavoritesButton subject={subject} />);
+    button = wrapper.find('button');
+    icon = wrapper.find('i');
+  });
+
+  it('should render a button and an icon', function() {
+    assert.equal(button.length, 1);
+    assert.equal(icon.length, 1);
+  });
+
+  it('should render an empty heart icon if subject is not a favorite', function() {
+    assert.equal(icon.props().className.includes('fa-heart-o'), true);
+    assert.equal(wrapper.state('favorited'), false);
+  });
+
+  it('should render a filled heart icon if subject is a favorite', function() {
+    wrapper.setProps({ isFavorite: true });
+    assert.equal(icon.props().className.includes('fa-heart'), true);
+    assert.equal(wrapper.state('favorited'), true);
+  });
+
+  it('should call toggleFavorite on click', function() {
+    button.simulate('click');
+    sinon.assert.calledOnce(toggleFavoriteSpy);
+  });
+});
--- app/collections/get-favorites-name.cjsx
+++
@@ -1,7 +0,0 @@
-# Get the name of the favorites collection for a project
-module.exports = (project) ->
-  #--> Naming Convention: "Favorites for #{project_id}"
-  #    Prevents global uniqueness validation conflict under project scope
-  name = "Favorites"
-  name += " (#{project.slug})" if project?
-  name
