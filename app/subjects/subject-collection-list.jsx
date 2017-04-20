2f407f4cff1d71e46dd6a53016d845379f863f77
Sarah Allen <sarah@zooniverse.org>
2017-04-20 11:05:22 -0500

    Fix collection card link

--- app/subjects/subject-collection-list.jsx
+++ app/subjects/subject-collection-list.jsx
@@ -4,40 +4,38 @@ import CollectionCard from '../pages/collections/collection-card';
 import Paginator from '../talk/lib/paginator';
 
 const SubjectCollectionList = (props) => {
-  let meta;
-  if (!props.collections) {
-    return (<Loading />);
-  }
-
   if (props.collections && props.collections.length === 0) {
     return (<p>There are no collections yet</p>);
   }
 
-  if (props.collections) {
-    meta = props.collections[0].getMeta();
+  if (props.collections && props.project) {
+    const meta = props.collections[0].getMeta();
+
+    return (
+      <div className="collection-card-list">
+        <h2>Collections:</h2>
+        <div>
+          {props.collections.map((collection) => {
+            return (
+              <CollectionCard
+                key={`collection-${collection.id}`}
+                collection={collection}
+                linkTo={`/projects/${props.project.slug}/collections/${collection.slug}`}
+              />
+            );
+          })}
+        </div>
+        <Paginator
+          className="talk"
+          page={meta.page}
+          onPageChange={props.onCollectionsPageChange}
+          pageCount={meta.page_count}
+        />
+      </div>
+    );
   }
 
-  return (
-    <div className="collection-card-list">
-      <h2>Collections:</h2>
-      <div>
-        {props.collections.map((collection) => {
-          return (
-            <CollectionCard
-              key={`collection-${collection.id}`}
-              collection={collection}
-            />
-          );
-        })}
-      </div>
-      <Paginator
-        className='talk'
-        page={meta.page}
-        onPageChange={props.onCollectionsPageChange}
-        pageCount={meta.page_count}
-      />
-    </div>
-  );
+  return (<Loading />);
 };
 
 SubjectCollectionList.defaultProps = {
@@ -47,7 +45,10 @@ SubjectCollectionList.defaultProps = {
 
 SubjectCollectionList.propTypes = {
   collections: React.PropTypes.arrayOf(React.PropTypes.object),
-  project: React.PropTypes.object
+  onCollectionsPageChange: React.PropTypes.func,
+  project: React.PropTypes.shape({
+    slug: React.PropTypes.string
+  })
 };
 
 export default SubjectCollectionList;
