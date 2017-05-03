d580d0923f4f3a2b457edb05334413d73f5e3eaa
Roger Hutchings <roger@hutchin.gs>
2017-05-03 16:53:16 +0100

    Finish ruleset tests

--- app/classifier/feedback/feedback-ruleset.spec.js
+++ app/classifier/feedback/feedback-ruleset.spec.js
@@ -7,22 +7,24 @@ import FeedbackRuleSet from './feedback-ruleset';
 
 const SUBJECT = {
   metadata: {
-    '#feedback_1_type': 'foo'
+    '#feedback_1_type': 'foo',
+    '#feedback_1_ping': 'pong',
+    '#feedback_1_fizz': 'buzz',
+    '#feedback_2_see': 'saw'
   }
 };
 
 const TASK = {
   feedback: {
     types: [
-      {
-        id: 'foo',
-        valid: false,
-      }
+      { id: 'foo', valid: false }
     ],
     enabled: true
   }
 };
 
+const successMessage = 'great success';
+const failureMessage = 'epic fail';
 
 describe('FeedbackRuleSet class', function () {
   it('should always return an array', function () {
@@ -40,8 +42,44 @@ describe('FeedbackRuleSet class', function () {
     assert(validRuleset.rules.length === 1);
   });
 
-  it('should use a subject success message if present');
-  it('should use a subject failure message if present');
-  it('should use the default success message if there\'s no subject success message');
-  it('should use the default failure message if there\'s no subject failure message');
+  it('should use a subject success message if present instead of the task one', function () {
+    const subjectWithSuccess = Object.assign({}, SUBJECT);
+    const taskWithSuccess = Object.assign({}, TASK);
+    subjectWithSuccess.metadata['#feedback_1_successMessage'] = successMessage;
+    taskWithSuccess.feedback.types[0].defaultSuccessMessage = 'foobar';
+    const ruleset = new FeedbackRuleSet(subjectWithSuccess, taskWithSuccess);
+    assert(ruleset.rules[0].successMessage === successMessage);
+  });
+
+  it('should use a subject failure message if present instead of the task one', function () {
+    const subjectWithFailure = Object.assign({}, SUBJECT);
+    const taskWithFailure = Object.assign({}, TASK);
+    subjectWithFailure.metadata['#feedback_1_failureMessage'] = failureMessage;
+    taskWithFailure.feedback.types[0].defaultFailureMessage = 'foobar';
+    const ruleset = new FeedbackRuleSet(subjectWithFailure, taskWithFailure);
+    assert(ruleset.rules[0].failureMessage === failureMessage);
+  });
+
+  it('should use the default success message if there\'s no subject success message', function () {
+    const taskWithSuccess = Object.assign({}, TASK);
+    taskWithSuccess.feedback.types[0].defaultSuccessMessage = successMessage;
+    const ruleset = new FeedbackRuleSet(SUBJECT, taskWithSuccess);
+    assert(ruleset.rules[0].successMessage === successMessage);
+  });
+
+  it('should use the default failure message if there\'s no subject failure message', function () {
+    const taskWithFailure = Object.assign({}, TASK);
+    taskWithFailure.feedback.types[0].defaultFailureMessage = failureMessage;
+    const ruleset = new FeedbackRuleSet(SUBJECT, taskWithFailure);
+    assert(ruleset.rules[0].failureMessage === failureMessage);
+  });
+
+  it.only('should copy over any other feedback fields set in the subject to the returned rule', function () {
+    const validTask = Object.assign({}, TASK);
+    validTask.feedback.types[0].valid = true;
+    const ruleset = new FeedbackRuleSet(SUBJECT, validTask);
+    assert(ruleset.rules[0].ping === 'pong');
+    assert(ruleset.rules[0].fizz === 'buzz');
+    assert(ruleset.rules[0].see === undefined);
+  });
 });
