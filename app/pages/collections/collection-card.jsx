723461ab76efdc1410704028c4c11b65f00afc3c
Jim O'Donnell <jim@zooniverse.org>
2017-05-11 15:32:58 +0100

    Check for image src before setting a collection preview

--- app/pages/collections/collection-card.jsx
+++ app/pages/collections/collection-card.jsx
@@ -13,7 +13,7 @@ export default class CollectionCard extends React.Component {
   }
 
   setSubjectPreview(src) {
-    const splitSrc = src.split('.');
+    const splitSrc = src ? src.split('.') : [];
     if (src && splitSrc[splitSrc.length - 1] !== 'mp4') {
       this.collectionCard.style.backgroundImage = `url('${src}')`;
       this.collectionCard.style.backgroundPosition = 'initial';
