45b6ecc47ad11a0e88d342296f4faab26c534653
Simone Duca <simone@zooniverse.org>
2017-06-05 14:41:59 +0100

    Fix tests and make component a stateless function

--- app/pages/home-common/featured-project.spec.js
+++ app/pages/home-common/featured-project.spec.js
@@ -3,7 +3,7 @@ import React from 'react';
 import assert from 'assert';
 import FeaturedProject from './featured-project';
 
-describe.only('FeaturedProject', () => {
+describe('FeaturedProject', () => {
   it('should render without crashing', () => {
     shallow(<FeaturedProject />);
   });
@@ -13,14 +13,22 @@ describe.only('FeaturedProject', () => {
     assert.strictEqual(wrapper.type(), 'section');
   });
 
-  it('should have an img tag', () => {
-    const wrapper = shallow(<FeaturedProject />);
-    assert.equal(wrapper.find('img').length, 1);
-  });
-
   it('should have an h1 tag containing the text "Featured Project"', () => {
     const wrapper = shallow(<FeaturedProject />);
     const title = wrapper.find('h1');
     assert.strictEqual(title.contains(<h1>Featured Project</h1>), true);
+  });
+
+  it('should have an img tag with src and alt properties', () => {
+    const wrapper = shallow(<FeaturedProject />);
+    const { alt, src } = wrapper.find('img').props();
+    assert.equal(wrapper.find('img').length, 1);
+    assert.ok(alt.length > 1, 'alt attribute is empty');
+    assert.ok(src.length > 1, 'src attribute is empty');
+  });
+
+  it('should have a link to the featured project', () => {
+    const wrapper = shallow(<FeaturedProject />);
+    assert.strictEqual(wrapper.find('Link').length, 1);
   });
 });
