0ecda65f7d0c443bec6eba6e79376b6c93916007
Simone Duca <simoneduca@users.noreply.github.com>
2017-06-06 09:47:06 +0100

    Add featured project tests (#3851)
    * Add feat project tests
    
    * Fix tests and make component a stateless function
    
    * Check for empty strings, not short ones
    
    * Use function declaration syntax
    
    * Fix typo

---
+++ app/pages/home-common/featured-project.spec.js
@@ -0,0 +1,34 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import assert from 'assert';
+import FeaturedProject from './featured-project';
+
+describe('FeaturedProject', () => {
+  it('should render without crashing', () => {
+    shallow(<FeaturedProject />);
+  });
+
+  it('should have a section as container', () => {
+    const wrapper = shallow(<FeaturedProject />);
+    assert.strictEqual(wrapper.type(), 'section');
+  });
+
+  it('should have an h1 tag containing the text "Featured Project"', () => {
+    const wrapper = shallow(<FeaturedProject />);
+    const title = wrapper.find('h1');
+    assert.strictEqual(title.contains(<h1>Featured Project</h1>), true);
+  });
+
+  it('should have an img tag with src and alt properties', () => {
+    const wrapper = shallow(<FeaturedProject />);
+    const { alt, src } = wrapper.find('img').props();
+    assert.equal(wrapper.find('img').length, 1);
+    assert.ok(alt.length > 0, 'alt attribute is empty');
+    assert.ok(src.length > 0, 'src attribute is empty');
+  });
+
+  it('should have a link to the featured project', () => {
+    const wrapper = shallow(<FeaturedProject />);
+    assert.strictEqual(wrapper.find('Link').length, 1);
+  });
+});
