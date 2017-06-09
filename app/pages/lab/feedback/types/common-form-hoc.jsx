eb367122088daae8849a3ab6384d09d24045dc93
Roger Hutchings <roger@hutchin.gs>
2017-06-09 10:38:34 +0100

    Make feedback optional for success or failure, and refactor lab feedback forms to abstract out common code

---
+++ app/pages/lab/feedback/types/common-form-hoc.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import counterpart from 'counterpart';
+import Translate from 'react-translate-component';
+import CommonFormHOCButtons from './common-form-hoc-buttons';
+import FeedbackInput from './feedback-input';
+import merge from 'lodash/merge';
+
+counterpart.registerTranslations('en', {
+  feedbackEditForm: {
+    title: 'Edit Feedback',
+    fields: {
+      id: {
+        title: 'ID',
+        help: 'A unique field used to identify a feedback type.',
+      },
+      successEnabled: {
+        title: 'Enable positive feedback',
+        help: 'Shows a feedback message to the user when they correctly identify a known subject.'
+      },
+      defaultSuccessMessage: {
+        title: 'Default success message',
+        help: 'The message to show to the volunteer when they make a correct classification. Can be overridden using subject metadata.',
+      },
+      failureEnabled: {
+        title: 'Enable negative feedback',
+        help: 'Shows a feedback message to the user when they incorrectly identify a known subject.'
+      },
+      defaultFailureMessage: {
+        title: 'Default failure message',
+        help: 'The message to show to the volunteer when they make an incorrect classification. Can be overridden using subject metadata.',
+      }
+    }
+  }
+});
+
+function returnCommonFormHOC(FeedbackTypeForm) {
+  return class CommonFormHOC extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleSave = this.handleSave.bind(this);
+      this.handleUpdateState = this.handleUpdateState.bind(this);
+      this.renderCommonFields = this.renderCommonFields.bind(this);
+      this.validate = this.validate.bind(this);
+      
+      const { feedback } = props;
+      this.state = {
+        form: {
+          id: feedback.id || '',
+          successEnabled: (typeof feedback.successEnabled === 'boolean') ? feedback.successEnabled : true,
+          defaultSuccessMessage: feedback.defaultSuccessMessage || '',
+          failureEnabled: (typeof feedback.failureEnabled === 'boolean') ? feedback.failureEnabled : true,
+          defaultFailureMessage: feedback.defaultFailureMessage || '',
+        },
+        validations: [
+          // Must have an id
+          (form) => (!!form.id),
+          // Must have either success or failure or both enabled
+          (form) => (!(!form.successEnabled && !form.failureEnabled)),
+          // Success must be disabled, or enabled with a message
+          (form) => (!form.successEnabled || (form.successEnabled && 
+            form.defaultSuccessMessage && form.defaultSuccessMessage.length > 0)),
+          // Failure must be disabled, or enabled with a message
+          (form) => (!form.failureEnabled || (form.failureEnabled && form.defaultFailureMessage && form.defaultFailureMessage.length > 0)),
+        ],
+      }
+    }
+
+    render() {
+      const newProps = {
+        formState: this.state.form,
+        updateState: this.handleUpdateState,
+      };
+
+      return (
+        <div className="edit-feedback-modal">
+          <Translate className="form-label" content="feedbackEditForm.title" />
+          {this.renderCommonFields()}
+          <FeedbackTypeForm {...this.props} {...newProps} />
+          <CommonFormHOCButtons 
+            disableSave={!this.state.form.valid} 
+            handleCancel={this.handleCancel} 
+            handleSave={this.handleSave} 
+          />
+        </div>
+      );
+    }
+
+    handleInputChange({ target }) {
+      this.handleUpdateState({
+        form: {
+          [target.name]: (target.type === 'checkbox') ? target.checked : target.value,
+        }
+      });
+    }
+
+    handleUpdateState(update) {
+      const newState = merge({}, this.state, update);
+      newState.form.valid = this.validate(newState);
+      this.setState(newState);
+    }
+
+    handleSave() {
+      this.props.onSubmit(this.state.form, this.props.index)
+        .catch(error => console.error(error));
+    }
+
+    renderCommonFields() {
+      const commonFields = [
+        { id: 'id', type: 'text' },
+        { id: 'successEnabled', type: 'checkbox' },
+        { id: 'defaultSuccessMessage', type: 'text', disabled: (this.state.form.successEnabled !== true) },
+        { id: 'failureEnabled', type: 'checkbox' },
+        { id: 'defaultFailureMessage', type: 'text', disabled: (this.state.form.failureEnabled !== true) },
+      ];
+
+      return commonFields.map(field => 
+        <FeedbackInput 
+          key={`feedback-input-${field.id}`}
+          field={field}
+          title={counterpart(`feedbackEditForm.fields.${field.id}.title`)}
+          help={counterpart(`feedbackEditForm.fields.${field.id}.help`)}
+          value={this.state.form[field.id]}
+          onChange={this.handleInputChange.bind(this)}
+        />
+      );
+    }
+
+    validate(newState) {
+      return this.state.validations.every(validation => validation(newState.form));
+    }
+  }
+}
+
+export default returnCommonFormHOC;
--- app/pages/lab/feedback/types/point/point-edit-form.jsx
+++ app/pages/lab/feedback/types/point/point-edit-form.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import Select from 'react-select';
-import Translate from 'react-translate-component';
 import counterpart from 'counterpart';
+import CommonFormHOC from '../common-form-hoc';
+import FeedbackInput from '../feedback-input';
 
 counterpart.registerTranslations('en', {
   PointEditForm: {
-    title: 'Edit Feedback - Point',
-    save: 'Save',
-    cancel: 'Cancel',
     fields: {
-      id: {
-        title: 'ID',
-        help: 'A unique field used to identify a feedback type.',
-      },
       dud: {
         title: 'No markings',
         help: 'Check this box if there should be no annotations on this feedback type (also known as a dud).'
@@ -20,15 +13,7 @@ counterpart.registerTranslations('en', {
       tol: {
         title: 'Default tolerance',
         help: 'The radius around the point that counts as a correct classification. Can be overridden using subject metadata.',
-      },
-      defaultSuccessMessage: {
-        title: 'Default success message',
-        help: 'The message to show to the volunteer when they make a correct classification. Can be overridden using subject metadata.',
-      },
-      defaultFailureMessage: {
-        title: 'Default failure message',
-        help: 'The message to show to the volunteer when they make an incorrect classification. Can be overridden using subject metadata.',
-      },
+      }
     }
   }
 });
@@ -37,131 +22,53 @@ class PointEditForm extends React.Component {
   constructor(props) {
     super(props);
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSave = this.handleSave.bind(this);
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-    this.handleUpdateState = this.handleUpdateState.bind(this);
-    this.renderInput = this.renderInput.bind(this);
-    this.validate = this.validate.bind(this);
-    this.state = {
-      id: props.feedback.id || '',
-      dud: props.feedback.dud || false,
-      tol: props.feedback.tol || '',
-      defaultSuccessMessage: props.feedback.defaultSuccessMessage || '',
-      defaultFailureMessage: props.feedback.defaultFailureMessage || '',
-      valid: props.feedback.valid || false
-    };
+  }
+
+  componentWillMount() {
+    this.props.updateState({
+      form: {
+        dud: (typeof this.props.feedback.dud === 'boolean') ? this.props.feedback.dud : false,
+        tol: this.props.feedback.tol || ''
+      },
+      validations: [
+        (form) => (form.dud || (!form.dud && form.tol && form.tol !== ''))
+      ]
+    });
   }
 
   render() {
-    const fields = this.getFields()
-    return (
-      <div className="single-edit-feedback-modal">
-        <Translate content="PointEditForm.title" className="form-label" />
-        {fields.map(this.renderInput)}
-
-        <div className="single-edit-feedback-modal__buttons">
-          <button onClick={this.handleCancel} className="minor-button">
-            <Translate content="PointEditForm.cancel" />
-          </button>
-          <button onClick={this.handleSave} disabled={!this.state.valid} className="major-button">
-            <Translate content="PointEditForm.save" />
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  getFields() {
     const fields = [
-      { id: 'id' },
       { id: 'dud', type: 'checkbox' },
-      { id: 'defaultSuccessMessage' },
-      { id: 'defaultFailureMessage' }
+      { id: 'tol', type: 'number', disabled: this.props.formState.dud }
     ];
 
-    if (this.state.dud === false) {
-      fields.splice(2, 0, { id: 'tol', type: 'number' });
-    }
-
-    return fields;    
+    return (
+      <div>
+        {fields.map(field => 
+          <FeedbackInput 
+            key={`feedback-input-${field.id}`}
+            field={field}
+            title={counterpart(`PointEditForm.fields.${field.id}.title`)}
+            help={counterpart(`PointEditForm.fields.${field.id}.help`)}
+            value={this.props.formState[field.id]}
+            onChange={this.handleInputChange.bind(this)}
+          />
+        )}
+      </div>
+    );
   }
 
   handleInputChange({ target }) {
-    const args = [target.name, target.value];
-    if (target.type === 'checkbox') {
-      args.splice(1, 1, target.checked);
-    }
-    this.handleUpdateState.apply(this, args);
-  }
-
-  handleSave() {
-    const stateCopy = Object.assign({}, this.state);
-    if (stateCopy.dud && stateCopy.tol) {
-      delete stateCopy.tol;
-    }
-    this.props.onSubmit(stateCopy, this.props.index);
-  }
-
-  handleSelectChange(option) {
-    this.handleUpdateState('answerIndex', option.value);
-  };
-
-  handleUpdateState(key, value) {
-    const newFeedback = Object.assign({}, this.state);
-    newFeedback[key] = value;
-    newFeedback.valid = this.validate(newFeedback);
-    this.setState(newFeedback);
-  }
-
-  renderInput(field) {
-    const inputProps = {
-      type: field.type || 'text',
-      name: field.id,
-      onChange: this.handleInputChange.bind(this),
-    };
-
-    if (['text', 'number'].includes(inputProps.type)) {
-      inputProps.className = 'standard-input full';
-      inputProps.value = this.state[field.id];
-    }
-
-    if (field.type === 'checkbox') {
-      inputProps.checked = this.state[field.id];
-    }
-
-    return (
-      <div key={field.id}>
-        <label>
-          <div>
-            <Translate content={`PointEditForm.fields.${field.id}.title`} />
-          </div>
-          <small className="form-help">
-            <Translate content={`PointEditForm.fields.${field.id}.help`} />
-          </small>
-          <input {...inputProps} />
-        </label>
-      </div>
-    );
-  }
-
-  validate({ id, defaultSuccessMessage, defaultFailureMessage, dud, tol }) {
-    return [
-      (id.length > 0),
-      (defaultSuccessMessage.length > 0),
-      (defaultFailureMessage.length > 0),
-      (dud || (!dud && tol.length > 0))
-    ].every(validation => validation);
+    this.props.updateState({
+      form: {
+        [target.name]: (target.type === 'checkbox') ? target.checked : target.value,
+      }
+    });
   }
 }
 
 PointEditForm.propTypes = {
-  task: React.PropTypes.shape({
-    answers: React.PropTypes.arrayOf(React.PropTypes.shape({
-      option: React.PropTypes.string,
-    })),
-  }),
-  index: React.PropTypes.number,
-  onSubmit: React.PropTypes.func,
+
 };
 
-export default PointEditForm;
+export default CommonFormHOC(PointEditForm);
