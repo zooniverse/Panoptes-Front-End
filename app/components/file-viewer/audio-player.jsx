38adf7697710cac6271d65a0aa3e7be73abe180d
Hugh Dickinson <hughdickinson@users.noreply.github.com>
2017-06-15 16:57:13 -0500

    Audio subject (#3863)
    * Implemented audio subjects for Admin-enabled users.
    
    * Cleaned up some stray commented code.
    
    * Cleanup and modifications in repsonse to PR review
    
    Mainly changes related to file upload validation. Some cleanup of stray comments and removal of extra control rendering boolean switch

---
+++ app/components/file-viewer/audio-player.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+class AudioPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.player = null;
+
+    this.playAudio = this.playAudio.bind(this);
+
+    this.state = {
+      playing: false
+    };
+  }
+
+  componentDidMount() {
+    this.player.controlsList = "nodownload"; // Non-spec option for Chrome browsers to hide the display of a download button
+  }
+
+  componentDidUpdate() {
+  }
+
+  playAudio(playing) {
+    const { player } = this;
+    if (!player) return;
+
+    this.setState({ playing });
+
+    if (playing) {
+      player.play();
+    } else {
+      player.pause();
+    }
+  }
+
+  endAudio() {
+    this.setState({ playing: false });
+  }
+
+  render() {
+    return (
+      <div className="subject-audio-frame">
+        <audio
+          className="subject"
+          controls={true}
+          ref={(element) => { this.player = element; }}
+          src={this.props.src}
+          type={`${this.props.type}/${this.props.format}`}
+          preload="auto"
+          onCanPlay={this.props.onLoad}
+          onClick={this.playAudio.bind(this, !this.state.playing)}
+          onEnded={this.endAudio}
+        >
+          Your browser does not support the audio format. Please upgrade your browser.
+        </audio>
+
+        {this.props.children}
+      </div>
+    );
+  }
+
+}
+
+AudioPlayer.propTypes = {
+  children: React.PropTypes.node,
+  format: React.PropTypes.string,
+  frame: React.PropTypes.number,
+  onLoad: React.PropTypes.func,
+  showControls: React.PropTypes.bool,
+  src: React.PropTypes.string,
+  type: React.PropTypes.string
+};
+
+AudioPlayer.defaultProps = {
+};
+
+export default AudioPlayer;
