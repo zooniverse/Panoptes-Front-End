858e461da7f8469884429680a3168ba7385091ab
Will Granger <twgranger@gmail.com>
2017-06-19 06:45:04 -0500

    Minor Landing Page Changes (#3870)
    * Adjust Cards and Hover State
    
    * Give Key to Div
    
    * Add Focus to Links

--- app/pages/home-not-logged-in/promoted.jsx
+++ app/pages/home-not-logged-in/promoted.jsx
@@ -30,13 +30,14 @@ const HomePagePromoted = ({ promotedProjects }) => {
       <div className="home-promoted__cards">
         {promotedProjects.map((project) => {
           return (
-            <ProjectCard
-              key={project.id}
-              className="home-page-not-logged-in__project-card"
-              imageSrc={project.image}
-              landingPage={true}
-              project={project}
-            />);
+            <div key={project.id}>
+              <ProjectCard
+                className="home-page-not-logged-in__project-card"
+                imageSrc={project.image}
+                landingPage={true}
+                project={project}
+              />
+            </div>);
         })}
       </div>
 
