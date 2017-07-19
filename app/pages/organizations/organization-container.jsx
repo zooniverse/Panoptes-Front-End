12e07a9e59815c7476875790c8b16dc62f099136
mcbouslog <mcbouslog@gmail.com>
2017-07-19 16:16:45 -0500

    improve loading and error handling

--- app/pages/organizations/organization-container.jsx
+++ app/pages/organizations/organization-container.jsx
@@ -6,6 +6,7 @@ class OrganizationContainer extends React.Component {
     super(props);
 
     this.state = {
+      error: null,
       fetching: false,
       organization: null,
       organizationAvatar: {},
@@ -37,10 +38,13 @@ class OrganizationContainer extends React.Component {
       return;
     }
 
-    this.setState({ fetching: true });
+    this.setState({ fetching: true, error: null });
 
     apiClient.type('organizations').get(id.toString(), { include: ['avatar', 'background'] })
-      .catch(error => console.error('error loading organization', error)) // eslint-disable-line no-console
+      .catch((error) => {
+        console.error('error loading organization', error); // eslint-disable-line no-console
+        this.setState({ fetching: false, error });
+      })
       .then((organization) => {
         organization.projects = []; // eslint-disable-line no-param-reassign
         this.setState({ organization });
@@ -68,29 +72,47 @@ class OrganizationContainer extends React.Component {
   }
 
   fetchProjects(organization) {
-    organization.get('projects', { cards: true, include: ['avatar'] }).then((projects) => {
-      const org = this.state.organization;
-      org.projects = projects;
-      this.setState({ organization: org, fetching: false });
-    });
+    organization.get('projects', { cards: true, include: ['avatar'] })
+      .catch((error) => {
+        console.error('error loading projects', error); // eslint-disable-line no-console
+        this.setState({ fetching: false });
+      })
+      .then((projects) => {
+        const org = this.state.organization;
+        org.projects = projects;
+        this.setState({ organization: org, fetching: false });
+      });
   }
 
   render() {
-    if (!this.state.organization) {
+    if (this.state.organization) {
+      return React.Children.map(this.props.children, child =>
+        React.cloneElement(child, {
+          organization: this.state.organization,
+          organizationAvatar: this.state.organizationAvatar,
+          organizationBackground: this.state.organizationBackground
+        })
+      )[0];
+    } else if (this.state.fetching) {
       return (
-        <div>
-          <p>Loading...</p>
-        </div>
-      );
+        <div className="content-container">
+          <p>
+            Loading{' '}
+            <strong>Organization #{this.props.params.id}</strong>...
+          </p>
+        </div>);
+    } else {
+      return (
+        <div className="content-container">
+          <p>
+            There was an error retrieving organization <strong>#{this.props.params.id}</strong>.
+          </p>
+          {this.state.error &&
+            <p>
+              <code>{this.state.error.toString()}</code>
+            </p>}
+        </div>);
     }
-
-    return React.Children.map(this.props.children, child =>
-      React.cloneElement(child, {
-        organization: this.state.organization,
-        organizationAvatar: this.state.organizationAvatar,
-        organizationBackground: this.state.organizationBackground
-      })
-    )[0];
   }
 
 }
