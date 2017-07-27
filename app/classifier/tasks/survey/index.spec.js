4e37616d7e5f721b44cc4687429f5c46c3bede79
Jim O'Donnell <jim@zooniverse.org>
2017-07-27 10:55:21 +0100

    Test selecting a choice

--- app/classifier/tasks/survey/index.spec.js
+++ app/classifier/tasks/survey/index.spec.js
@@ -2,6 +2,7 @@ import { mount } from 'enzyme';
 import React from 'react';
 import assert from 'assert';
 import SurveyTask from './';
+import Choice from './choice';
 import { workflow } from '../../../pages/dev-classifier/mock-data';
 
 describe('Survey Task', function(){
@@ -17,22 +18,30 @@ describe('Survey Task', function(){
     assert.equal(wrapper.prop('task'), task);
   });
 
+  annotation = { 
+    value: [{
+      "choice": "ar",
+      "answers": {
+        "ho": "two",
+        "be": [
+          "ea"
+        ]
+      },
+      "filters": {}
+    }]
+  };
+
   it(`should render a valid annotation as a selected choice`, function(){
-    let annotation = { 
-      value: [{
-        "choice": "ar",
-        "answers": {
-          "ho": "two",
-          "be": [
-            "ea"
-          ]
-        },
-        "filters": {}
-      }]
-    };
     wrapper.setProps({ annotation });
     wrapper.update();
     const selectedChoice = wrapper.find('[data-choiceID="ar"]');
     assert.equal(selectedChoice.prop('className'), 'survey-task-chooser-choice-button survey-task-chooser-choice-button-chosen');
+  });
+
+  it('should render the Choice component when a choice is selected', function(){
+    wrapper.setState({ selectedChoiceID: 'ar' });
+    const choice = wrapper.find(Choice);
+    assert.equal(choice.props().choiceID, 'ar');
+    assert.equal(choice.props().annotationValue, annotation.value[0]);
   });
 })
