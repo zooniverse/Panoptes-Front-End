7d60348abc2f69477d798d31fe301b5a515e96ba
Jim O'Donnell <jim@zooniverse.org>
2017-07-27 13:44:13 +0100

    Add a describe block to make tests clearer

--- app/classifier/tasks/survey/index.spec.js
+++ app/classifier/tasks/survey/index.spec.js
@@ -6,7 +6,7 @@ import Choice from './choice';
 import { workflow } from '../../../pages/dev-classifier/mock-data';
 
 describe('Survey Task', function(){
-  let annotation = { value: [] };
+  const annotation = { value: [] };
   const task = workflow.tasks.survey
   let wrapper;
 
@@ -18,53 +18,55 @@ describe('Survey Task', function(){
     assert.equal(wrapper.prop('task'), task);
   });
 
-  annotation = { 
-    value: [{
-      "choice": "ar",
-      "answers": {
-        "ho": "two",
-        "be": [
-          "ea"
-        ]
-      },
-      "filters": {}
-    }]
-  };
-
-  it(`should render a valid annotation as a selected choice`, function(){
-    wrapper.setProps({ annotation });
-    wrapper.update();
-    const selectedChoice = wrapper.find('[data-choiceID="ar"]');
-    assert.equal(selectedChoice.prop('className'), 'survey-task-chooser-choice-button survey-task-chooser-choice-button-chosen');
-  });
-
   it('should render the Choice component when a choice is selected', function(){
     wrapper.setState({ selectedChoiceID: 'ar' });
     const choice = wrapper.find(Choice);
     assert.equal(choice.props().choiceID, 'ar');
   });
-
-  it('should pass existing answers to the Choice component', function(){
-    wrapper.setProps({ annotation });
-    wrapper.update();
-    wrapper.setState({ selectedChoiceID: 'ar' });
-    const choice = wrapper.find(Choice);
-    assert.equal(choice.props().annotationValue, annotation.value[0]);
-  });
   
-  it('should reset saved choices when the annotation resets', function(){
-    wrapper.setProps({ annotation });
-    wrapper.update();
-    wrapper.setState({ selectedChoiceID: 'ar' });
-    let choice = wrapper.find(Choice);
-    assert.equal(choice.props().annotationValue.answers.ho, "two");
-    assert.equal(choice.node.state.answers.ho, "two")
-    wrapper.setState({ selectedChoiceID: '' });
-    wrapper.setProps({ annotation: { value: [] } });
-    wrapper.update();
-    wrapper.setState({ selectedChoiceID: 'ar' });
-    choice = wrapper.find(Choice);
-    assert.equal(choice.props().annotationValue.answers.ho, undefined);
-    assert.equal(choice.node.state.answers.ho, undefined)
+  describe('with an existing annotation', function(){
+    const annotation = { 
+      value: [{
+        "choice": "ar",
+        "answers": {
+          "ho": "two",
+          "be": [
+            "ea"
+          ]
+        },
+        "filters": {}
+      }]
+    };
+    const task = workflow.tasks.survey
+    let wrapper;
+
+    beforeEach(function(){
+      wrapper = mount(<SurveyTask task={task} annotation={annotation} />);
+    });
+
+    it(`should render a valid annotation as a selected choice`, function(){
+      const selectedChoice = wrapper.find('[data-choiceID="ar"]');
+      assert.equal(selectedChoice.prop('className'), 'survey-task-chooser-choice-button survey-task-chooser-choice-button-chosen');
+    });
+
+    it('should pass existing answers to the Choice component', function(){
+      wrapper.setState({ selectedChoiceID: 'ar' });
+      const choice = wrapper.find(Choice);
+      assert.equal(choice.props().annotationValue, annotation.value[0]);
+    });
+  
+    it('should reset saved choices when the annotation resets', function(){
+      wrapper.setState({ selectedChoiceID: 'ar' });
+      let choice = wrapper.find(Choice);
+      assert.equal(choice.props().annotationValue.answers.ho, "two");
+      assert.equal(choice.node.state.answers.ho, "two")
+      wrapper.setState({ selectedChoiceID: '' });
+      wrapper.setProps({ annotation: { value: [] } });
+      wrapper.update();
+      wrapper.setState({ selectedChoiceID: 'ar' });
+      choice = wrapper.find(Choice);
+      assert.equal(choice.props().annotationValue.answers.ho, undefined);
+      assert.equal(choice.node.state.answers.ho, undefined)
+    });
   });
 })
