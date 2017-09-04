a81bc92c47018a4f2090d3e1b5d06cb0a54ffcfa
Jim O'Donnell <jim@zooniverse.org>
2017-09-04 16:21:47 +0100

    Default translation strings to an empty object

--- app/pages/project/translations.js
+++ app/pages/project/translations.js
@@ -3,7 +3,7 @@ import apiClient from 'panoptes-client/lib/api-client';
 const translations = {
   strings: {},
   load: (translated_type, translated_id, language) => {
-    delete translations.strings[translated_type];
+    translations.strings[translated_type] = {};
     return apiClient
       .type('translations')
       .get({ translated_type, translated_id, language })
