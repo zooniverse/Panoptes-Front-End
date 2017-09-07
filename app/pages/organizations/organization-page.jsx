f657427d7d4051237b32bc58795ca2015a2a30a1
mcbouslog <mcbouslog@gmail.com>
2017-09-07 15:27:26 -0500

    refactor styling

--- app/pages/organizations/organization-page.jsx
+++ app/pages/organizations/organization-page.jsx
@@ -45,90 +45,40 @@ OrganizationProjectCard.propTypes = {
   })
 };
 
-class OrganizationPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      backgroundHeight: null
-    };
-
-    this.resizeBackground = this.resizeBackground.bind(this);
-  }
-
-  componentDidMount() {
-    this.resizeBackground();
-    addEventListener('resize', this.resizeBackground);
-  }
-
-  componentWillUnmount() {
-    removeEventListener('resize', this.resizeBackground);
-  }
-
-  resizeBackground() {
-    const orgHero = this.organizationHero;
-    if (orgHero) {
-      const sectionBottom = orgHero.getBoundingClientRect().bottom;
-      const sectionHeight = document.body.scrollTop + sectionBottom;
-      if (this.state.backgroundHeight !== sectionHeight) {
-        this.setState({ backgroundHeight: sectionHeight });
-      }
-    }
-  }
-
-  render() {
-    let backgroundStyle = {};
-    if (this.props.organizationBackground) {
-      backgroundStyle = {
-        backgroundImage: `url(${this.props.organizationBackground.src})`,
-        height: this.state.backgroundHeight
-      };
-    }
-
-    return (
-      <div className="organization-page">
-        <div
-          className="project-background"
-          style={backgroundStyle}
-        />
-        <div className="project-home-page">
-          <section
-            className="organization-hero"
-            ref={(node) => { this.organizationHero = node; }}
-          >
-            <div className="organization-hero__container">
-              {this.props.organizationAvatar &&
-                <Thumbnail
-                  src={this.props.organizationAvatar.src}
-                  className="avatar organization-hero__avatar"
-                  width={AVATAR_SIZE}
-                  height={AVATAR_SIZE}
-                />}
-              <div>
-                <h1 className="organization-hero__title">{this.props.organization.display_name}</h1>
-                <h5 className="organization-hero__description">{this.props.organization.description}</h5>
-              </div>
-            </div>
-          </section>
-          <section className="resources-container">
-            <div className="organization-projects">
-              <div className="project-card-list">
-                {this.props.organization.projects.map(project =>
-                  <OrganizationProjectCard
-                    collaboratorView={this.props.collaboratorView}
-                    key={project.id}
-                    project={project}
-                  />)}
-              </div>
-            </div>
-          </section>
-          <section className="organization-details">
-            <p>Details like description, stats and about info will go here, omitting to keep PR compact.</p>
-          </section>
+const OrganizationPage = ({ collaboratorView, organization, organizationAvatar, organizationBackground }) =>
+  <div className="organization-page">
+    <section
+      className="organization-hero"
+      style={{ backgroundImage: `url(${organizationBackground.src})` }}
+    >
+      <div className="organization-hero__container">
+        {organizationAvatar &&
+          <Thumbnail
+            src={organizationAvatar.src}
+            className="avatar organization-hero__avatar"
+            width={AVATAR_SIZE}
+            height={AVATAR_SIZE}
+          />}
+        <div>
+          <h1 className="organization-hero__title">{organization.display_name}</h1>
+          <h5 className="organization-hero__description">{organization.description}</h5>
         </div>
-      </div>);
-  }
-}
+      </div>
+    </section>
+    <section className="resources-container">
+      <div className="project-card-list">
+        {organization.projects.map(project =>
+          <OrganizationProjectCard
+            collaboratorView={collaboratorView}
+            key={project.id}
+            project={project}
+          />)}
+      </div>
+    </section>
+    <section className="organization-details">
+      <p>Details like description, stats and about info will go here, omitting to keep PR compact.</p>
+    </section>
+  </div>;
 
 OrganizationPage.defaultProps = {
   collaboratorView: false,
