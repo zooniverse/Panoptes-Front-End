83d41cc1102d9ebb443c8ee7239c09dc868fab7e
jelliotartz <jelliotartz@gmail.com>
2017-09-21 12:54:35 -0500

    WIP: unit tests for HomePageRoot.

--- app/pages/home.spec.js
+++ app/pages/home.spec.js
@@ -5,56 +5,25 @@ import HomePageRoot from './home';
 
 const user = {
   display_name: 'Test User'
-}
+};
 
 const location = {
   hash: '#page'
-}
+};
 
-describe('HomePageRoot', function () {
-  describe('renders a homepage', function() {
-    let wrapper;
+describe('HomePageRoot', () => {
+  let wrapper;
 
-    beforeEach(function () {
-      wrapper = shallow(<HomePageRoot user={user} location={location} />)
-    });
+  beforeEach(() => {
+    wrapper = shallow(<HomePageRoot />);
+  });
 
-    it('without crashing', function () {
-      assert.equal(wrapper, wrapper)
-    });
+  it('renders <HomePageNotLoggedIn /> when there isn\'t a user', () => {
+    assert.equal(wrapper.find('HomePageNotLoggedIn').root.length, 1);
+  });
 
-    it('with a user prop', function() {
-      const userDisplayName = wrapper.nodes[0].props.user.display_name
-      assert.equal(userDisplayName, 'Test User')
-    });
-
-    it('with a location prop', function() {
-      const location = wrapper.nodes[0].props.location.hash
-      assert.equal(location, '#page')
-    })
-  })
-
-  describe('renders a homepage', function() {
-    let wrapper;
-
-    beforeEach(function () {
-      wrapper = shallow(<HomePageRoot />)
-    })
-
-    it('without crashing', function () {
-      assert.equal(wrapper, wrapper)
-    });
-
-    it('for a not authenticated user', function() {
-      const child = wrapper.children().root.length
-      assert.equal(child, 1)
-    })
-
-    it('without a user prop', function() {
-      const user = wrapper.nodes[0].props.user
-      assert.equal(user, undefined)
-    });
-
-  })
-
+  it('renders <HomePageLoggedIn /> when there is a user', () => {
+    wrapper.setProps({ user, location });
+    assert.equal(wrapper.find('HomePageLoggedIn').first().length, 1);
+  });
 });
