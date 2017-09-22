2849f895b293bd7ba31eaf70d395e98b9198d168
jelliotartz <jelliotartz@gmail.com>
2017-09-22 13:11:14 -0500

    Add unit test for PrivacyPolicy.

--- app/pages/privacy-policy.spec.js
+++ app/pages/privacy-policy.spec.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import assert from 'assert';
 import { shallow } from 'enzyme';
-import PrivacyPage from './privacy-policy';
+import PrivacyPolicy from './privacy-policy';
 
+describe('PrivacyPolicy', () => {
+  it('should render without crashing', () => {
+    const wrapper = shallow(<PrivacyPolicy />);
+    assert.equal(wrapper.find('div.content-container').length, 1);
+  });
+});
