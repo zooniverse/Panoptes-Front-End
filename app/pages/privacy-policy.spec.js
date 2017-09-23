34844409f1f46997f5f92e200fd9872b4b43f68f
jelliotartz <jelliotartz@gmail.com>
2017-09-23 15:54:18 -0500

    Add tests for heading and content.

--- app/pages/privacy-policy.spec.js
+++ app/pages/privacy-policy.spec.js
@@ -4,8 +4,98 @@ import { shallow } from 'enzyme';
 import PrivacyPolicy from './privacy-policy';
 
 describe('PrivacyPolicy', () => {
-  it('should render without crashing', () => {
-    const wrapper = shallow(<PrivacyPolicy />);
-    assert.equal(wrapper.find('div.content-container').length, 1);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<PrivacyPolicy />);
+  });
+
+  it('renders without crashing', () => {
+    const privacyPolicyContainer = wrapper.find('div.content-container');
+    assert.equal(privacyPolicyContainer.length, 1);
+  });
+
+  describe('heading', () => {
+    it('renders the page title', () => {
+      const headingElement = wrapper
+        .find('div.content-container').children()
+        .first()
+        .props().component;
+      assert.equal(headingElement, 'h1');
+    });
+
+    it('renders page title content', () => {
+      const headingContent = wrapper
+        .find('div.content-container').children()
+        .first()
+        .props().content;
+      assert.equal(headingContent, 'privacy.title');
+    });
+  });
+
+  describe('user agreement section', () => {
+    let markdownElements;
+
+    beforeEach(() => {
+      markdownElements = wrapper
+        .find('div.content-container').children()
+        .find('div.columns-container').children()
+        .find('div.column')
+        .first()
+        .props().children;
+    });
+
+    it('renders four markdown elements', () => {
+      assert.equal(markdownElements.length, 4);
+    });
+
+    it('renders each of its sections', () => {
+      const sections = [
+        'userAgreementSummary',
+        'userAgreementContribution',
+        'userAgreementData',
+        'userAgreementLegal'
+      ];
+
+      markdownElements.forEach((element, index) => {
+        assert.ok(element.props.children.match(sections[index]));
+      });
+    });
+  });
+
+  describe('privacy policy section', () => {
+    let markdownElements;
+
+    beforeEach(() => {
+      markdownElements = wrapper
+        .find('div.content-container').children()
+        .find('div.columns-container').children()
+        .find('div.column')
+        .last()
+        .props().children;
+    });
+
+    it('renders ten markdown elements', () => {
+      assert.equal(markdownElements.length, 10);
+    });
+
+    it('renders each of its sections', () => {
+      const sections = [
+        'privacyPolicyIntro',
+        'privacyPolicyData',
+        'privacyPolicyInfo',
+        'privacyPolicyThirdParties',
+        'privacyPolicyCookies',
+        'privacyPolicyDataStorage',
+        'privacyPolicySecurity',
+        'privacyPolicyDataRemoval',
+        'privacyPolicyContactUser',
+        'privacyPolicyFurtherInfo'
+      ];
+
+      markdownElements.forEach((element, index) => {
+        assert.ok(element.props.children.match(sections[index]));
+      });
+    });
   });
 });
