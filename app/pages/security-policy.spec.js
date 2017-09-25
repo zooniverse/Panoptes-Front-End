6e81a672de23a31a1d4993429ef6ab32d7860f04
jelliotartz <jelliotartz@gmail.com>
2017-09-25 11:33:43 -0500

    Add tests for heading and content.

--- app/pages/security-policy.spec.js
+++ app/pages/security-policy.spec.js
@@ -1,11 +1,79 @@
 import React from 'react';
 import assert from 'assert';
 import { shallow } from 'enzyme';
-import SecurityPolicy from './privacy-policy';
+import SecurityPolicy from './security-policy';
 
 describe('SecurityPolicy', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<SecurityPolicy />);
+  });
+
   it('should render without crashing', () => {
-    const wrapper = shallow(<SecurityPolicy />);
-    assert.equal(wrapper.find('div.content-container').length, 1);
+    const securityPolicyContainer = wrapper.find('div.content-container');
+    assert.equal(securityPolicyContainer.length, 1);
+  });
+
+  describe('heading', () => {
+    it('renders the page title', () => {
+      const headingElement = wrapper
+        .find('div.content-container').children()
+        .first()
+        .props().component;
+      assert.equal(headingElement, 'h1');
+    });
+
+    it('renders page title content', () => {
+      const headingContent = wrapper
+        .find('div.content-container').children()
+        .first()
+        .props().content;
+      assert.equal(headingContent, 'security.title');
+    });
+  });
+
+  describe('intro section', () => {
+    it('renders one markdown element', () => {
+      const markdownElement = wrapper
+        .find('div.content-container').children()
+        .find('div.columns-container').children()
+        .find('div.column')
+        .first();
+      assert.equal(markdownElement.length, 1);
+    });
+
+    it('renders the intro section', () => {
+      const section = 'intro';
+      const markdownElement = wrapper
+        .find('div.content-container').children()
+        .find('div.columns-container').children()
+        .find('div.column')
+        .first()
+        .props().children.props.children;
+      assert.ok(markdownElement.match(section));
+    });
+  });
+
+  describe('details section', () => {
+    it('renders one markdown element', () => {
+      const markdownElement = wrapper
+        .find('div.content-container').children()
+        .find('div.columns-container').children()
+        .find('div.column')
+        .last();
+      assert.equal(markdownElement.length, 1);
+    });
+
+    it('renders the details section', () => {
+      const section = 'details';
+      const markdownElement = wrapper
+        .find('div.content-container').children()
+        .find('div.columns-container').children()
+        .find('div.column')
+        .last()
+        .props().children.props.children;
+      assert.ok(markdownElement.match(section));
+    });
   });
 });
