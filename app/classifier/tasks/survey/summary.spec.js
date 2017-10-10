896d958a05146762ea6f0707df772b5812253c42
Simone Duca <simoneduca@users.noreply.github.com>
2017-10-10 16:58:34 +0100

    Expand survey task summary tests (#4087)
    Add test cases for zero identifications and two identifications.

--- app/classifier/tasks/survey/summary.spec.js
+++ app/classifier/tasks/survey/summary.spec.js
@@ -7,8 +7,9 @@ import { workflow } from '../../../pages/dev-classifier/mock-data';
 import enLocale from '../../../locales/en';
 
 counterpart.registerTranslations('en', enLocale);
+const noIdentification = { value: [] };
 
-const annotation = { 
+const oneIdentification = {
   value: [{
     "choice": "ar",
     "answers": {
@@ -21,20 +22,84 @@ const annotation = {
     "filters": {}
   }]
 };
-const task = workflow.tasks.survey
-const expectedSummary = 'Armadillo: 2; Moving, Eating';
 
-describe('Survey task summary, not expanded', function() {
-  const wrapper = mount(<Summary annotation={annotation} task={task} />);
+const twoIdentifications = {
+  value: [{
+    "choice": "ar",
+    "answers": {
+      "ho": "one",
+      "be": [
+        "mo"
+      ]
+    },
+    "filters": {}
+    }, {
+    "choice": "to",
+    "answers": {
+      "ho": "two",
+      "be": [
+        "ea"
+      ],
+      "in": "n",
+      "bt": "Y"
+    },
+    "filters": {}
+  }]
+};
+
+const task = workflow.tasks.survey;
+const oneExpectedSummary = 'Armadillo: 2; Moving, Eating';
+const twoExpectedSummary = ['Armadillo: 1; Moving', 'Tortoise: 2; Eating; Nope; HECK YES'];
+
+describe('Survey task summary, no identifications, not expanded', function() {
+  const wrapper = mount(<Summary annotation={noIdentification} task={task} />);
   const question = wrapper.find('.question span').first();
   const answers = wrapper.find('.answers .answer');
 
-  it('should summarise the survey task', function(){
+  it('should summarise the survey task', function() {
     const count = task.choicesOrder.length;
     assert.equal(`Survey of ${count}`, question.text());
   });
 
-  it('should show one answer node', function(){
+  it('should show one answer node', function() {
+    assert.equal(answers.length, 1);
+  });
+
+  it('should summarise the number of identifications', function() {
+    assert.equal(answers.text(), 'No identifications');
+  });
+});
+
+describe('Survey task summary, no identifications, expanded', function() {
+  const wrapper = mount(<Summary annotation={noIdentification} task={task} />);
+  const question = wrapper.find('.question span').first();
+  const answers = wrapper.find('.answers .answer');
+
+  it('should summarise the survey task', function() {
+    const count = task.choicesOrder.length;
+    assert.equal(`Survey of ${count}`, question.text());
+  });
+
+  it('should show one answer node', function() {
+    assert.equal(answers.length, 1);
+  });
+
+  it('should summarise the number of identifications', function() {
+    assert.equal(answers.text(), 'No identifications');
+  });
+});
+
+describe('Survey task summary, one identification, not expanded', function() {
+  const wrapper = mount(<Summary annotation={oneIdentification} task={task} />);
+  const question = wrapper.find('.question span').first();
+  const answers = wrapper.find('.answers .answer');
+
+  it('should summarise the survey task', function() {
+    const count = task.choicesOrder.length;
+    assert.equal(`Survey of ${count}`, question.text());
+  });
+
+  it('should show one answer node', function() {
     assert.equal(answers.length, 1);
   });
 
@@ -43,21 +108,61 @@ describe('Survey task summary, not expanded', function() {
   });
 });
 
-describe('Survey task summary, expanded', function() {
-  const wrapper = mount(<Summary annotation={annotation} task={task} expanded={true} />);
+describe('Survey task summary, one identification, expanded', function() {
+  const wrapper = mount(<Summary annotation={oneIdentification} task={task} expanded={true} />);
   const question = wrapper.find('.question span').first();
   const answers = wrapper.find('.answers .answer');
 
-  it('should summarise the survey task', function(){
+  it('should summarise the survey task', function() {
     const count = task.choicesOrder.length;
     assert.equal(`Survey of ${count}`, question.text());
   });
 
-  it('should show two answer nodes', function(){
+  it('should show two answer nodes', function() {
     assert.equal(answers.length, 2);
   });
-  
+
   it('should summarise the identification and questions', function() {
-    assert.equal(answers.last().text(), expectedSummary);
+    assert.equal(answers.last().text(), oneExpectedSummary);
   });
-})
+});
+
+describe('Survey task summary, two identifications, not expanded', function() {
+  const wrapper = mount(<Summary annotation={twoIdentifications} task={task} />);
+  const question = wrapper.find('.question span').first();
+  const answers = wrapper.find('.answers .answer');
+
+  it('should summarise the survey task', function() {
+    const count = task.choicesOrder.length;
+    assert.equal(`Survey of ${count}`, question.text());
+  });
+
+  it('should show one answer node', function() {
+    assert.equal(answers.length, 1);
+  });
+
+  it('should summarise the number of identifications', function() {
+    assert.equal(answers.text(), '2 identifications');
+  });
+});
+
+describe('Survey task summary, two identifications, expanded', function() {
+  const wrapper = mount(<Summary annotation={twoIdentifications} task={task} expanded={true} />);
+  const question = wrapper.find('.question span').first();
+  const answers = wrapper.find('.answers .answer');
+
+  it('should summarise the survey task', function() {
+    const count = task.choicesOrder.length;
+    assert.equal(`Survey of ${count}`, question.text());
+  });
+
+  it('should show three answer nodes', function() {
+    assert.equal(answers.length, 3);
+  });
+
+  it('should summarise the identification and questions', function() {
+    assert.equal(answers.at(0).text(), '2 identifications');
+    assert.equal(answers.at(1).text(), twoExpectedSummary[0]);
+    assert.equal(answers.at(2).text(), twoExpectedSummary[1]);
+  });
+});
