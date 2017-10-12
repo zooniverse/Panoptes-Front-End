30e6094b93dead222fc1d4f7fa6bc7df4445e2cc
jelliotartz <jelliotartz@gmail.com>
2017-10-12 17:43:14 -0500

    Refactor WorkflowToggle to stateless functional component. - Handle toggle event in parent component (ProjectStatus).

--- app/components/workflow-toggle.spec.js
+++ app/components/workflow-toggle.spec.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import assert from 'assert';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
 import WorkflowToggle from './workflow-toggle';
-
-const project = {
-  display_name: 'test'
-};
 
 const workflow = {
   display_name: 'test'
@@ -14,12 +9,9 @@ const workflow = {
 
 describe('WorkflowToggle', () => {
   let wrapper;
-  let handleToggleActiveStub;
 
   before(() => {
-    handleToggleActiveStub = sinon.stub(WorkflowToggle.prototype, 'handleToggleActive');
-    wrapper = shallow(<WorkflowToggle project={project} workflow={workflow} field="active" />);
-    handleToggleActiveStub.restore();
+    wrapper = shallow(<WorkflowToggle workflow={workflow} />);
   });
 
   it('renders without crashing', () => {
@@ -29,11 +21,5 @@ describe('WorkflowToggle', () => {
 
   it('renders a checkbox input', () => {
     assert.equal(wrapper.find('input[type="checkbox"]').length, 1);
-  });
-
-  it('calls this.handleToggleActive() when checkbox is clicked', () => {
-    const toggleActiveInput = wrapper.find('input');
-    toggleActiveInput.simulate('change', { target: { checked: false }});
-    sinon.assert.calledOnce(handleToggleActiveStub);
   });
 });
