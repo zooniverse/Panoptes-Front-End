b1f82a156b0be455f9f67f85df0939752b54f171
jelliotartz <jelliotartz@gmail.com>
2017-10-17 13:38:32 -0500

    Cleanup linter errors.

--- app/components/workflow-default-dialog.jsx
+++ app/components/workflow-default-dialog.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Translate from 'react-translate-component';
 
-const WorkflowDefaultDialog = ({ onSuccess }) => {
+const WorkflowDefaultDialog = ({ onSuccess }) => (
+  <div>
+    <Translate content="workflowDefaultDialog.text" />
+    <br />
+    <button type="submit" onSubmit={onSuccess}>ok</button>
+  </div>
+  );
 
-  return (
-    <div>
-      <Translate content="workflowDefaultDialog.text" />
-      <br />
-      <button type="submit" onSubmit={onSuccess}>ok</button>
-    </div>
-  )
-}
+WorkflowDefaultDialog.defaultProps = {
+  onSuccess: () => {}
+};
+
+WorkflowDefaultDialog.propTypes = {
+  onSuccess: React.PropTypes.func
+};
 
 export default WorkflowDefaultDialog;
