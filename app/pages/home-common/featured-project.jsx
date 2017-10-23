ed62df3589f98438b1e546eeffa0d81ad56d53a2
Sarah Allen <sarah@zooniverse.org>
2017-10-23 13:03:05 -0500

    Use img tag. Comment about compressing and resizing static assets

--- app/pages/home-common/featured-project.jsx
+++ app/pages/home-common/featured-project.jsx
@@ -5,7 +5,8 @@ Featured Project Component
 * NOT to be confused with <HomePagePromoted />
 * Content is hardcoded for the moment; improvements on this are welcome.
 * Originally created to highlight 2017 Mar/Apr Stargazing Live projects.
-* Design: @beckyrother; code: @shaunanoordin; documented/updated: 20170403
+* Design: @beckyrother; code: @shaunanoordin; documented/updated: 20171023
+* Compress and resize any static images before uploading and deploying!
 */
 
 import React from 'react';
@@ -16,10 +17,9 @@ const FeaturedProject = (() => (
   <section className="home-featured">
     <h1 className="secondary-kicker">Featured Project</h1>
     <div className="home-featured-images">
-      <Thumbnail
+      <img
         alt="Seabird Watch"
         src="/assets/featured-projects/featured-project-20171023-autumnwatch.jpg"
-        width={800}
       />
     </div>
     <h2 className="secondary-headline">Seabird Watch</h2>
