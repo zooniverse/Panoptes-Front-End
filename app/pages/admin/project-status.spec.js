1c4f3a56020f8cadb23adeea29fde6d76b52a703
jelliotartz <jelliotartz@gmail.com>
2017-11-01 16:10:32 -0500

    Refactor ProjectStatus unit tests.

--- app/pages/admin/project-status.spec.js
+++ app/pages/admin/project-status.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import assert from 'assert';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
 import ProjectStatus from './project-status';
 
 const project = {
@@ -34,12 +33,10 @@ const workflows = [
 
 describe('ProjectStatus', () => {
   let wrapper;
-  let handleWorkflowToggleSpy;
+  let loadingIndicator;
 
   before(() => {
-    handleWorkflowToggleSpy = sinon.spy();
-
-    wrapper = shallow(<ProjectStatus handleToggle={handleWorkflowToggleSpy} />);
+    wrapper = shallow(<ProjectStatus />);
 
     wrapper.setState({
       project,
@@ -48,67 +45,60 @@ describe('ProjectStatus', () => {
   });
 
   it('renders without crashing', () => {
-    const ProjectStatusContainer = wrapper.find('div.project-status');
-    assert.equal(ProjectStatusContainer.length, 1);
+    assert.equal(wrapper, wrapper);
   });
 
-  describe('page layout', () => {
-    it('renders the ProjectIcon component', () => {
-      const projectIconComponent = wrapper.find('ProjectIcon');
-      assert.equal(projectIconComponent.length, 1);
-    });
-
-    it('renders a list of basic info about a project\'s status', () => {
-      const projectStatusInfo = wrapper.find('div.project-status__section').find('ul').first().children();
-      assert.equal(projectStatusInfo.length, 5);
-    });
-
-    it('renders a list of visibility settings', () => {
-      const visibilitySettings = wrapper.find('ul.project-status__section-list').first().children();
-      assert.equal(visibilitySettings.length, 6);
-    });
-
-    it('renders the ExperimentalFeatures component', () => {
-      const experimentalFeaturesComponent = wrapper.find('ExperimentalFeatures');
-      assert.equal(experimentalFeaturesComponent.length, 1);
-    });
-
-    it('renders workflows when they are present', () => {
-      const workflowList = wrapper.find('li.section-list__item');
-      assert.equal(workflowList.length, workflows.length);
-    });
-
-    it('renders the VersionList component', () => {
-      const versionListComponent = wrapper.find('VersionList');
-      assert.equal(versionListComponent.length, 1);
-    });
+  it('renders a loading indicator if project is not in state', () => {
+    wrapper.setState({ project: null });
+    loadingIndicator = wrapper.find('LoadingIndicator');
+    assert.equal(loadingIndicator.length, 1);
   });
 
-  describe('Workflow Settings', () => {
-    it('displays an asterisk next to the default workflow, if one is set', () => {
-      const defaultWorkflow = wrapper.find('li.section-list__item').first().text();
-      assert.ok(defaultWorkflow.match(' * '), true);
-    });
+  it('does not render a loading indicator if project is in state', () => {
+    wrapper.setState({ project });
+    loadingIndicator = wrapper.find('LoadingIndicator');
+    assert.equal(loadingIndicator.length, 0);
+  });
 
-    it('does not display an asterisk next to a workflow that is not the default workflow', () => {
-      const defaultWorkflow = wrapper.find('li.section-list__item').last().text();
-      assert.ok(defaultWorkflow.match(' * '), false);
-    });
+  it('renders the ProjectIcon component', () => {
+    const projectIconComponent = wrapper.find('ProjectIcon');
+    assert.equal(projectIconComponent.length, 1);
+  });
 
-    it('renders a WorkflowToggle component for each workflow', () => {
-      const workflowToggleComponents = wrapper.find('WorkflowToggle');
-      assert.equal(workflowToggleComponents.length, workflows.length);
-    });
+  it('renders the RedirectToggle component', () => {
+    const redirectToggleComponent = wrapper.find('RedirectToggle');
+    assert.equal(redirectToggleComponent.length, 1);
+  });
 
-    it('renders a WorkflowDefaultDialog component when a user toggles a default workflow to inactive', () => {
-      // RFC: is this necessary/good practice? or out of scope of this component?
-      // TODO: this test is currently failing.
-      // wrapper.find('WorkflowToggle').first().simulate('change');
-      // sinon.assert.calledOnce(handleWorkflowToggleSpy);
-    });
+  it('renders the ExperimentalFeatures component', () => {
+    const experimentalFeaturesComponent = wrapper.find('ExperimentalFeatures');
+    assert.equal(experimentalFeaturesComponent.length, 1);
+  });
 
-    it('handles onCancel functionality for modal dialog', () => {
-      // RFC: is this necessary/good practice? or out of scope of this component?
-    });
+  it('renders the VersionList component', () => {
+    const versionListComponent = wrapper.find('VersionList');
+    assert.equal(versionListComponent.length, 1);
+  });
+
+  it('displays an asterisk next to the default workflow, if one is set', () => {
+    const defaultWorkflow = wrapper.find('li.section-list__item').first().text();
+    assert.ok(defaultWorkflow.match(' * '), true);
+  });
+
+  it('does not display an asterisk next to a workflow that is not the default workflow', () => {
+    const defaultWorkflow = wrapper.find('li.section-list__item').last().text();
+    assert.ok(defaultWorkflow.match(' * '), false);
+  });
+
+  it('renders a WorkflowToggle component for each workflow', () => {
+    const workflowToggleComponents = wrapper.find('WorkflowToggle');
+    assert.equal(workflowToggleComponents.length, workflows.length);
+  });
+
+  it('renders the WorkflowDefaultDialog component when dialogIsOpen state is true', () => {
+    wrapper.setState({ dialogIsOpen: true });
+    const workflowDefaultDialog = wrapper.find('WorkflowDefaultDialog');
+    // TODO: Refactor ProjectStatus to only render one WorkflowDefaultDialog component
+    assert.equal(workflowDefaultDialog.length, workflows.length);
   });
 });
