563dd6a24af746b0f8fd494c7834d01ae9d51be6
jelliotartz <jelliotartz@gmail.com>
2017-11-02 15:10:43 -0500

    Refactor tests.

--- app/pages/admin/project-status.spec.js
+++ app/pages/admin/project-status.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import assert from 'assert';
+import sinon from 'sinon';
 import { shallow } from 'enzyme';
 import ProjectStatus from './project-status';
 
@@ -31,11 +32,14 @@ const workflows = [
   }
 ];
 
-describe('ProjectStatus', () => {
+describe.only('ProjectStatus', () => {
   let wrapper;
   let loadingIndicator;
+  let onChangeWorkflowLevelSpy;
 
   before(() => {
+    onChangeWorkflowLevelSpy = sinon.stub(ProjectStatus.prototype, 'onChangeWorkflowLevel');
+
     wrapper = shallow(<ProjectStatus />);
 
     wrapper.setState({
@@ -48,57 +52,67 @@ describe('ProjectStatus', () => {
     assert.equal(wrapper, wrapper);
   });
 
-  it('renders a loading indicator if project is not in state', () => {
-    wrapper.setState({ project: null });
-    loadingIndicator = wrapper.find('LoadingIndicator');
-    assert.equal(loadingIndicator.length, 1);
+  describe('custom components', () => {
+    it('LoadingIndicator renders if project is not in state', () => {
+      wrapper.setState({ project: null });
+      loadingIndicator = wrapper.find('LoadingIndicator');
+      assert.equal(loadingIndicator.length, 1);
+    });
+
+    it('LoadingIndicator does not render if project is in state', () => {
+      wrapper.setState({ project });
+      loadingIndicator = wrapper.find('LoadingIndicator');
+      assert.equal(loadingIndicator.length, 0);
+    });
+
+    it('ProjectIcon renders', () => {
+      const projectIconComponent = wrapper.find('ProjectIcon');
+      assert.equal(projectIconComponent.length, 1);
+    });
+
+    it('RedirectToggle renders', () => {
+      const redirectToggleComponent = wrapper.find('RedirectToggle');
+      assert.equal(redirectToggleComponent.length, 1);
+    });
+
+    it('ExperimentalFeatures renders', () => {
+      const experimentalFeaturesComponent = wrapper.find('ExperimentalFeatures');
+      assert.equal(experimentalFeaturesComponent.length, 1);
+    });
+
+    it('VersionList renders', () => {
+      const versionListComponent = wrapper.find('VersionList');
+      assert.equal(versionListComponent.length, 1);
+    });
   });
 
-  it('does not render a loading indicator if project is in state', () => {
-    wrapper.setState({ project });
-    loadingIndicator = wrapper.find('LoadingIndicator');
-    assert.equal(loadingIndicator.length, 0);
-  });
+  describe('workflow settings', () => {
+    it('displays an asterisk next to the default workflow, if one is set', () => {
+      const defaultWorkflow = wrapper.find('li.section-list__item').first().text();
+      assert.ok(defaultWorkflow.match(' * '), true);
+    });
 
-  it('renders the ProjectIcon component', () => {
-    const projectIconComponent = wrapper.find('ProjectIcon');
-    assert.equal(projectIconComponent.length, 1);
-  });
+    it('does not display an asterisk next to a workflow that is not the default workflow', () => {
+      const notDefaultWorkflow = wrapper.find('li.section-list__item').last().text();
+      assert.ok(notDefaultWorkflow.match(' * '), false);
+    });
 
-  it('renders the RedirectToggle component', () => {
-    const redirectToggleComponent = wrapper.find('RedirectToggle');
-    assert.equal(redirectToggleComponent.length, 1);
-  });
+    it('renders a WorkflowToggle component for each workflow', () => {
+      const workflowToggleComponents = wrapper.find('WorkflowToggle');
+      assert.equal(workflowToggleComponents.length, workflows.length);
+    });
 
-  it('renders the ExperimentalFeatures component', () => {
-    const experimentalFeaturesComponent = wrapper.find('ExperimentalFeatures');
-    assert.equal(experimentalFeaturesComponent.length, 1);
-  });
+    it('renders the WorkflowDefaultDialog component when dialogIsOpen state is true', () => {
+      wrapper.setState({ dialogIsOpen: true });
+      const workflowDefaultDialog = wrapper.find('WorkflowDefaultDialog');
+      // TODO: Refactor ProjectStatus to only render one WorkflowDefaultDialog component
+      assert.equal(workflowDefaultDialog.length, workflows.length);
+      wrapper.setState({ dialogIsOpen: false });
+    });
 
-  it('renders the VersionList component', () => {
-    const versionListComponent = wrapper.find('VersionList');
-    assert.equal(versionListComponent.length, 1);
-  });
-
-  it('displays an asterisk next to the default workflow, if one is set', () => {
-    const defaultWorkflow = wrapper.find('li.section-list__item').first().text();
-    assert.ok(defaultWorkflow.match(' * '), true);
-  });
-
-  it('does not display an asterisk next to a workflow that is not the default workflow', () => {
-    const defaultWorkflow = wrapper.find('li.section-list__item').last().text();
-    assert.ok(defaultWorkflow.match(' * '), false);
-  });
-
-  it('renders a WorkflowToggle component for each workflow', () => {
-    const workflowToggleComponents = wrapper.find('WorkflowToggle');
-    assert.equal(workflowToggleComponents.length, workflows.length);
-  });
-
-  it('renders the WorkflowDefaultDialog component when dialogIsOpen state is true', () => {
-    wrapper.setState({ dialogIsOpen: true });
-    const workflowDefaultDialog = wrapper.find('WorkflowDefaultDialog');
-    // TODO: Refactor ProjectStatus to only render one WorkflowDefaultDialog component
-    assert.equal(workflowDefaultDialog.length, workflows.length);
+    it('calls #onChangeWorkflowLevel when a user changes a workflow\'s configuration level', () => {
+      wrapper.find('select').first().simulate('change');
+      sinon.assert.calledOnce(onChangeWorkflowLevelSpy);
+    });
   });
 });
