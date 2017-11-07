4f8e5e7a8a9cf7896c78f6163e548ab4ce2720d1
jelliotartz <jelliotartz@gmail.com>
2017-11-07 13:30:28 -0600

    Refactor tests.

--- app/pages/admin/project-status.spec.js
+++ app/pages/admin/project-status.spec.js
@@ -33,68 +33,73 @@ const workflows = [
 ];
 
 describe('ProjectStatus', () => {
-  let wrapper;
+  let handleDialogCancelStub;
+  let handleDialogSuccessStub;
   let loadingIndicator;
-  let onChangeWorkflowLevelSpy;
-  let handleDialogCancelSpy;
-  let handleDialogSuccessSpy;
-  let handleToggleSpy;
-  let event;
-
+  let onChangeWorkflowLevelStub;
+  let wrapper;
 
   before(() => {
-    onChangeWorkflowLevelSpy = sinon.stub(ProjectStatus.prototype, 'onChangeWorkflowLevel');
-    handleDialogCancelSpy = sinon.stub(ProjectStatus.prototype, 'handleDialogCancel');
-    handleDialogSuccessSpy = sinon.stub(ProjectStatus.prototype, 'handleDialogSuccess');
-    handleToggleSpy = sinon.stub(ProjectStatus.prototype, 'handleToggle');
+    onChangeWorkflowLevelStub = sinon.stub(ProjectStatus.prototype, 'onChangeWorkflowLevel');
+    handleDialogCancelStub = sinon.stub(ProjectStatus.prototype, 'handleDialogCancel');
+    handleDialogSuccessStub = sinon.stub(ProjectStatus.prototype, 'handleDialogSuccess');
 
     wrapper = shallow(<ProjectStatus />);
-
-    wrapper.setState({
-      project,
-      workflows
-    });
   });
 
   it('renders without crashing', () => {
     assert.equal(wrapper, wrapper);
   });
 
-  describe('custom components', () => {
-    it('LoadingIndicator renders if project is not in state', () => {
-      wrapper.setState({ project: null });
+  describe('when no project is in component state', () => {
+    it('renders Loading Indicator component', () => {
       loadingIndicator = wrapper.find('LoadingIndicator');
       assert.equal(loadingIndicator.length, 1);
     });
+  });
 
-    it('LoadingIndicator does not render if project is in state', () => {
+  describe('when project is in component state', () => {
+    before(() => {
       wrapper.setState({ project });
+    });
+
+    it('does not render the LoadingIndicator component', () => {
       loadingIndicator = wrapper.find('LoadingIndicator');
       assert.equal(loadingIndicator.length, 0);
     });
 
-    it('ProjectIcon renders', () => {
+    it('renders a ProjectIcon component', () => {
+      wrapper.setState({ project });
       const projectIconComponent = wrapper.find('ProjectIcon');
       assert.equal(projectIconComponent.length, 1);
     });
 
-    it('RedirectToggle renders', () => {
+    it('renders a RedirectToggle component', () => {
       const redirectToggleComponent = wrapper.find('RedirectToggle');
       assert.equal(redirectToggleComponent.length, 1);
     });
 
-    it('ExperimentalFeatures renders', () => {
+    it('renders a ExperimentalFeatures component', () => {
       const experimentalFeaturesComponent = wrapper.find('ExperimentalFeatures');
       assert.equal(experimentalFeaturesComponent.length, 1);
     });
 
-    it('VersionList renders', () => {
+    it('renders a VersionList component', () => {
       const versionListComponent = wrapper.find('VersionList');
       assert.equal(versionListComponent.length, 1);
     });
+
+    it('renders a no workflows found message when no workflow is in component state', () => {
+      const noWorkflowsMessage = wrapper.find('div[children="No workflows found"]');
+      assert.equal(noWorkflowsMessage.length, 1);
+    });
   });
 
-  describe('workflow settings', () => {
+  describe('when workflow is in component state', () => {
+    before(() => {
+      wrapper.setState({ workflows });
+    });
+
     it('displays an asterisk next to the default workflow, if one is set', () => {
       const defaultWorkflow = wrapper.find('li.section-list__item').first().text();
       assert.ok(defaultWorkflow.match(' * '), true);
@@ -110,15 +115,9 @@ describe('ProjectStatus', () => {
       assert.equal(workflowToggleComponents.length, workflows.length);
     });
 
-    it('calls #handleToggle when a user clicks the active toggle on a workflow', () => {
-      const notDefaultWorkflow = wrapper.find('WorkflowToggle').last();
-      notDefaultWorkflow.simulate('click', event);
-      sinon.assert.calledOnce(handleToggleSpy);
-    });
-
     it('calls #onChangeWorkflowLevel when a user changes a workflow\'s configuration level', () => {
       wrapper.find('select').first().simulate('change');
-      sinon.assert.calledOnce(onChangeWorkflowLevelSpy);
+      sinon.assert.calledOnce(onChangeWorkflowLevelStub);
     });
 
     it('renders the WorkflowDefaultDialog component when dialogIsOpen state is true', () => {
@@ -130,13 +129,13 @@ describe('ProjectStatus', () => {
     it('calls #handleDialogCancel when a user cancels the WorkflowDefaultDialog modal', () => {
       const workflowDefaultDialog = wrapper.find('WorkflowDefaultDialog');
       workflowDefaultDialog.simulate('cancel');
-      sinon.assert.calledOnce(handleDialogCancelSpy);
+      sinon.assert.calledOnce(handleDialogCancelStub);
     });
 
     it('calls #handleDialogSuccess when a user clicks ok on the WorkflowDefaultDialog modal', () => {
       const workflowDefaultDialog = wrapper.find('WorkflowDefaultDialog');
       workflowDefaultDialog.simulate('success');
-      sinon.assert.calledOnce(handleDialogSuccessSpy);
+      sinon.assert.calledOnce(handleDialogSuccessStub);
     });
   });
 });
