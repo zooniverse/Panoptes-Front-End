338abb4fbeb916494c6e00a07ec705f6dc60e0f9
jelliotartz <jelliotartz@gmail.com>
2017-11-10 11:41:48 -0600

    Update CollectionsCreateForm spec for collection description field.

--- app/collections/create-form.spec.js
+++ app/collections/create-form.spec.js
@@ -5,14 +5,16 @@ import { shallow } from 'enzyme';
 import CollectionsCreateForm from './create-form';
 
 describe('<CollectionsCreateForm />', () => {
-  let wrapper;
-  let onSubmitStub;
   let addCollectionButton;
+  let handleDescriptionInputChangeStub;
   let handleNameInputChangeStub;
+  let onSubmitStub;
+  let wrapper;
 
   before(() => {
-    onSubmitStub = sinon.stub(CollectionsCreateForm.prototype, 'onSubmit');
+    handleDescriptionInputChangeStub = sinon.stub(CollectionsCreateForm.prototype, 'handleDescriptionInputChange');
     handleNameInputChangeStub = sinon.stub(CollectionsCreateForm.prototype, 'handleNameInputChange');
+    onSubmitStub = sinon.stub(CollectionsCreateForm.prototype, 'onSubmit');
 
     wrapper = shallow(<CollectionsCreateForm />);
   });
@@ -21,24 +23,65 @@ describe('<CollectionsCreateForm />', () => {
     assert.equal(wrapper, wrapper);
   });
 
-  it('renders a disabled button when there is no text in the input', () => {
-    addCollectionButton = wrapper.find('button');
-    assert.equal(addCollectionButton.prop('disabled'), true);
+  describe('should render a disabled submit button', () => {
+    it('when there is no text in the name input', () => {
+      wrapper.setState({
+        collectionNameLength: 0
+      });
+      addCollectionButton = wrapper.find('button');
+      assert.equal(addCollectionButton.prop('disabled'), true);
+    });
+
+    it('when name input is valid but description text is more than 300 characters', () => {
+      wrapper.setState({
+        collectionNameLength: 5,
+        descriptionText: 'this is a test description that is more than 300 characters in length. this is a test description that is more than 300 characters in length. this is a test description that is more than 300 characters in length. this is a test description that is more than 300 characters in length. this is a test description that is more than 300 characters in length.' // eslint-disable-line max-len
+      });
+      addCollectionButton = wrapper.find('button');
+      assert.equal(addCollectionButton.prop('disabled'), true);
+    });
   });
 
-  it('renders a submit button when there is text in the input', () => {
-    wrapper.setState({ collectionNameLength: 5 });
+  it('should render an enabled submit button when name and description are valid', () => {
+    wrapper.setState({
+      collectionNameLength: 5,
+      descriptionText: 'valid description text length'
+    });
     addCollectionButton = wrapper.find('button');
     assert.equal(addCollectionButton.prop('disabled'), false);
   });
 
-  it('calls #onSubmit when the add collection button is clicked', () => {
-    wrapper.find('form').simulate('submit');
-    sinon.assert.calledOnce(onSubmitStub);
+  describe('event handlers', () => {
+    it('should call #onSubmit when the add collection button is clicked', () => {
+      wrapper.find('form').simulate('submit');
+      sinon.assert.calledOnce(onSubmitStub);
+    });
+
+    it('should call #handleNameInputChange when a user changes text in the name input', () => {
+      wrapper.find('input.collection-name-input').simulate('change');
+      sinon.assert.calledOnce(handleNameInputChangeStub);
+    });
+
+    it('should call #handleDescriptionInputChange when a user changes text in the description input', () => {
+      wrapper.find('textarea.collection-name-input').simulate('change');
+      sinon.assert.calledOnce(handleDescriptionInputChangeStub);
+    });
   });
 
-  it('calls #handleNameInputChange when a user changes text in the name input', () => {
-    wrapper.find('input.collection-name-input').simulate('change');
-    sinon.assert.calledOnce(handleNameInputChangeStub);
+  describe('should handle errors', () => {
+    it('when there is an error', () => {
+      wrapper.setState({ error: ['it broke!'] });
+      assert.equal(wrapper.find('.error').length, 1);
+    });
+
+    it('when a user tries to enter a duplicate collection name ', () => {
+      wrapper.setState({
+        error: {
+          status: 400
+        }
+      });
+      const errorMessage = wrapper.find('.error');
+      assert.equal(errorMessage.text(), 'You can\'t name two collections the same thing!');
+    });
   });
 });
