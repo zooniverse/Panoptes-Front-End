d788908986696cb9727594025dffb0bc6b0b3ff5
jelliotartz <jelliotartz@gmail.com>
2017-11-10 11:43:16 -0600

    Update CollectionSettings spec for collection description field. - Also fix linter errors/warnings.

--- app/collections/settings.spec.js
+++ app/collections/settings.spec.js
@@ -19,34 +19,42 @@ const collectionWithoutDefaultSubject = {
   display_name: 'A collection',
   private: false,
   slug: 'username/a-collection'
-}
+};
 
-describe('<CollectionSettings />', function() {
+describe('<CollectionSettings />', () => {
   let wrapper;
   let confirmationSpy;
   let deleteButton;
-  before(function() {
+  let handleDescriptionInputChangeStub;
+
+  before(() => {
     confirmationSpy = sinon.spy(CollectionSettings.prototype, 'confirmDelete');
-    wrapper = shallow(<CollectionSettings canCollaborate={true} collection={collectionWithoutDefaultSubject} />, { context: { router: {} } });
+    handleDescriptionInputChangeStub = sinon.stub(CollectionSettings.prototype, 'handleDescriptionInputChange');
+    wrapper = shallow(<CollectionSettings
+      canCollaborate={true}
+      collection={collectionWithoutDefaultSubject}
+    />,
+      { context: { router: {}}}
+    );
     deleteButton = wrapper.find('button.error');
   });
 
-  it('should render without crashing', function() {
+  it('should render without crashing', () => {
     assert.equal(wrapper, wrapper);
   });
 
-  it('should render a <DisplayNameSlugEditor /> component', function() {
+  it('should render a <DisplayNameSlugEditor /> component', () => {
     assert.equal(wrapper.find(DisplayNameSlugEditor).length, 1);
   });
 
-  it('should render the correct default checked attribute for visibility', function() {
+  it('should render the correct default checked attribute for visibility', () => {
     const privateChecked = wrapper.find('input[type="radio"]').first().props().defaultChecked;
     const publicChecked = wrapper.find('input[type="radio"]').last().props().defaultChecked;
     assert.equal(privateChecked, collectionWithoutDefaultSubject.private);
     assert.notEqual(publicChecked, collectionWithoutDefaultSubject.private);
   });
 
-  it('should render the thumbnail correctly depending on presence of default_subject_src', function() {
+  it('should render the thumbnail correctly depending on presence of default_subject_src', () => {
     const thumbnailFirstInstance = wrapper.find(Thumbnail);
     assert.equal(thumbnailFirstInstance.length, 0);
     wrapper.setProps({ collection: collectionWithDefaultSubject });
@@ -54,13 +62,18 @@ describe('<CollectionSettings />', function() {
     assert.equal(thumbnailSecondInstance.length, 1);
   });
 
-  it('should render permission messaging if there is no user', function() {
+  it('should call this.handleDescriptionInputChange() when a user changes description text', () => {
+    wrapper.find('textarea').simulate('change');
+    sinon.assert.calledOnce(handleDescriptionInputChangeStub);
+  });
+
+  it('should render permission messaging if there is no user', () => {
     wrapper.setProps({ canCollaborate: false, user: null });
     const permissionMessage = wrapper.contains(<p>Not allowed to edit this collection</p>);
     assert(permissionMessage, true);
   });
 
-  it('should call this.confirmDelete() when the delete button is clicked', function() {
+  it('should call this.confirmDelete() when the delete button is clicked', () => {
     deleteButton.simulate('click');
     sinon.assert.calledOnce(confirmationSpy);
   });
