e66f3e0760cc1ea12e1c4dee051d0f164e4233df
jelliotartz <jelliotartz@gmail.com>
2017-11-29 09:51:16 -0600

    Add unit tests for AboutHome component.

---
+++ app/pages/about/about-home.spec.js
@@ -0,0 +1,23 @@
+/* eslint prefer-arrow-callback: 0, func-names: 0, 'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx"] }] */
+
+import React from 'react';
+import assert from 'assert';
+import { shallow } from 'enzyme';
+import AboutHome from './about-home';
+
+describe.only('AboutHome', function () {
+  let wrapper;
+
+  before(function () {
+    wrapper = shallow(<AboutHome />);
+  });
+
+  it('renders without crashing', function () {
+    assert.equal(wrapper, wrapper);
+  });
+
+  it('renders five markdown elements', function () {
+    const markdownElements = wrapper.find('div.on-secondary-page').children();
+    assert.equal(markdownElements.length, 5);
+  });
+});
