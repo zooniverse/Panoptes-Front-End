e962a91d06510d7360269c5d03c2e7a3ac79351a
Simone Duca <simone@zooniverse.org>
2017-12-05 16:12:24 +0000

    Convert project page to js

---
+++ app/pages/project/project-page.jsx
@@ -0,0 +1,167 @@
+import React from 'react';
+import Translate from 'react-translate-component';
+import { Markdown } from 'markdownz';
+import { sugarClient } from 'panoptes-client/lib/sugar';
+import ProjectNavbar from './project-navbar';
+import PotentialFieldGuide from './potential-field-guide';
+
+export default class ProjectPage extends React.Component {
+  constructor() {
+    super();
+    const lastSugarSubscribedID = null;
+  }
+
+  getChildContext() {
+    return this.context.geordi;
+  }
+
+  componentDidMount() {
+    document.documentElement.classList.add('on-project-page');
+    this.updateSugarSubscription(this.props.project);
+    (this.context.geordi) ? this.context.geordi.remember({ projectToken: this.props.project.slug }) : null;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.project !== this.props.project) {
+      this.updateSugarSubscription(nextProps.project);
+      (this.context.geordi) ? this.context.geordi.remember({ projectToken: nextProps.project.slug }) : null;
+    }
+  }
+
+  componentWillUnmount() {
+    document.documentElement.classList.remove('on-project-page');
+    this.updateSugarSubscription(null);
+    this.context.geordi.forget(['projectToken']);
+  }
+
+  updateSugarSubscription(project) {
+    if (this.lastSugarSubscribedID) {
+      sugarClient.unsubscribeFrom(`project-${this.lastSugarSubscribedID}`);
+    }
+    if (project) {
+      sugarClient.subscribeTo(`project-${this.lastSugarSubscribedID}`);
+    }
+  }
+
+  render() {
+    const projectPath = `/projects/${this.props.project.slug}`;
+    const onHomePage = this.props.routes[2].path === undefined;
+    const pages = this.props.pages.reduce((map, page) => {
+      map[page.url_key] = page;
+      return map;
+    }, {});
+
+    const renderAnnouncement = (this.props.project.configuration && !!this.props.project.configuration.announcement) ?
+      <div className="informational project-announcement-banner">
+        <Markdown>
+          {this.props.project.configuration.announcement}
+        </Markdown>
+      </div> :
+      null;
+
+    const renderNavbar = (!onHomePage) ?
+      <div>
+        <ProjectNavbar {...this.props} />
+        {renderAnnouncement}
+      </div> :
+      null;
+
+    const renderLaunchApproved = (!this.props.project.launch_approved) ?
+      <Translate
+        component="p"
+        className="project-disclaimer"
+        content="project.disclaimer"
+      /> :
+      null;
+
+    const renderPotentialFieldGuide = (this.props.location.pathname !== projectPath) ?
+      <PotentialFieldGuide
+        guide={this.props.guide}
+        guideIcons={this.props.guideIcons}
+      /> :
+      null;
+
+    let backgroundStyle = {};
+    if (this.props.background) {
+      backgroundStyle = {
+        backgroundImage: `radial-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.8)), url('${this.props.background.src}')`
+      };
+    }
+
+    return (
+      <div className="project-page project-background" style={backgroundStyle}>
+        {renderNavbar}
+        {renderLaunchApproved}
+        {renderPotentialFieldGuide}
+        {React.cloneElement(this.props.children, {
+          background: this.props.background,
+          loadingSelectedWorkflow: this.props.loadingSelectedWorkflow,
+          onChangePreferences: this.props.onChangePreferences,
+          organization: this.props.organization,
+          owner: this.props.owner,
+          pages: this.props.pages,
+          preferences: this.props.preferences,
+          project: this.props.project,
+          projectAvatar: this.props.projectAvatar,
+          projectIsComplete: this.props.projectIsComplete,
+          projectRoles: this.props.projectRoles,
+          splits: this.props.splits,
+          translation: this.props.translation,
+          user: this.props.user,
+          workflow: this.props.workflow
+        })}
+      </div>
+    );
+  }
+}
+
+ProjectPage.defaultProps = {
+  loading: false,
+  organization: null,
+  pages: null,
+  project: {
+    id: '',
+    display_name: '',
+    redirect: '',
+    slug: ''
+  },
+  projectAvatar: null,
+  projectRoles: [],
+  routes: [],
+  translation: {
+    id: '',
+    display_name: ''
+  },
+  user: null,
+  workflow: null
+};
+
+ProjectPage.childContextTypes = {
+  geordi: React.PropTypes.object
+};
+
+ProjectPage.propTypes = {
+  loading: React.PropTypes.bool,
+  pages: React.PropTypes.shape({
+    content: React.PropTypes.string
+  }),
+  project: React.PropTypes.shape({
+    id: React.PropTypes.string,
+    display_name: React.PropTypes.string,
+    slug: React.PropTypes.string
+  }),
+  projectAvatar: React.PropTypes.shape({
+    src: React.PropTypes.string
+  }),
+  projectRoles: React.PropTypes.array,
+  user: React.PropTypes.shape({
+    id: React.PropTypes.string
+  }),
+  routes: React.PropTypes.array,
+  translation: React.PropTypes.shape({
+    display_name: React.PropTypes.string
+  }),
+  workflow: React.PropTypes.shape({
+    id: React.PropTypes.string
+  })
+};
