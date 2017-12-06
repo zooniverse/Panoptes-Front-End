41f3d93d691710bb1d35dbdf088e65667deb8922
jelliotartz <jelliotartz@gmail.com>
2017-12-06 16:04:16 -0600

    Add lifecycle hooks on AboutHome to add/remove .on-secondary-page.

--- app/pages/about/about-home.jsx
+++ app/pages/about/about-home.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import counterpart from 'counterpart';
 import { Markdown } from 'markdownz';
 
-const AboutHome = () =>
-  (<div className="on-secondary-page">
-    <Markdown>{counterpart('about.home.title')}</Markdown>
-    <Markdown>{counterpart('about.home.whatIsZooniverse')}</Markdown>
-    <Markdown>{counterpart('about.home.anyoneCanResearch')}</Markdown>
-    <Markdown>{counterpart('about.home.accelerateResearch')}</Markdown>
-    <Markdown>{counterpart('about.home.discoveries')}</Markdown>
-  </div>);
+class AboutHome extends React.Component {
+  componentDidMount() {
+    if (document) {
+      document.documentElement.classList.add('on-secondary-page');
+    }
+  }
+
+  componentWillUnmount() {
+    document.documentElement.classList.remove('on-secondary-page');
+  }
+
+  render() {
+    return (
+      <div>
+        <Markdown>{counterpart('about.home.title')}</Markdown>
+        <Markdown>{counterpart('about.home.whatIsZooniverse')}</Markdown>
+        <Markdown>{counterpart('about.home.anyoneCanResearch')}</Markdown>
+        <Markdown>{counterpart('about.home.accelerateResearch')}</Markdown>
+        <Markdown>{counterpart('about.home.discoveries')}</Markdown>
+      </div>
+    );
+  }
+}
 
 export default AboutHome;
