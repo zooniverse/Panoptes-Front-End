bbe59844f486c11a4949aac1e98daaaf3542bda2
jelliotartz <jelliotartz@gmail.com>
2017-12-06 15:53:28 -0600

    Add lifecycle hooks for add/remove of .on-secondary-page.

--- app/pages/about/contact.jsx
+++ app/pages/about/contact.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import counterpart from 'counterpart';
 import { Markdown } from 'markdownz';
 
-const Contact = () =>
-  (<div className="on-secondary-page">
-    <Markdown>{counterpart('about.contact.title')}</Markdown>
-    <Markdown>{counterpart('about.contact.discussionBoards')}</Markdown>
-    <Markdown>{counterpart('about.contact.email')}</Markdown>
-    <Markdown>{counterpart('about.contact.collaborating')}</Markdown>
-    <Markdown>{counterpart('about.contact.pressInquiries')}</Markdown>
-    <Markdown>{counterpart('about.contact.dailyZoo')}</Markdown>
-  </div>);
+class Contact extends React.Component {
+  componentDidMount() {
+    if (document) {
+      document.documentElement.classList.add('on-secondary-page');
+    }
+  }
+
+  componentWillUnmount() {
+    document.documentElement.classList.remove('on-secondary-page');
+  }
+
+  render() {
+    return (
+      <div>
+        <Markdown>{counterpart('about.contact.title')}</Markdown>
+        <Markdown>{counterpart('about.contact.discussionBoards')}</Markdown>
+        <Markdown>{counterpart('about.contact.email')}</Markdown>
+        <Markdown>{counterpart('about.contact.collaborating')}</Markdown>
+        <Markdown>{counterpart('about.contact.pressInquiries')}</Markdown>
+        <Markdown>{counterpart('about.contact.dailyZoo')}</Markdown>
+      </div>
+    )
+  }
+}
 
 export default Contact;
