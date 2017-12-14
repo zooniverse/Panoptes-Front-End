9a13d927365d65bbedaa7b5cc16018f680301ac0
Jim O'Donnell <jim@eatyourgreens.org.uk>
2017-12-14 17:06:58 +0000

    Fix fatal project page bug (#4231)
    * default matchingPage to an empty object
    
    * Clean up linter errors
    Rewrite constructPagesData() to use Array.map.
    Tidy up other minor linter warnings.
    Add prop types and default props.
    
    * Add tests
    
    * Fix evergreen tests
    Use mount to test full component lifecycle.
    Fix default translations prop.

---
+++ app/pages/project/about/index.spec.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import { AboutProject } from './';
+
+function Page() {
+  return (
+    <p>Hello world!</p>
+  );
+}
+
+describe('AboutProject', () => {
+  describe('with only default props', () => {
+    it('should render', () => {
+      const wrapper = shallow(<AboutProject />);
+      expect(wrapper.instance()).to.be.instanceOf(AboutProject);
+    });
+  });
+  describe('with a child page', () => {
+    const wrapper = mount(
+      <AboutProject>
+        <Page />
+      </AboutProject>
+    );
+    const page = wrapper.update().find('Page');
+    it('should render', () => {
+      expect(wrapper.instance()).to.be.instanceOf(AboutProject);
+    });
+    it('should pass pages and team arrays to the child page', () => {
+      expect(page.props().pages).to.equal(wrapper.state().pages);
+      expect(page.props().pages).to.be.instanceOf(Array);
+      expect(page.props().team).to.equal(wrapper.state().team);
+      expect(page.props().team).to.be.instanceOf(Array);
+    });
+  });
+});
