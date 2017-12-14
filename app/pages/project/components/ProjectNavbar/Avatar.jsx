01bec911bb56d3fe9912b437b9e9c219029e9044
Roger Hutchings <roger@hutchin.gs>
2017-12-14 12:34:52 +0000

    Add size attribute for Avatar

--- app/pages/project/components/ProjectNavbar/Avatar.jsx
+++ app/pages/project/components/ProjectNavbar/Avatar.jsx
@@ -1,12 +1,9 @@
 import React, { PropTypes } from 'react';
 import styled from 'styled-components';
 
-const commonStyles = () => `
-  height: 80px;
-  width: 80px;
+const commonStyles = `
   border-radius: 100%;
   display: block;
-  margin-right: 30px;
   box-shadow: 5px 10px 20px 0 rgba(0,0,0,0.22);
 `;
 
@@ -14,29 +11,38 @@ const Img = styled.img.attrs({
   alt: props => `Project icon for ${props.projectTitle}`,
   src: props => props.src
 })`
-  ${commonStyles()}
+  ${commonStyles}
+  height: ${props => props.size}px;
+  width: ${props => props.size}px;
 `;
 
 const DefaultImg = styled.div.attrs({
   'aria-label': 'Default project icon'
 })`
-  ${commonStyles()}
+  ${commonStyles}
   background: url("/assets/simple-pattern.png") repeat;
   background-size: 45px;
+  height: ${props => props.size}px;
+  width: ${props => props.size}px;
 `;
 
-function Avatar({ projectTitle, src }) {
-  const imgProps = { projectTitle, src };
-  return (src) ? <Img {...imgProps} /> : <DefaultImg />;
+function Avatar({ projectTitle, size, src }) {
+  const imgProps = { projectTitle, size, src };
+  return (src) ? <Img {...imgProps} /> : <DefaultImg {...size} />;
 }
 
 Avatar.defaultProps = {
   projectTitle: '',
+  size: 40,
   src: ''
 };
 
 Avatar.propTypes = {
   projectTitle: PropTypes.string,
+  size: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]),
   src: PropTypes.string
 };
 
