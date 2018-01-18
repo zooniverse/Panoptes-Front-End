860dfe1e50bcd63813077019f44a9255d5a4c510
Jim O'Donnell <jim@zooniverse.org>
2018-01-18 15:12:09 +0000

    Fix SVGImage tests

--- app/components/svg-image.spec.js
+++ app/components/svg-image.spec.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import assert from 'assert'
 import SVGImage from './svg-image'
-import {shallow, render} from 'enzyme'
+import {shallow} from 'enzyme'
 
 describe('SVGImage', function(){
   it('should render without crashing', function(){
@@ -10,12 +10,12 @@ describe('SVGImage', function(){
 
   it('it should contain an image element', function(){
     const filename = 'foobar.jpeg'
-    const wrapper = render(<SVGImage src={filename} width={100} height={100} />);
+    const wrapper = shallow(<SVGImage src={filename} width={100} height={100} />);
     const image = wrapper.find('image');
 
-    assert(image.length, 1);
+    assert.equal(image.length, 1);
 
-    assert(image.attr('xlink:href', filename));
+    assert.equal(image.prop('xlinkHref'), filename);
 
   });
 
