c28b57d74bec6cf7b61e9d4622699edf92ea628d
Sarah Allen <sarah@zooniverse.org>
2018-01-26 10:49:46 -0600

    Add Header and Wrapper tests

---
+++ app/pages/project/components/ProjectNavbar/components/StyledHeader/Header.spec.js
@@ -0,0 +1,17 @@
+/* eslint
+  func-names: 0,
+  import/no-extraneous-dependencies: ["error", { "devDependencies": true }]
+  prefer-arrow-callback: 0,
+  "react/jsx-boolean-value": ["error", "always"]
+*/
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import Header from './Header';
+
+describe('Header', function() {
+  it('should render without crashing', function() {
+    shallow(<Header />);
+  });
+});
