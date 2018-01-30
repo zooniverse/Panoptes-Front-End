6fdf6f704f69890674392d6cbb7e185b1a05dce1
Sarah Allen <sarah@zooniverse.org>
2018-01-30 11:16:59 -0600

    First pass on an IE fallback for contrast

--- app/pages/project/components/ProjectNavbar/components/Background/Background.jsx
+++ app/pages/project/components/ProjectNavbar/components/Background/Background.jsx
@@ -21,6 +21,14 @@ export const BackgroundWrapper = styled.div.attrs({
   width: 100%;
 `;
 
+// IE 11 doesn't support filter. Not even their own propriety MS filter. :faceplam:
+export const IEContrastLayer = styled.div`
+  background-color: ${zooTheme.colors.teal.mid};
+  filter: alpha(opacity=50);
+  position: absolute;
+  width: 100%;
+`;
+
 export const ImgBackground = styled.div`
   background-image: url("${props => props.src}");
   background-position: center;
@@ -28,14 +36,23 @@ export const ImgBackground = styled.div`
   background-size: cover;
   filter: blur(${pxToRem(5)}) brightness(50%);
   flex: 1;
+  position: relative;
   transform: scale(1.15);
 `;
+
+function isIE11() {
+  return ('ActiveXObject' in window);
+}
 
 function Background({ src, ...otherProps }) {
   return (
     <ThemeProvider theme={{ mode: 'light' }}>
       <BackgroundWrapper hasBg={!!src} {...otherProps}>
-        {src && <ImgBackground src={src} />}
+        {src &&
+          <ImgBackground src={src}>
+            {isIE11() &&
+              <IEContrastLayer />}
+          </ImgBackground>}
       </BackgroundWrapper>
     </ThemeProvider>
   );
