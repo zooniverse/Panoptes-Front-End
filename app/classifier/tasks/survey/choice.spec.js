cd2b85520fefd4735991d02f7b9196c6399dd4a5
Jim O'Donnell <jim@zooniverse.org>
2018-02-05 11:10:39 +0000

    Add test expectations for single answer questions

---
+++ app/classifier/tasks/survey/choice.spec.js
@@ -0,0 +1,27 @@
+import { shallow } from 'enzyme';
+import assert from 'assert';
+import React from 'react';
+import Choice from './choice';
+
+describe('Choice', function () {
+  describe('with single answer questions', function () {
+    it('should render radio buttons for answers', function () {
+      
+    });
+    it('should render the chosen answer as checked', function () {
+      
+    });
+    it('should clear the chosen answer on click', function () {
+      
+    });
+    it('should clear the chosen answer on space', function () {
+      
+    });
+    it('should clear the chosen answer on backspace', function () {
+      
+    });
+    it('should not clear the chosen answer on any other key press', function () {
+      
+    });
+  })
+});
