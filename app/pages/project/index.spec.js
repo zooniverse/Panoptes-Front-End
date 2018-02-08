13d12605adfaaa4abe5bdfd9545800dd26f68ebd
Jim O'Donnell <jim@zooniverse.org>
2018-02-08 15:35:58 +0000

    Add some tests for context and props Add tests for combinations of initialLoadComplete true or false, URL project params and user change.

--- app/pages/project/index.spec.js
+++ app/pages/project/index.spec.js
@@ -1,34 +1,154 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import assert from 'assert';
 import sinon from 'sinon';
-import ProjectPageController from './';
+import apiClient from 'panoptes-client/lib/api-client';
+import { Split } from 'seven-ten';
+import { ProjectPageController } from './';
+
+const location = {
+  query: {}
+};
+
+const params = {
+  owner: 'owner',
+  name: 'test-project'
+};
+
+const context = {
+  router: {}
+};
 
 describe('ProjectPageController', function () {
   describe('with initial load complete', function () {
-    it('should fetch project data on mount.', function () {
+    let wrapper;
+    let apiRequestStub;
+    const payload = {
+      slug: 'owner/test-project',
+      include: 'avatar,background,owners'
+    };
     
+    before(function () {
+      apiRequestStub = sinon.stub(apiClient, 'request').callsFake(function (method, url, payload) {
+        return Promise.resolve([]);
+      }); 
+      sinon.stub(Split, 'load').callsFake(() => Promise.resolve([]));
     });
+    
+    after(function () {
+      apiRequestStub.restore();
+      Split.load.restore();
+    })
+    
+    beforeEach(function () {
+      context.initialLoadComplete = true;
+      wrapper = mount(
+        <ProjectPageController
+          location={location}
+          params={params}
+        />,
+        { context }
+      );
+    });
+    
+    afterEach(function () {
+      apiRequestStub.resetHistory();
+    })
+    
+    it('should fetch project data on mount.', function () {
+      sinon.assert.calledOnce(apiRequestStub);
+      sinon.assert.calledWith(apiRequestStub, 'get', '/projects', payload);
+    });
+    
     it('should fetch project data again on project change.', function () {
-      
+      wrapper.setState({ loading: false });
+      wrapper.setProps({
+        params: {
+          owner: 'someone',
+          name: 'another-project'
+        }
+      });
+      const newPayload = {
+        slug: 'someone/another-project',
+        include: 'avatar,background,owners'
+      };
+      sinon.assert.calledTwice(apiRequestStub);
+      sinon.assert.calledWith(apiRequestStub, 'get', '/projects', newPayload);
     });
+    
     it('should fetch project data again on user change.', function () {
-      
+      const user = { id: '1' };
+      wrapper.setState({ loading: false });
+      wrapper.setProps({ user });
+      sinon.assert.calledTwice(apiRequestStub);
+      sinon.assert.calledWith(apiRequestStub, 'get', '/projects', payload);
     });
     it('should not fetch project data again on any other prop change.', function () {
       
     });
   });
   describe('without initial load complete', function () {
-    it('should not fetch project data on mount.', function () {
+    let wrapper;
+    let apiRequestStub;
+    const payload = {
+      slug: 'owner/test-project',
+      include: 'avatar,background,owners'
+    };
     
+    before(function () {
+      apiRequestStub = sinon.stub(apiClient, 'request').callsFake(function (method, url, payload) {
+        return Promise.resolve([]);
+      }); 
+      sinon.stub(Split, 'load').callsFake(() => Promise.resolve([]));
     });
+    
+    after(function () {
+      apiRequestStub.restore();
+      Split.load.restore();
+    })
+    
+    beforeEach(function () {
+      context.initialLoadComplete = false;
+      wrapper = mount(
+        <ProjectPageController
+          location={location}
+          params={params}
+        />,
+        { context }
+      );
+    });
+    
+    afterEach(function () {
+      apiRequestStub.resetHistory();
+    })
+    
+    it('should not fetch project data on mount.', function () {
+      sinon.assert.notCalled(apiRequestStub);
+    });
+    
     it('should fetch project data again on project change.', function () {
-      
+      wrapper.setState({ loading: false });
+      wrapper.setProps({
+        params: {
+          owner: 'someone',
+          name: 'another-project'
+        }
+      });
+      const newPayload = {
+        slug: 'someone/another-project',
+        include: 'avatar,background,owners'
+      };
+      sinon.assert.calledOnce(apiRequestStub);
+      sinon.assert.calledWith(apiRequestStub, 'get', '/projects', newPayload);
     });
+    
     it('should not fetch project data again on user change.', function () {
-      
+      const user = { id: '1' };
+      wrapper.setState({ loading: false });
+      wrapper.setProps({ user });
+      sinon.assert.notCalled(apiRequestStub);
     });
+    
     it('should not fetch project data again on any other prop change.', function () {
       
     });
