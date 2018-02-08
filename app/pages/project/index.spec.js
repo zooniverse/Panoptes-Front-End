086c96315784de1d938334d9c7ecf4120e673f09
Jim O'Donnell <jim@zooniverse.org>
2018-02-08 11:43:50 +0000

    Set out some expectations for project loading

---
+++ app/pages/project/index.spec.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import assert from 'assert';
+import sinon from 'sinon';
+import ProjectPageController from './';
+
+describe('ProjectPageController', function () {
+  describe('with initial load complete', function () {
+    it('should fetch project data on mount.', function () {
+    
+    });
+    it('should fetch project data again on project change.', function () {
+      
+    });
+    it('should fetch project data again on user change.', function () {
+      
+    });
+    it('should not fetch project data again on any other prop change.', function () {
+      
+    });
+  });
+  describe('without initial load complete', function () {
+    it('should not fetch project data on mount.', function () {
+    
+    });
+    it('should fetch project data again on project change.', function () {
+      
+    });
+    it('should not fetch project data again on user change.', function () {
+      
+    });
+    it('should not fetch project data again on any other prop change.', function () {
+      
+    });
+  })
+});
