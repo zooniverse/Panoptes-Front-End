ca5c7e857d8af1e18d9134b390d19e82404287d5
mcbouslog <mcbouslog@gmail.com>
2018-02-15 13:28:08 -0600

    refactor user resources owned lists to link to lab and home

--- app/pages/admin/user-settings/resources.jsx
+++ app/pages/admin/user-settings/resources.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import apiClient from 'panoptes-client/lib/api-client';
+import LAB_APP_URL from '../../../lib/lab-app-url';
 
 class Resources extends Component {
   constructor(props) {
@@ -32,8 +33,16 @@ class Resources extends Component {
   }
 
   resourceLink(resource) {
+    let labUrl = '/lab';
+    if (this.props.type === 'projects') {
+      labUrl = `/lab/${resource.id}`;
+    } else if (this.props.type === 'organizations') {
+      labUrl = `${LAB_APP_URL}/organizations/${resource.id}`;
+    }
     return (
       <li key={resource.id}>
+        <a href={labUrl}>{resource.id}</a>
+        {' - '}
         <Link to={`/${this.props.type}/${resource.slug}`}>
           {resource.display_name}
         </Link>
@@ -45,9 +54,12 @@ class Resources extends Component {
     return (
       <div>
         <h3>{this.props.type.charAt(0).toUpperCase() + this.props.type.slice(1)}</h3>
-        {this.state.resources.length > 0 ? <ul>
-          {this.state.resources.map(resource => this.resourceLink(resource))}
-        </ul> : <p>None</p>}
+        {this.state.resources.length > 0 ? <div>
+          <span>links = &apos;lab - home&apos;</span>
+          <ul>
+            {this.state.resources.map(resource => this.resourceLink(resource))}
+          </ul>
+        </div> : <p>None</p>}
       </div>
     );
   }
