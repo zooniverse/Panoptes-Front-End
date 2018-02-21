743b7abefc3282083c687b6f3cc6a8517fc6f566
Jim O'Donnell <jim@eatyourgreens.org.uk>
2018-02-21 11:49:49 +0000

    Reduce workflow request page size (#4370)
    * Rewrite getWorkflowsInOrder in ES6
    
    * Reduce workflow request size
    Use recursive, paged API requests.
    Reduce the default page size to 20.
    
    * Always return allWorkflows from the promise chain
    
    * Use meta.next_page to page through the query

---
+++ app/lib/get-workflows-in-order.js
@@ -0,0 +1,51 @@
+function getWorkflowsInOrder(project, query) {
+  query = query || {};
+  const order = (project.configuration && project.configuration.workflow_order) ?
+    project.configuration.workflow_order :
+    [];
+
+  // TODO remove default page_size once pagination solution implemented
+  query.page_size = query.page_size || 20;
+
+  function getAllWorkflows(query) {
+    let allWorkflows = [];
+    return getWorkflows(query, 1);
+
+    function getWorkflows(query, page) {
+      return project.get('workflows', Object.assign({}, query, { page }))
+        .then((workflows) => {
+          allWorkflows = allWorkflows.concat(workflows);
+          const meta = workflows[0] ? workflows[0].getMeta() : null;
+          if (meta && meta.next_page) {
+            return getWorkflows(query, meta.next_page);
+          }
+        })
+        .catch((error) => {
+          console.info(error);
+        })
+        .then(() => Promise.resolve(allWorkflows));
+    }
+  }
+
+  return getAllWorkflows(query).then((workflows) => {
+    const workflowsByID = {};
+    workflows.forEach((workflow) => { workflowsByID[workflow.id] = workflow; });
+
+    const workflowsInOrder = order.map(workflowID => workflowsByID[workflowID]).filter(Boolean);
+
+    // Append any workflows that exist but do not appear in the order.
+    workflows.forEach((workflow) => {
+      if (workflowsInOrder.indexOf(workflow) === -1) {
+        workflowsInOrder.push(workflow);
+      }
+    });
+
+    return workflowsInOrder;
+  })
+  .catch((error) => {
+    console.info(error);
+    return Promise.resolve([]);
+  });
+}
+
+export default getWorkflowsInOrder;
