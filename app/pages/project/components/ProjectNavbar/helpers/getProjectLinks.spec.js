4702dd822e46a324cab11d61b6bb8f6c9768fbe1
Sarah Allen <sarah@zooniverse.org>
2018-02-21 13:42:12 -0600

    Update tests

--- app/pages/project/components/ProjectNavbar/helpers/getProjectLinks.spec.js
+++ app/pages/project/components/ProjectNavbar/helpers/getProjectLinks.spec.js
@@ -14,14 +14,14 @@ import {
 
 
 describe('getProjectLinks', function() {
-  it('uses the project slug in building the navbar links', function() {
+  it('should use the project slug in building the navbar links', function() {
     const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: projectOwnerUser });
     Object.keys(navLinks).forEach((link) => {
       expect(navLinks[link].url.includes(projectWithoutRedirect.slug));
     });
   });
 
-  it('returns about, classify, talk, and collections set of navbar links', function() {
+  it('should return about, classify, talk, and collections set of navbar links', function() {
     const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: null });
     expect(Object.keys(navLinks).indexOf('about') > -1).to.be.true;
     expect(Object.keys(navLinks).indexOf('classify') > -1).to.be.true;
@@ -30,45 +30,45 @@ describe('getProjectLinks', function() {
   });
 
   describe('without a user', function() {
-    it('does not return a recents link', function() {
+    it('should not return a recents link', function() {
       const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: null });
       expect(Object.keys(navLinks).indexOf('recents') > -1).to.be.false;
     });
 
-    it('does not return a lab link', function() {
+    it('should not return a lab link', function() {
       const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: null });
       expect(Object.keys(navLinks).indexOf('lab') > -1).to.be.false;
     });
 
-    it('does not return an admin link', function() {
+    it('should not return an admin link', function() {
       const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: null });
       expect(Object.keys(navLinks).indexOf('admin') > -1).to.be.false;
     });
   });
 
   describe('with a user', function() {
-    it('returns a recents link', function() {
+    it('should return a recents link', function() {
       const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: randomUser });
       expect(Object.keys(navLinks).indexOf('recents') > -1).to.be.true;
     });
 
-    it('does not return a lab link if the user is not the project owner or a collaborator', function() {
+    it('should not return a lab link if the user is not the project owner or a collaborator', function() {
       const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: randomUser });
       expect(Object.keys(navLinks).indexOf('lab') > -1).to.be.false;
     });
 
-    it('does not return an admin page link if the user is not an admin', function() {
+    it('should not return an admin page link if the user is not an admin', function() {
       const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: randomUser });
       expect(Object.keys(navLinks).indexOf('admin') > -1).to.be.false;
     });
 
     describe('who have a project role', function() {
-      it('returns the lab link if the user is the project owner', function() {
+      it('should return the lab link if the user is the project owner', function() {
         const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: projectOwnerUser });
         expect(Object.keys(navLinks).indexOf('lab') > -1).to.be.true;
       });
 
-      it('returns the lab link if the user is a project collaborator', function() {
+      it('should return the lab link if the user is a project collaborator', function() {
         const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: projectCollabUser });
         expect(Object.keys(navLinks).indexOf('lab') > -1).to.be.true;
       });
@@ -83,7 +83,7 @@ describe('getProjectLinks', function() {
         apiClient.params.admin = false;
       });
 
-      it('returns the admin link if the user is an admin', function() {
+      it('should return the admin link if the user is an admin', function() {
         const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow, user: adminUser });
         expect(Object.keys(navLinks).indexOf('admin') > -1).to.be.true;
       });
@@ -91,22 +91,22 @@ describe('getProjectLinks', function() {
   });
 
   describe('a project with a redirect', function() {
-    it('resets the classify link to the redirected project\'s classify page', function() {
+    it('should reset the classify link to the redirected project\'s classify page', function() {
       const navLinks = getProjectLinks({ project: projectWithRedirect, projectRoles, workflow, user: randomUser });
       expect(navLinks.classify.url.includes(projectWithRedirect.redirect)).to.be.true;
       expect(navLinks.classify.isExternalLink).to.be.true;
     });
 
-    it('does not return the about link', function() {
+    it('should not return the about link', function() {
       const navLinks = getProjectLinks({ project: projectWithRedirect, projectRoles, workflow, user: randomUser });
       expect(Object.keys(navLinks).indexOf('about') > -1).to.be.false;
     });
   });
 
-  describe('a project without a workflow and without a redirect', function() {
-    it('does not return the classify link', function() {
+  describe('a project without a workflow', function() {
+    it('should return the classify link with disabled: true', function() {
       const navLinks = getProjectLinks({ project: projectWithoutRedirect, projectRoles, workflow: null, user: null });
-      expect(Object.keys(navLinks).indexOf('classify') > -1).to.be.false;
+      expect(navLinks.classify.disabled).to.be.true;
     });
   });
 });
