c16c66303b9f925194d885a88e7245c704b20640
Marten Veldthuis <marten@veldthuis.com>
2018-02-27 17:50:46 +0000

    Indent 2 spaces instead of 4

--- app/lib/classification-queue.spec.js
+++ app/lib/classification-queue.spec.js
@@ -4,26 +4,26 @@ import FakeLocalStorage from '../../test/fake-local-storage';
 import { FakeApiClient } from '../../test/fake-api-client';
 
 describe('ClassificationQueue', function() {
-    it('sends classifications to the backend', function() {
-        let apiClient = new FakeApiClient();
-        let storage = new FakeLocalStorage();
+  it('sends classifications to the backend', function() {
+    let apiClient = new FakeApiClient();
+    let storage = new FakeLocalStorage();
 
-        let classificationData = {annotations: [], metadata: {}};
-        let classificationQueue = new ClassificationQueue(storage, apiClient);
-        classificationQueue.add(classificationData);
+    let classificationData = {annotations: [], metadata: {}};
+    let classificationQueue = new ClassificationQueue(storage, apiClient);
+    classificationQueue.add(classificationData);
 
-        assert.equal(apiClient.saves.length, 1);
-    });
+    assert.equal(apiClient.saves.length, 1);
+  });
 
-    it('keeps classifications in localStorage if backend fails', function() {
-        let apiClient = new FakeApiClient({canSave: () => { return false; }});
-        let storage = new FakeLocalStorage();
+  it('keeps classifications in localStorage if backend fails', function() {
+    let apiClient = new FakeApiClient({canSave: () => { return false; }});
+    let storage = new FakeLocalStorage();
 
-        let classificationData = {annotations: [], metadata: {}};
-        let classificationQueue = new ClassificationQueue(storage, apiClient);
-        classificationQueue.add(classificationData);
+    let classificationData = {annotations: [], metadata: {}};
+    let classificationQueue = new ClassificationQueue(storage, apiClient);
+    classificationQueue.add(classificationData);
 
-        assert.equal(apiClient.saves.length, 0);
-        assert.equal(classificationQueue.length(), 1);
-    });
+    assert.equal(apiClient.saves.length, 0);
+    assert.equal(classificationQueue.length(), 1);
+  });
 });
--- test/fake-local-storage.js
+++ test/fake-local-storage.js
@@ -1,32 +1,32 @@
 class FakeLocalStorage {
-    constructor() {
-        this.storage = {};
-    }
+  constructor() {
+    this.storage = {};
+  }
 
-    getItem(key) {
-        if (key in this.storage) {
-            return this.storage[key];
-        } else {
-            return null;
-        }
+  getItem(key) {
+    if (key in this.storage) {
+      return this.storage[key];
+    } else {
+      return null;
     }
+  }
 
-    setItem(key, value) {
-        this.storage[key] = value.toString();
-    }
+  setItem(key, value) {
+    this.storage[key] = value.toString();
+  }
 
-    removeItem(key) {
-        delete this.storage[key];
-    }
+  removeItem(key) {
+    delete this.storage[key];
+  }
 
-    get length() {
-        return Object.keys(this.storage).length;
-    }
+  get length() {
+    return Object.keys(this.storage).length;
+  }
 
-    key(i) {
-        let keys = Object.keys(this.storage);
-        return keys[i] || null;
-    }
+  key(i) {
+    let keys = Object.keys(this.storage);
+    return keys[i] || null;
+  }
 }
 
 export default FakeLocalStorage;
