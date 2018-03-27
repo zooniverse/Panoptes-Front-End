a6f6f01219df4ba7feb4884775bc9148d540082b
Sarah Allen <sarah@zooniverse.org>
2018-03-27 15:29:08 -0500

    Fix tests

--- app/pages/project/classify.spec.js
+++ app/pages/project/classify.spec.js
@@ -9,10 +9,9 @@ describe('ProjectClassifyPage', function () {
   let wrapper;
   let project = {};
   let apiUser = {id: 123};
-  let storage = new FakeLocalStorage();
 
   before(function () {
-    wrapper = shallow(<ProjectClassifyPage project={project} storage={storage} />);
+    wrapper = shallow(<ProjectClassifyPage project={project} />);
   });
 
   it('renders when not logged in', function () {
