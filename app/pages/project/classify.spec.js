5c18923bdc15490c43d45c5e4fbf8889143a6a45
Sarah Allen <sarah@zooniverse.org>
2018-03-27 09:48:21 -0500

    Fix broken test

--- app/pages/project/classify.spec.js
+++ app/pages/project/classify.spec.js
@@ -2,7 +2,6 @@ import React from 'react';
 import assert from 'assert';
 import { shallow } from 'enzyme';
 
-import { FakeApiClient } from '../../../test/fake-api-client';
 import { ProjectClassifyPage } from './classify';
 import FinishedBanner from './finished-banner';
 
@@ -10,10 +9,9 @@ describe('ProjectClassifyPage', function () {
   let wrapper;
   let project = {};
   let apiUser = {id: 123};
-  let storage = new FakeLocalStorage();
 
   before(function () {
-    wrapper = shallow(<ProjectClassifyPage project={project} storage={storage} />);
+    wrapper = shallow(<ProjectClassifyPage project={project} />);
   });
 
   it('renders when not logged in', function () {
