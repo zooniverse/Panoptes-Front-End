574e6e84cc7f1858c4615508479d813bfd9f6f58
Simone Duca <simone@zooniverse.org>
2018-03-29 13:55:24 +0100

    Fix tests

--- app/pages/admin/featured-project-toggle.spec.js
+++ app/pages/admin/featured-project-toggle.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import FeaturedProjectToggle from './featured-project-toggle';
@@ -8,12 +8,13 @@ let checkbox;
 let featuredChangeSpy;
 let wrapper;
 const project = {
-  featured: false
+  featured: false,
+  launch_approved: true
 };
 
 function setup() {
   featuredChangeSpy = sinon.spy();
-  wrapper = shallow(
+  wrapper = mount(
     <FeaturedProjectToggle
       handleProjectChange={featuredChangeSpy}
       project={project}
@@ -29,8 +30,33 @@ function tearDown() {
 describe('Featured Project Toggle', function () {
   beforeEach(setup);
   afterEach(tearDown);
+
+  it('should render the featured checkbox when the project is lauch_approved', function () {
+    expect(checkbox).to.have.lengthOf(1);
+  });
+
+  it('should render a checked featured check box when the project is lauch_approved and featured', function () {
+    wrapper.setProps({
+      project: {
+        featured: true,
+        launch_approved: true
+      }
+    });
+    expect(checkbox).to.have.lengthOf(1);
+  });
+
   it('should call handleProjectChange on checkbox change', function () {
     checkbox.simulate('change');
     expect(featuredChangeSpy.called).to.be.true;
+  });
+
+  it('should not render the featured checkbox when the project is not lauch_approved', function () {
+    wrapper.setProps({
+      project: {
+        featured: false,
+        launch_approved: false
+      }
+    });
+    expect(wrapper.find('input[name="featured"]')).to.have.lengthOf(0);
   });
 });
