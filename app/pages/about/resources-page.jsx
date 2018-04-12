5808199000259c04bebf73dc57bd6b7220525cb2
Sarah Allen <sarah@zooniverse.org>
2018-04-12 17:32:49 +0100

    Add content to the page. Fixed translation parsing.

--- app/pages/about/resources-page.jsx
+++ app/pages/about/resources-page.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import counterpart from 'counterpart';
 import { Markdown } from 'markdownz';
 
-export default function ResourcesPage() {
-  return (
-    <div>
-      <p>Placeholder</p>
-    </div>
-  );
+class ResourcesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Markdown>{counterpart('about.resources.title')}</Markdown>
+        <Markdown>{counterpart('about.resources.filler')}</Markdown>        
+        <Markdown>{counterpart('about.resources.introduction')}</Markdown>
+        <Markdown>{counterpart('about.resources.officialMaterials')}</Markdown>
+        <Markdown>{counterpart('about.resources.printables')}</Markdown>
+        <Markdown>{counterpart('about.resources.press')}</Markdown>
+        <Markdown>{counterpart('about.resources.tips')}</Markdown>
+        <Markdown>{counterpart('about.resources.listOne')}</Markdown>
+        <Markdown>{counterpart('about.resources.listTwo')}</Markdown>
+        <Markdown>{counterpart('about.resources.listThree')}</Markdown>
+      </div>
+    );
+  }
 }
+
+export default ResourcesPage;
