aad20fc9524ba0224980cf41296442d05443264f
Sam Blickhan <samantha@zooniverse.org>
2018-04-12 12:22:42 +0100

    Update acknowledgements.jsx
    Updated strings based on amendments to text of the Acknowledgements page.

--- app/pages/about/acknowledgements.jsx
+++ app/pages/about/acknowledgements.jsx
@@ -7,11 +7,16 @@ class Acknowledgements extends React.Component {
     return (
       <div>
         <Markdown>{counterpart('about.acknowledgements.title')}</Markdown>
+        <Markdown>{counterpart('about.acknowledgements.citation')}</Markdown>
         <Markdown>{counterpart('about.acknowledgements.instructions')}</Markdown>
         <Markdown>{counterpart('about.acknowledgements.supportText')}</Markdown>
         <Markdown>{counterpart('about.acknowledgements.publicationRequest')}</Markdown>
         <Markdown>{counterpart('about.acknowledgements.publicationShareForm')}</Markdown>
         <Markdown>{counterpart('about.acknowledgements.questions')}</Markdown>
+        <Markdown>{counterpart('about.acknowledgements.press')}</Markdown>
+        <Markdown>{counterpart('about.acknowledgements.projectURLs')}</Markdown>
+        <Markdown>{counterpart('about.acknowledgements.ZooURL')}</Markdown>
+        <Markdown>{counterpart('about.acknowledgements.enquiries')}</Markdown>
       </div>
     );
   }
