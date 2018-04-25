61160d4ee83a104dcaf5f9043dd1ff31b90ef642
Tim Lingard <tklingard@gmail.com>
2018-04-25 11:51:25 +0100

    Reduced maximum number of spiral arm segments to hopefully fix mobile

--- app/features/modelling/galaxy-builder/galaxyRegls.js
+++ app/features/modelling/galaxy-builder/galaxyRegls.js
@@ -111,7 +111,7 @@ export const drawSersic = r => r(Object.assign({}, baseObj, {
 }));
 
 export const drawSpiral = (r) => {
-  const maxPointCount = 300;
+  const maxPointCount = 240;
   const spiralArgs = {
     frag: `
       precision highp float;
