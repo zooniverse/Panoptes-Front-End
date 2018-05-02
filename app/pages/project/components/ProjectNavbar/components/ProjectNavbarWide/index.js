f4a4ac1bb373216b9b0316f2ae88bd5de050d347
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2018-05-02 15:29:38 +0100

    Remove react-rendered-size (#4567)
    * Use react-sizeme to handle dynamic navbar
    
    * Fix test command
    
    * Fix linter errors and test
    
    * Use native Array map method. Leave comment about lodash difference
    
    * Simple loading state before navigation is rendered
    
    * Fix targeted name for wrapper find call

--- app/pages/project/components/ProjectNavbar/components/ProjectNavbarWide/index.js
+++ app/pages/project/components/ProjectNavbar/components/ProjectNavbarWide/index.js
@@ -1,3 +1,6 @@
+import sizeMe from 'react-sizeme';
 import ProjectNavbarWide from './ProjectNavbarWide';
 
 export default ProjectNavbarWide;
+
+export const SizeAwareProjectNavbarWide = sizeMe()(ProjectNavbarWide);
