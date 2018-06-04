ec400906fe840fd5fec18dcc9cc43c00cdb55bfe
Jim O'Donnell <jim@zooniverse.org>
2018-06-04 15:10:46 +0100

    Spec basic classifier annotations state.

---
+++ app/classifier/classifier.spec.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import assert from 'assert';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
+
+describe('Classifier', function () {
+  describe('on mount', function () {
+    it('should initialise annotations and workflow history', function () {
+      
+    });
+    it('should preserve annotations from an incomplete classification', function () {
+      
+    });
+  });
+  describe('on receiving a new classification', function () {
+    it('should reset annotations and workflow history', function () {
+      
+    });
+    it('should preserve annotations from an incomplete classification', function () {
+      
+    });
+  });
+  describe('on receiving a new subject', function () {
+    it('should reset annotations and workflow history', function () {
+      
+    });
+  });
+  describe('on completing a classification', function () {
+    describe('with summaries enabled', function () {
+      it('should display a classification summary', function () {
+        
+      });
+    });
+    describe('with summaries disabled', function () {
+      it('should reset annotations and workflow history', function () {
+        
+      });
+    })
+  })
+});
