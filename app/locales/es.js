630130ffb1464e2dc9b31f55430f5085903f99ec
unknown <jari.haapatalo@gmail.com>
2018-07-03 19:24:39 +0200

    Copy team page content to es,it,nl locales

--- app/locales/es.js
+++ app/locales/es.js
@@ -189,5 +189,347 @@ export default {
   },
   workflowToggle: {
     label: 'Active'
+  },
+  about: {
+    team: {
+      nav: {
+        showAll: 'Show All',
+        oxford: 'Oxford',
+        chicago: 'The Adler Planetarium',
+        minnesota: 'Minnesota',
+        portsmouth: 'Portsmouth',
+        california: 'California',
+        hilo: 'Hilo',
+        alumni: 'Alumni'
+      },
+      content: {
+        header: {
+          showAll: 'Zooniverse Team'
+        },
+        adamMcMaster: {
+          title: 'Infrastructure Engineer',
+          bio: `Adam is responsible for managing the Zooniverse's web hosting infrastructure.,
+          He has a computer science degree and has worked in web hosting and development for many years.
+          He's also working on a degree in astronomy with the OU.`
+        },
+        alexBowyer: {
+          title: 'Web Science Architect',
+          bio: `Alex worked remotely for Zooniverse from his home in Northumberland.
+          He designed and ran Zooniverse experiments and developed infrastructure and front-end code. Alex
+          is a Dad of three, an improv player, a board gamer, and is running five half-marathons in 2016.`
+        },
+        alexWeiksnar: {
+          title: 'Developer',
+          bio: `Alex previously attended University of Miami, where he studied Psychology,
+          Biology, and English. Alex enjoys reading, coding, and sailing in his free time.`
+        },
+        aliSwanson: {
+          title: 'Researcher',
+          bio: `Ali spent most of her PhD chasing lions around the Serengeti. She finished
+          her PhD in Ecology, Evolution, and Behavior at the University of Minnesota in 2014,
+          and joined the Zooniverse as a Postdoc in Ecology and Citizen Science.`
+        },
+        amyBoyer: {
+          title: 'Developer',
+          bio: `Amy has been a Zooniverse developer at the Adler since November 2015. She holds a BS and
+          MS in computer science and has over a decade of industry experience. An aspiring astronomer as
+          a child, she couldn't be happier to have landed here.`  
+        },
+        andreaSimenstad: {
+          title: 'Developer',
+          bio: `Andrea joined Zooniverse as a developer at the University of Minnesota in 2015. When she is not captivated by code, she can be found enjoying snow on skis and exploring lakes by kayak. She graduated from Carleton College with a degree in Cognitive Science.`  
+        },
+        arfonSmith: {
+          title: 'Technical Lead',
+          bio: `Arfon was co-founder and Technical Lead of the Zooniverse for the first five years of the project.
+          Arfon served as Director of Citizen Science at the Adler Planetarium and co-lead of the Zooniverse until fall 2013.`
+        },
+        beckyRother: {
+          title: 'Visual Designer',
+          bio: `Becky joined the Zooniverse team as designer in 2017. With a background in mobile apps and product design and a degree in journalism, she's excited to use her powers for good with the Zooniverse team.`  
+        },
+        beverleyNewing: {
+          title: 'Web Developer Intern',
+          bio: `Beverley is an English and German literature graduate and worked as a Web Developer Intern at Zooniverse. In her spare time she's an avid fan of geese and enjoys motorbiking.`  
+        },
+        brianCarstensen: {
+          title: 'UX Developer',
+          bio: `Brian Carstensen moved from Chicago to Oxford and back again. Brian has a degree
+          in graphic design from Columbia College in Chicago, and worked in that field for a
+          number of years before finding a niche in web development.`  
+        },
+        brookeSimmons: {
+          title: 'Researcher',
+          bio: `Brooke is an astrophysicist studying black holes, galaxies, and how citizen
+          science can be applied to other non-traditional problems.`
+        },
+        camAllen: {
+          title: 'Developer',
+          bio: `Responsible for building the Zooniverse's API infrastructure.
+          Cam considers himself a music and fine wine connoisseur - others do not.
+          In his spare time he enjoys playing an obscure form of rugby.`  
+        },
+        chrisLintott: {
+          title: 'Zooniverse PI',
+          bio: `Astronomer and co-founder of both Galaxy Zoo and the Zooniverse that grew from it,
+          Chris is interested in how galaxies form and evolve, how citizen science can change the
+          world, and whether the Chicago Fire can get their act together.`
+        },
+        chrisSnyder: {
+          title: 'Project Manager',
+          bio: `Chris Snyder began working on the Zooniverse team in fall 2012 as a web developer. In July 2013,
+          he became the technical project manager. He received a degree in computer science from the University of Dayton.`  
+        },
+        christopherDoogue: {
+          title: 'Project Assistant',
+          bio: `Chris supported the Zooniverse Oxford team from 2014-16. He had been with the Department
+          of Astrophysics for over 2 years administratively supporting various projects. A former trained actor,
+          he has the ability to look like he's smiling in the face of adversity!`
+        },
+        colemanKrawczyk: {
+          title: 'Data Scientist',
+          bio: `Coleman is helping to create new data analysis and visualization tools for existing Zooniverse
+          projects as well as identifying new projects within the University of Portsmouth. He received
+          his PhD in astrophysics from Drexel University.`
+        },
+        darrenMcRoy: {
+          title: 'Community Manager',
+          bio: `Darren (DZM) served as a liaison to the Zooniverse community and assisted with strategic
+          content for projects. A Northwestern University graduate in journalism, he is also a golf addict,
+          amateur author, and hopeless animal lover.`
+        }, 
+        darrylWright: {
+          title: 'Researcher',
+          bio: 'Darryl is doing machine learning research with the Zooniverse, based in Oxford.'
+        },
+        grantMiller: {
+          title: 'Communications Lead',
+          bio: `A former exoplanetary scientist, Grant is now responsible for communicating with our volunteers and researchers. He is also behind the Zooniverse's presence on
+          social media and publishes the Daily Zooniverse blog.`
+        },
+        hannahSewell: {
+          title: 'Web Development placement student',
+          bio: `Hannah is a PhD student in plant genetics from The University of Sheffield.
+          She is visiting Zooniverse on a placement to learn web development for a few months and is definitely not an intern.`
+        },
+        helenSpiers: {
+          title: 'Researcher',
+          bio: `After finishing a PhD studying the molecular basis of human brain development, Helen joined the Zooniverse as a Postdoc in citizen science and medical research.`  
+        },
+        gregHines: {
+          title: 'Data Scientist',
+          bio: `Greg used machine learning and statistics to help projects analyse their volunteer
+          classifications. He has a PhD in Computer Science from University of Waterloo, Canada.
+          Greg loves to eat pancakes with real maple syrup and run ultramarathons.`
+    
+        },
+        heathVanSingel: {
+          title: 'Designer',
+          bio: `Heath was the UX/UI Designer at the Zooniverse where he worked to craft thoughtful and
+          engaging user experiences. Apart from design, Heath also enjoys illustration, a good sci-fi or fantasy
+          novel, and exploring his new home in the Pacific Northwest.`
+        }, 
+        hughDickinson: {
+          title: 'Researcher',
+          bio: `Hugh is a postdoc studying the Zooniverse.`
+        },
+        jamesArtz: {
+          title: 'Developer',
+          bio: `James joined the Zooniverse team in 2017. Before discovering his passion for code,
+          he received a Ph.D in Mediterranean Archaeology from SUNY Buffalo, focusing on ancient Greece.
+          In his free time he enjoys wood working and the ancient art of fermentation.`    
+        },
+        jimODonnell: {
+          title: 'UX Developer',
+          bio: `Professional cynic but his heart's not in it. Web developer for the Zooniverse,
+          Web Standards organiser, Amnesty UK activist.`  
+        },
+        jenGupta: {
+          title: 'Educator',
+          bio: `Jen works on Galaxy Zoo education in her role as the SEPnet/Ogden Outreach Officer
+          for the Institute of Cosmology and Gravitation, and has a PhD in astrophysics. Active
+          galactic nuclei were her first love.`
+        },
+        jordanMartin: {
+          title: 'Visual Designer',
+          bio: `Jordan joined at the Adler Planetarium as Zooniverse's Visual Designer
+          starting in 2015. She spent her time making our user interfaces beautiful,
+          engaging, and a rewarding experience. At home she spends time with her
+          two cats and many houseplants.`
+        },
+        julieFeldt: {
+          title: 'Educator',
+          bio: `Julie has a background in space physics and museum studies. She works on educational
+          experiences, and Skype in the Classroom lessons for the Zooniverse. She loves to run, mostly
+          to compensate for her love of cupcakes and chocolate.`
+        },
+        karenMasters: {
+          title: 'Researcher',
+          bio: `Karen is an astronomer and the Project Scientist of Galaxy Zoo, which she's been
+          involved in as a researcher since 2008. She's particularly interested in the evolution of
+          spiral galaxies.`
+        },
+        
+        kellyBorden: {
+          title: 'Educator',
+          bio: `With a background in museum education, Kelly joined the Zooniverse in 2011 to bring
+          an educator's perspective and spread the word amongst teachers and students. She's fond of
+          several C's: chocolate, cats, coffee, and more chocolate.`
+        }, 
+        kyleWillett: {
+          title: 'Researcher',
+          bio: `Kyle is an astronomer who studies galaxies, masers, and black holes (although he's
+          been getting more into data science). He worked on both Galaxy Zoo and Radio Galaxy Zoo,
+          and developed advanced tools for the volunteers.`
+        },
+        lauraTrouille: {
+          title: 'Zooniverse co-I',
+          bio: `In July 2015, Laura became Director of Citizen Science at the Adler Planetarium and
+          co-I of the Zooniverse. While earning her Ph.D. in 2010 studying galaxy evolution,
+          she embodied cosmic collisions as a roller derby queen aptly named ‘The Big Bang’.`
+        },
+        lauraWhyte: {
+          title: 'Adler Planetarium Zooniverse Director',
+          bio: `Laura was the Director of Citizen Science at Adler Planetarium and co-lead of the
+          Zooniverse from fall 2013 to summer 2015.`
+
+        },
+        lucyFortson: {
+          title: 'University of Minnesota Zooniverse Director',
+          bio: `Associate Professor of Physics and Astronomy at the University of Minnesota. One of the Zooniverse
+          co-founders, she helps with project wrangling (aka management) and strategic planning while trying
+          to squeeze a bit of galaxy evolution science in.`
+        },
+        mariamSaeedi: {
+          title: 'Web Developer Intern',
+          bio: `Mariam is the 2017 Web Development Intern at Zooniverse.
+          She is proficient with HTML and CSS and is currently learning JavaScript and Rails.`
+        },
+        markBouslog: {
+          title: 'Developer',
+          bio: `Mark is a front-end web developer for the Zooniverse team at the Adler Planetarium. Joining in
+          November 2015 from a career in accounting, he’s thrilled to focus his number crunching to 1’s and 0’s
+          and is constantly inspired and humbled by the power of programming, citizen science and coffee.`
+        },
+        martenVeldthuis: {
+          title: 'Developer',
+          bio: `Marten joined the development team in 2015. He has a degree in
+          computer science and enjoys crafting maintainable software. Marten learned about orbital
+          mechanics from Kerbal Space Program, and feels like he's therefore basically an astrophysicist.`
+        },
+        megSchwamb: {
+          title: 'Researcher',
+          bio: `Meg is a planetary scientist and astronomer involved in the Planet Four projects,
+          and Comet Hunters for which she is project scientist. She studies small bodies in the
+          Solar System, exoplanets, and Mars. Meg has a fondness for baking, soccer, and champagne.`
+        },
+        melanieBeck: {
+          title: 'Researcher',
+          bio: `A PhD student at the University of Minnesota, Melanie works on combining machine learning
+          with volunteer votes for classification tasks on the Galaxy Zoo project with the goal of classifying
+          *billions* of galaxies that future telescopes will see.`  
+        },
+        michaelParrish: {
+          title: 'Rails/Backend Developer',
+          bio: `Software developer at the Zooniverse. He pwns databases daily. Dog, fishing, snakes, and bourbon.`  
+        },
+        noahMalmed: {
+          title: 'Mobile App Developer',
+          bio: `Noah mainly has a background in mobile app development as well as some firmware and backend experience. 
+          Outside of work, Noah enjoys cooking, baking and climbing.`
+        },
+        perryRoper: {
+          title: 'Developer',
+          bio: 'Perry was a developer with the Zooniverse, based in Oxford.'
+        },
+        rebeccaSmethurst: {
+          title: 'Researcher',
+          bio: `Becky is an astrophysicist working towards her doctorate in Oxford. She is
+          interested in how galaxies change over time and how we can track this evolution.
+          Happy-go-lucky about outreach, the Zooniverse and everything.`
+        },
+        robinSchaaf: {
+          title: 'Mobile App Developer',
+          bio: `Robin has a background in web development for educational institutions.  In her free time she enjoys board games and playing bass guitar.`
+        },
+        robertSimpson: {
+          title: 'Developer',
+          bio: `Robert was one of the founding developers of the Zooniverse, based in Oxford.
+          If you can keep your head when all around you are losing theirs, you are probably
+          Rob Simpson.`
+        },
+        rogerHutchings: {
+          title: 'Developer',
+          bio: `Roger builds front end interfaces for the Zooniverse, and enjoys coding, music and rebuilding
+          motorbikes in his spare time.`
+        },
+        samanthaBlickhan: {
+          title: 'IMLS Postdoctoral Fellow',
+          bio: `Samantha Blickhan is the IMLS Postdoctoral Fellow in the Department of Citizen Science at the Adler Planetarium, working on transcription projects for the Zooniverse. She received her Ph.D. in Musicology from Royal Holloway, University of London, with a thesis on the palaeography of British song notation in the 12th and 13th centuries. Her research interests include music and perception, and their relationships with writing systems, technology and pedagogy.`
+        },
+        sandorKruk: {
+          title: 'Researcher',
+          bio: `Sandor is a graduate student working on his PhD in Astrophysics at Oxford.
+          He is looking at how galaxies evolve using data from Galaxy Zoo and enjoys stargazing,
+          using telescopes, and dancing in his free time.`
+        },
+        sarahAllen: {
+          title: 'Developer',
+          bio: `Sarah Allen is a front-end web developer for the Zooniverse team at the Adler
+          Planetarium starting in September 2014. She also has a degree in music and enjoys cooking
+          and live concerts in her free time.`
+        },
+        saschaIshikawa: {
+          title: 'Developer',
+          bio: `Sascha joined the Zooniverse team in November 2013 as a front-end developer.
+          A former researcher and software developer at NASA Ames, he received degrees in both
+          computer science and mechanical engineering.`
+        },
+        shaunANoordin: {
+          title: 'Developer',
+          bio: `Shaun was raised by Nintendo consoles and somehow transformed his love
+          for video games into a love for creating interactive experiences. When not at
+          his PC playing games or reading comics, he's at his PC studying web design and
+          coding experimental apps.`
+        },
+        simoneDuca: {
+          title: 'Developer',
+          bio: `Simone joined the Zooniverse in 2015 as a frontend developer. In a previous life, he received a Phd in logic and philosophy
+          from Bristol. After moving "Up North", he joined another worthy cause in 2018.`
+        },
+        stuartLynn: {
+          title: 'Developer',
+          bio: `Stuart arrived at the Adler as a developer in July 2011, having been one of the founding
+          developers at Oxford. He studied mathematical physics at Edinburgh University and received his doctorate
+          in astrophysics from Royal Observatory Edinburgh. Data analysis maestro and web developer.`
+        },
+        steveRaden: {
+          title: 'Developer',
+          bio: 'Steve Raden was a developer at the Adler in Chicago.'
+        },
+        veronicaMaidel: {
+          title: 'Data Scientist',
+          bio: `Veronica is a Data Scientist who worked on discovering patterns in Zooniverse data, by manipulating
+          it and using it to create machine learning models. She received a PhD in Information Science and Technology
+          from Syracuse University.`
+        },
+        victoriaVanHyning: {
+          title: 'Researcher',
+          bio: `Victoria is a British Academy postdoctoral fellow in English at Oxford, and is the humanities PI
+          at Zooniverse. She works on Science Gossip, Annotate, and Shakespeare's World and more. Coffee is her lifeblood.`
+        },
+        willGranger: {
+          title: 'Developer',
+          bio: `Will joined the Adler Planetarium in 2016 as a front-end developer for the Zooniverse.
+          In the past, he has taught English overseas, toured in bands, and helped open a music venue in Alabama.`
+        },
+        zachWolfenbarger: {
+          title: 'Developer',
+          bio: `Zach is a software developer. He was a molecular biologist, but then the lab needed some code
+          to be written and the die was cast. He’s also in a couple of bands and can be found playing shows at bars
+          and comic conventions all over the midwest.`
+        }
+      }
+    }
   }
 };
