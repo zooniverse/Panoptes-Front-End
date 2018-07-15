25007baee4288cefff1fae90f8229fa5b3eb919c
jarihaa <jari.haapatalo@gmail.com>
2018-07-15 13:25:25 +0200

    Add tests

--- app/pages/about/publications-page.jsx
+++ app/pages/about/publications-page.jsx
@@ -117,7 +117,6 @@ export default class PublicationsPage extends React.Component {
                             })
                           }
                         </ul>
-                       
                       </div>
                     );
                   })
