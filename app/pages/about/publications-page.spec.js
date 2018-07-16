06b4ccae2cc2a25dfa1ec4914e89361d3521fea2
jarihaa <jari.haapatalo@gmail.com>
2018-07-16 19:41:43 +0200

    Publication list test

--- app/pages/about/publications-page.spec.js
+++ app/pages/about/publications-page.spec.js
@@ -2,18 +2,25 @@
 
 import React from 'react';
 import assert from 'assert';
-import { shallow } from 'enzyme';
 import PublicationsPage from './publications-page';
+import { shallow } from 'enzyme';
 
-describe('PublicationsPage', function () {
+describe('PublicationsPage', () => {
+
   it('renders without crashing', function () {
     shallow(<PublicationsPage />);
   });
 
-  it('renders side navigation', function () {
+  it('renders side navigation', () => {
     const wrapper = shallow(<PublicationsPage />);
     const navItems = wrapper.find('.side-bar-button');
     assert.notEqual(navItems.length, 0);
   });
 
+  it('renders publication lists when projects not null', () =>  {
+    const wrapper = shallow(<PublicationsPage />);
+    wrapper.setState({ projects: {} });
+    const publications = wrapper.find('.publications-list');
+    assert.notEqual(publications.length, 0);
+  });
 });
