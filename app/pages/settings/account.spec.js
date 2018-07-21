6c41642188084b11d0aa3200994021a0dab314a7
jarihaa <jari.haapatalo@gmail.com>
2018-07-21 12:00:00 +0200

    Add tests

---
+++ app/pages/settings/account.spec.js
@@ -0,0 +1,18 @@
+/* eslint prefer-arrow-callback: 0, func-names: 0, 'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx"] }] */
+
+import React from 'react';
+import assert from 'assert';
+import { shallow } from 'enzyme';
+import AccountInformationPage from './account';
+
+describe('AccountInformationPage', function () {
+ 
+  it('renders without crashing', function () {
+    const user = {
+      display_name: '',
+      credited_name: ''
+    }
+    shallow(<AccountInformationPage user={user} />);
+  });
+
+});
