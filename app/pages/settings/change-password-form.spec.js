c11034c8214bd66d4013ce55658ea3a7eaf02c14
jarihaa <jari.haapatalo@gmail.com>
2018-07-22 08:37:09 +0200

    Change password error message test

--- app/pages/settings/change-password-form.spec.js
+++ app/pages/settings/change-password-form.spec.js
@@ -1,10 +1,19 @@
 /* eslint prefer-arrow-callback: 0, func-names: 0, 'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx"] }] */
 import React from 'react';
 import { shallow } from 'enzyme';
+import assert from 'assert';
 import ChangePasswordForm from './change-password-form';
 
 describe('ChangePasswordForm', () => {
+ 
   it('renders without crashing', () => {
     shallow(<ChangePasswordForm />);
+  });
+
+  it('renders error message', () => {
+    const wrappper =  shallow(<ChangePasswordForm />);
+    wrappper.setState({ error: "Error message" });
+    const error = wrappper.find('.error');
+    assert.equal(error.length, 1);
   });
 });
