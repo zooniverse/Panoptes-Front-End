e1569ba2134c3481e129736a3917a152059ce73f
jarihaa <jari.haapatalo@gmail.com>
2018-07-22 09:31:23 +0200

    Submit buttons tests

--- app/pages/settings/change-password-form.spec.js
+++ app/pages/settings/change-password-form.spec.js
@@ -31,10 +31,23 @@ describe('ChangePasswordForm', () => {
     assert.equal(message.length, 1);
   });
 
-  it('renders dosent match message', () => {
+  it('renders doesnt match message', () => {
     const wrappper =  shallow(<ChangePasswordForm />);
     wrappper.setState({ new: 'newpassword', confirmation: 'notsamepass' });
     const message = wrappper.find('.error');
     assert.equal(message.length, 1);   
   });
+
+  it('renders disabled submit button', () => {
+    const wrappper =  shallow(<ChangePasswordForm />);
+    const button = wrappper.find('button');
+    assert.equal(button.prop('disabled'), true);
+  });
+
+  it('renders enabled submit button when form is valid', () => {
+    const wrappper =  shallow(<ChangePasswordForm />);
+    wrappper.setState({ old: 'oldpassword', new: 'newpassword', confirmation: 'newpassword'});
+    const button = wrappper.find('button');
+    assert.equal(button.prop('disabled'), false);  
+  })
 });
