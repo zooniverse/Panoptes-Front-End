09ab1947df20680300ca5287881236d23e1a24ef
jarihaa <jari.haapatalo@gmail.com>
2018-08-11 09:48:09 +0200

    Rename change password form file name to match component name

--- app/pages/settings/ChangePasswordForm.spec.js
+++ app/pages/settings/ChangePasswordForm.spec.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import assert from 'assert';
-import ChangePasswordForm from './change-password-form';
+import ChangePasswordForm from './ChangePasswordForm';
 
 describe('ChangePasswordForm', () => {
  
