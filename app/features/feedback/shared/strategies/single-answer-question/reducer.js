2e68883114491dcad34814d22e9656c2761fa375
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2018-08-23 11:55:32 +0100

    Use object.assign instead of lodash

--- app/features/feedback/shared/strategies/single-answer-question/reducer.js
+++ app/features/feedback/shared/strategies/single-answer-question/reducer.js
@@ -1,9 +1,7 @@
-import _ from 'lodash';
-
 function singleAnswerQuestionReducer(rule, annotations) {
   const answerId = annotations || -1
   const result = rule.answer === answerId.toString()
-  return _.assign({}, rule, {
+  return Object.assign({}, rule, {
     success: result
   });
 }
