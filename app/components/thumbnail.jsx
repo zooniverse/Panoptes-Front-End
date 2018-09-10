42c711aa66e8d9f723907e227b3774fc8c9d9fdc
srallen <srallen@users.noreply.github.com>
2018-09-10 04:18:25 -0500

    Do not compress gifs in Thumbnail component (#4871)

--- app/components/thumbnail.jsx
+++ app/components/thumbnail.jsx
@@ -74,6 +74,7 @@ Thumbnail.getThumbnailSrc = function getThumbnailSrc({ origin, width, height, sr
   if (!src) {
     return undefined;
   }
+  if (src.includes('.gif')) return src;
   let srcPath = src.split('//').pop();
   srcPath = srcPath.replace('static.zooniverse.org/', '');
   return (`${origin}/${width}x${height}/${srcPath}`);
