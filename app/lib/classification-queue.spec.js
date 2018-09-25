f58b9b1a96cc0c3fe59344ea1e10c824ae83b581
Jim O'Donnell <jim@zooniverse.org>
2018-09-25 13:53:44 +0100

    Add a test for slow network connections Stub resource save to return an unresolved Promise, then check that save is only called once for each classification added to the queue.

--- app/lib/classification-queue.spec.js
+++ app/lib/classification-queue.spec.js
@@ -1,7 +1,8 @@
-import assert from 'assert';
+import { expect } from 'chai';
+import sinon from 'sinon';
 import ClassificationQueue from './classification-queue';
 import FakeLocalStorage from '../../test/fake-local-storage';
-import { FakeApiClient } from '../../test/fake-api-client';
+import { FakeApiClient, FakeResource } from '../../test/fake-api-client';
 
 describe('ClassificationQueue', function() {
   it('sends classifications to the backend', function() {
@@ -12,7 +13,7 @@ describe('ClassificationQueue', function() {
     let classificationQueue = new ClassificationQueue(storage, apiClient);
     classificationQueue.add(classificationData);
 
-    assert.equal(apiClient.saves.length, 1);
+    expect(apiClient.saves).to.have.lengthOf(1);
   });
 
   it('keeps classifications in localStorage if backend fails', function() {
@@ -23,7 +24,28 @@ describe('ClassificationQueue', function() {
     let classificationQueue = new ClassificationQueue(storage, apiClient);
     classificationQueue.add(classificationData);
 
-    assert.equal(apiClient.saves.length, 0);
-    assert.equal(classificationQueue.length(), 1);
+    expect(apiClient.saves).to.have.lengthOf(0);
+    expect(classificationQueue.length()).to.equal(1);
+  });
+  describe('with a slow network connection', function () {
+    let apiClient;
+    let classificationQueue;
+    let saveSpy;
+    before(function () {
+      saveSpy = sinon.stub(FakeResource.prototype, 'save').callsFake(function() {
+        // stub save so that the API call never completes.
+        return new Promise(function (resolve, reject) {});
+      });
+      apiClient = new FakeApiClient();
+      classificationQueue = new ClassificationQueue(window.storage, apiClient);
+      classificationQueue.add({annotations: [], metadata: {}});
+      classificationQueue.add({annotations: [], metadata: {}});
+    });
+    after(function () {
+      saveSpy.restore();
+    });
+    it('saves each classification once', function () {
+      expect(saveSpy.callCount).to.equal(2);
+    });
   });
 });
