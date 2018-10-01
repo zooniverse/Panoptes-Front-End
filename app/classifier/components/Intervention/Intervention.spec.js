13768ac552b0a30a3715e42663401b2c80c33ad9
Jim O'Donnell <jim@zooniverse.org>
2018-10-01 17:04:32 +0100

    Add tests Test that the component renders a notification message, and that the checkbox updates user preferences.

---
+++ app/classifier/components/Intervention/Intervention.spec.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import Intervention from './Intervention';
+
+describe('Intervention', function () {
+  let wrapper;
+  const notifications = [{
+    data: {
+      message: 'Hello!'
+    }
+  }];
+  const { message } = notifications[0].data;
+  const user = {
+    id: 'a',
+    update: sinon.stub().callsFake(() => {
+      return { save: () => true };
+    })
+  };
+  before(function () {
+    wrapper = mount(
+      <Intervention
+        notifications={notifications}
+        user={user}
+      />);
+  });
+  it('should render', function () {
+    expect(wrapper).to.be.ok;
+  });
+  it('should show a notification message', function () {
+    expect(wrapper.contains(<p>{message}</p>)).to.be.true;
+  });
+  describe('opt-out checkbox', function () {
+    let optOut;
+    before(function () {
+      optOut = wrapper.find('input[type="checkbox"]');
+      optOut.simulate('change');
+    });
+    after(function () {
+      user.update.resetHistory();
+    });
+    it('should update the user when checked/unchecked', function () {
+      expect(user.update.callCount).to.equal(1);
+    });
+    it('should set the user opt-out preference', function () {
+      const changes = { intervention_notifications: true }
+      expect(user.update.calledWith(changes)).to.be.true;
+    })
+  });
+});
