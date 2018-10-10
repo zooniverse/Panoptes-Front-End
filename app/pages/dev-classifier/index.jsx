9ce585776000ca2d9f5c46c375ad2fdc58f478ee
Jim O'Donnell <jim@zooniverse.org>
2018-10-10 11:40:12 +0100

    Connect the dev classifier to the classify store Store the mock workflow and subjects. Create a new classification rather than using the mock data.

--- app/pages/dev-classifier/index.jsx
+++ app/pages/dev-classifier/index.jsx
@@ -7,6 +7,7 @@ import { ClassifierWrapper } from '../../classifier';
 import tasks from '../../classifier/tasks';
 import ClassificationViewer from './ClassificationViewer';
 import * as userInterfaceActions from '../../redux/ducks/userInterface';
+import * as classifierActions from '../../redux/ducks/classify';
 import { zooTheme } from '../../theme';
 
 export class DevClassifierPage extends React.Component {
@@ -22,16 +23,32 @@ export class DevClassifierPage extends React.Component {
   }
 
   componentDidCatch(error, info) {
-    console.log(error, info)
+    console.log(error, info);
     this.setState({ error, info });
   }
 
+  componentDidMount() {
+    const { actions } = this.props;
+    const workflow = mockData.classification._workflow;
+    actions.classify.setWorkflow(workflow);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { actions, project, user } = this.props;
+    const workflow = mockData.classification._workflow;
+    const subjects = mockData.classification._subjects
+    if (user !== prevProps.user) {
+      actions.classify.appendSubjects(subjects, workflow.id);
+      actions.classify.createClassification(project);
+    }
+  }
+
   reload() {
-    const workflow = this.props.classification._workflow;
+    const workflow = mockData.classification._workflow;
     const firstTask = workflow.tasks[workflow.first_task];
     const FirstTaskComponent = tasks[firstTask.type];
     const firstAnnotation = Object.assign({}, { task: workflow.first_task }, FirstTaskComponent.getDefaultAnnotation());
-    this.props.classification.update({
+    mockData.classification.update({
       annotations: [firstAnnotation],
       completed: false
     });
@@ -39,30 +56,37 @@ export class DevClassifierPage extends React.Component {
 
   render() {
     const { error, info } = this.state;
+    const { classification, workflow } = this.props;
     const classname = classNames({
       'classify-page--dark-theme': this.props.theme === zooTheme.mode.dark
     });
-    const [subject] = this.props.classification._subjects;
+    const [subject] = mockData.classification._subjects;
 
     return (
       <div className={classname}>
-        {!!error ?
-          <p>
+        {!!error &&
+          <div>
             {error.message}
             <hr/>
             <pre>{info.componentStack}</pre>
-          </p> :
+          </div>
+        }
+        {!error &&
+        !!classification &&
           <ClassifierWrapper
             className="classifier--dev"
             user={this.props.user}
             project={this.props.project}
-            workflow={this.props.classification._workflow}
+            workflow={workflow}
             preferences={this.props.preferences}
-            classification={this.props.classification}
+            classification={classification}
             subject={subject}
             onClickNext={this.reload}
           >
-            <ClassificationViewer classification={this.props.classification} />
+            <ClassificationViewer
+              classification={classification}
+              workflow={workflow}
+            />
           </ClassifierWrapper>
         }
       </div>
@@ -71,7 +95,15 @@ export class DevClassifierPage extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  theme: state.userInterface.theme
+  classification: state.classify.classification,
+  theme: state.userInterface.theme,
+  workflow: state.classify.workflow
 });
 
-export default connect(mapStateToProps)(DevClassifierPage);
+const mapDispatchToProps = dispatch => ({
+  actions: {
+    classify: bindActionCreators(classifierActions, dispatch)
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DevClassifierPage);
