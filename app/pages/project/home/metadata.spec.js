fac4bf7987362ed3ef729d39f8138e1134e6c8c5
mcbouslog <mcbouslog@gmail.com>
2018-10-10 15:52:43 -0500

    Revert mocha functions

--- app/pages/project/home/metadata.spec.js
+++ app/pages/project/home/metadata.spec.js
@@ -4,10 +4,10 @@ import { render } from 'enzyme';
 
 import ProjectMetadata from './metadata';
 
-describe('ProjectMetadata', () => {
+describe('ProjectMetadata', function() {
   let project;
 
-  before(() => {
+  before(function() {
     project = {
       classifiers_count: 0,
       classifications_count: 0,
@@ -19,25 +19,25 @@ describe('ProjectMetadata', () => {
     };
   });
 
-  describe('render', () => {
+  describe('render', function() {
     let wrapper;
-    before(() => {
+    before(function() {
       wrapper = render(<ProjectMetadata project={project} translation={project} />);
     });
 
-    it('renders stats', () => {
+    it('renders stats', function() {
       assert(wrapper.find('.project-metadata-stat').length, 4);
     });
 
-    it('renders 56% completeness', () => {
+    it('renders 56% completeness', function() {
       assert(wrapper.find('rect').first().prop('width'), 0.56);
     });
 
-    it('excludes TalkStatus by default', () => {
+    it('excludes TalkStatus by default', function() {
       assert.notEqual(wrapper.find('.project-home-page__talk-stat').length, 1);
     });
 
-    it('includes TalkStatus if showTalkStatus prop is true', () => {
+    it('includes TalkStatus if showTalkStatus prop is true', function() {
       wrapper = render(<ProjectMetadata project={project} translation={project} showTalkStatus={true} />);
       assert(wrapper.find('.project-home-page__talk-stat').length, 1);
     });
