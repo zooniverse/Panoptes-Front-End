b545565e3fc039c5f6f452f8678359181e971028
Jim O'Donnell <jim@eatyourgreens.org.uk>
2018-10-11 16:51:52 +0100

    Fix single answer feedback when the answer is 0 (#4983)
    * Update single answer feedback tests
    Change the correct answer test to test for the case where the answer is 0 (ie. falsy) but correct.
    Update the incorrect answer test to make sure we aren't being tripped up by 0 values.
    
    * Handle answers of 0 correctly
    Compare with -1 so that 0 returns true, when determining the answer ID.
    Rename annotations to answer for clarity.

--- app/features/feedback/shared/strategies/single-answer-question/reducer.js
+++ app/features/feedback/shared/strategies/single-answer-question/reducer.js
@@ -1,6 +1,6 @@
-function singleAnswerQuestionReducer(rule, annotations) {
-  const answerId = annotations || -1
-  const result = rule.answer === answerId.toString()
+function singleAnswerQuestionReducer(rule, answer) {
+  const answerId = (answer > -1) ? answer : -1;
+  const result = rule.answer === answerId.toString();
   return Object.assign({}, rule, {
     success: result
   });
