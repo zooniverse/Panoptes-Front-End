d99fdb9ab209eb3f10968a01e6cd0e1a6d266ce8
Mark Bouslog <mcbouslog@gmail.com>
2018-11-02 00:32:37 -0500

    Duplicate radial feedback

--- app/features/feedback/shared/strategies/index.js
+++ app/features/feedback/shared/strategies/index.js
@@ -1,8 +1,10 @@
+import column from './column';
 import dud from './dud';
 import singleAnswerQuestion from './single-answer-question';
 import radial from './radial';
 
 export default {
+  column,
   dud,
   singleAnswerQuestion,
   radial
