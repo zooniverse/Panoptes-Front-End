8c5008765c8ec6295ca0bc5908537428d1562715
Will Granger <twgranger@gmail.com>
2018-11-09 13:14:13 -0600

    Rewrite Survey Utility

---
+++ app/classifier/tasks/survey/utility.js
@@ -0,0 +1,12 @@
+function getQuestionIDs(task, choiceID) {
+  if (task.choices[choiceID].noQuestions) {
+    return [];
+  }
+  if (!(task.questionsMap && Object.keys(task.questionsMap).indexOf(choiceID) >= 0)) {
+    return task.questionsOrder;
+  }
+  return task.questionsMap[choiceID];
+}
+
+export default getQuestionIDs;
+export { getQuestionIDs };
