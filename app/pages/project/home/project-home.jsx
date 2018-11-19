5cf74e4c71875a2220e6f66e3ad648bbc79479e8
Jim O'Donnell <jim@zooniverse.org>
2018-11-19 12:30:46 +0000

    Check for active workflows on project home Don't show the call to action link if there are no active workflows.

--- app/pages/project/home/project-home.jsx
+++ app/pages/project/home/project-home.jsx
@@ -16,6 +16,7 @@ import ExternalLinksBlock from '../../../components/ExternalLinksBlock';
 
 const ProjectHomePage = (props) => {
   const projectIsNotRedirected = props.project && !props.project.redirect;
+  const canClassify = props.project.links.active_workflows && props.project.links.active_workflows.length > 0;
   const avatarSrc = props.researcherAvatar || '/assets/simple-avatar.png';
 
   const descriptionClass = classnames(
@@ -60,7 +61,7 @@ const ProjectHomePage = (props) => {
             <Link to={`/projects/${props.project.slug}/about`} className="project-home-page__button call-to-action__button call-to-action__button--learn-more">
               <Translate content="project.home.learnMore" />
             </Link>}
-          {showGetStartedLink &&
+          {canClassify && showGetStartedLink &&
             <VisibilitySplit splits={props.splits} splitKey='workflow.assignment' elementKey='link'>
               <Link
                 to={`/projects/${props.project.slug}/classify`}
