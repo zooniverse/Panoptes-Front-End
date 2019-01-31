941ec34dc9741c9be5a8122c941d6af7480ee4d0
Jim O'Donnell <jim@zooniverse.org>
2019-01-31 15:43:25 +0000

    Language Menu tests With translations, expect translated languages to be listed, enabled and checked if approved and in the project config.

--- app/pages/lab/translations/index.spec.js
+++ app/pages/lab/translations/index.spec.js
@@ -5,8 +5,10 @@ import mockPanoptesResource from '../../../../test/mock-panoptes-resource';
 import { TranslationsManager } from './';
 
 describe('TranslationsManager', function () {
-  let wrapper = shallow(<TranslationsManager />);
+  let wrapper;
+
   it('should render with default props', function () {
+    wrapper = shallow(<TranslationsManager />);
     expect(wrapper).to.be.ok;
   });
 
@@ -21,15 +23,12 @@ describe('TranslationsManager', function () {
         project: ['en']
       }
     };
-
-    before(function () {
-      wrapper = shallow(
-        <TranslationsManager
-          project={project}
-          translations={translations}
-        />
-      );
-    });
+    wrapper = shallow(
+      <TranslationsManager
+        project={project}
+        translations={translations}
+      />
+    );
 
     describe('the project primary language', function () {
       const languageMenu = wrapper.find('table');
@@ -45,6 +44,137 @@ describe('TranslationsManager', function () {
 
       it('should be disabled', function () {
         expect(languageInput.prop('disabled')).to.be.true;
+      });
+    });
+  });
+
+  describe('with translations', function () {
+    const project = mockPanoptesResource('project',
+    {
+      configuration: {},
+      primary_language: 'en'
+    });
+    const translations = {
+      languages: {
+        project: ['en', 'fr', 'es']
+      }
+    };
+    wrapper = shallow(
+      <TranslationsManager
+        project={project}
+        translations={translations}
+      />
+    );
+
+    describe('the project primary language', function () {
+      const languageMenu = wrapper.find('table');
+      const languageInput = languageMenu.find('input[type="checkbox"]').first();
+
+      it('should be listed', function () {
+        expect(languageInput).to.have.lengthOf(1);
+      });
+
+      it('should be checked', function () {
+        expect(languageInput.prop('checked')).to.be.true;
+      });
+
+      it('should be disabled', function () {
+        expect(languageInput.prop('disabled')).to.be.true;
+      });
+    });
+
+    describe('all translation languages', function () {
+      const languageMenu = wrapper.find('table');
+      const languageInputs = languageMenu.find('input[type="checkbox"]');
+
+      it('should be listed', function () {
+        expect(languageInputs).to.have.lengthOf(3);
+      });
+
+      describe('translated languages', function () {
+        const inputs = Array.from(languageInputs);
+        inputs.shift();
+        inputs.forEach(function (input) {
+          const language = input.props.value;
+          const checked = input.props.checked;
+          const disabled = input.props.disabled;
+          it(`${language} should not be checked`, function () {
+            expect(checked).to.be.false;
+          });
+
+          it(`${language} should not be disabled`, function () {
+            expect(disabled).to.be.false;
+          });
+        });
+      });
+    });
+  });
+
+  describe('with approved translations', function () {
+    const project = mockPanoptesResource('project',
+    {
+      configuration: {
+        languages: ['en', 'fr']
+      },
+      primary_language: 'en'
+    });
+    const translations = {
+      languages: {
+        project: ['en', 'fr', 'es']
+      }
+    };
+    wrapper = shallow(
+      <TranslationsManager
+        project={project}
+        translations={translations}
+      />
+    );
+
+    describe('approved languages', function () {
+      const languageMenu = wrapper.find('table');
+      const languageInputs = languageMenu.find('input[value="fr"]');
+
+      it('should be listed', function () {
+        expect(languageInputs).to.have.lengthOf(1);
+      });
+
+      describe('translated languages', function () {
+        languageInputs.forEach(function (input) {
+          const language = input.prop('value');
+          const checked = input.prop('checked');
+          const disabled = input.prop('disabled');
+          it(`${language} should be checked`, function () {
+            expect(checked).to.be.true;
+          });
+
+          it(`${language} should not be disabled`, function () {
+            expect(disabled).to.be.false;
+          });
+        });
+      });
+    });
+
+    describe('pending languages', function () {
+      const languageMenu = wrapper.find('table');
+      const languageInputs = languageMenu.find('input[value="es"]');
+
+      it('should be listed', function () {
+        expect(languageInputs).to.have.lengthOf(1);
+      });
+
+      describe('translated languages', function () {
+        languageInputs.forEach(function (input) {
+          const language = input.prop('value');
+          const checked = input.prop('checked');
+          const disabled = input.prop('disabled');
+          it(`${language} should not be checked`, function () {
+            expect(checked).to.be.false;
+          });
+
+          it(`${language} should not be disabled`, function () {
+            expect(disabled).to.be.false;
+          });
+        });
       });
     });
   });
