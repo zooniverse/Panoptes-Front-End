126d87f3c0a799b478eb7f771d99b581a1f3bb20
mcbouslog <mcbouslog@gmail.com>
2019-02-18 15:48:50 -0600

    Select related elements, remove cruft

--- app/components/svg-image.jsx
+++ app/components/svg-image.jsx
@@ -1,26 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const FILTERS = {
-  invert: "url('#svg-invert-filter')"
-};
-
-const INVERT =
-  `<svg style="position: fixed; right: 100%; top: 100%; visibility: hidden;">
-    <defs>
-      <filter id="svg-invert-filter" color-interpolation-filters="sRGB">
-        <feComponentTransfer>
-          <feFuncR type="table" tableValues="1 0"/>
-          <feFuncG type="table" tableValues="1 0"/>
-          <feFuncB type="table" tableValues="1 0"/>
-        </feComponentTransfer>
-      </filter>
-    </defs>
-  </svg>`;
-
-
 class SVGImage extends React.Component {
-
   constructor() {
     super();
     this.fixWeirdSize = this.fixWeirdSize.bind(this);
@@ -46,31 +27,16 @@ class SVGImage extends React.Component {
     }
   }
 
-  filterFinder() {
-    if (this.props.modification.invert) {
-      if (!document.getElementById('svg-invert-filter')) {
-        document.body.insertAdjacentHTML('afterbegin', INVERT);
-      }
-      return { filter: FILTERS.invert };
-    }
-    return {};
-  }
-
   render() {
     const imageProps = Object.assign({}, this.props);
     delete imageProps.modification;
-    return <image ref="image" xlinkHref={this.props.src} style={this.filterFinder()} {...imageProps} />;
+    return <image ref="image" xlinkHref={this.props.src} {...imageProps} />;
   }
 }
 SVGImage.propTypes = {
   src: PropTypes.string.isRequired,
   height: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired,
-  modification: PropTypes.object
-};
-
-SVGImage.defaultProps = {
-  modification: {}
+  width: PropTypes.number.isRequired
 };
 
 export default SVGImage;
