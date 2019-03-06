02b4387e1d1670e6c7958b782375fc61d15d3fe5
Chris Lintott <cjl@astro.ox.ac.uk>
2019-03-06 10:00:17 +0000

    Update publications.js

--- app/lib/publications.js
+++ app/lib/publications.js
@@ -513,6 +513,16 @@ const Publications = {
           citation: "Planet Hunters: Assessing the Kepler Inventory of Short-period Planets, Schwamb+ 2012.",
           href: "http://adsabs.harvard.edu/abs/2012ApJ...754..129S",
           date: "Aug 2012"
+        },
+        {
+          citation: "WASP-47: A Hot Jupiter System with Two Additional Planets Discovered by K2, Becker et al. 2015",
+          href: "https://arxiv.org/abs/1508.02411",
+          date: "Feb 2015"
+        },
+        {
+           citation: "Likely transiting exocomets detected by Kepler, Rappaport et al., 2017",
+           href: "http://arxiv.org/abs/1708.06069",
+           date: "June 2017"
         }
       ]
     },
