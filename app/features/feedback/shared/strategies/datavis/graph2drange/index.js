f7236cc33b272210b6ded2d9d3e2ea98f375868a
mcbouslog <mcbouslog@gmail.com>
2019-05-22 16:51:44 -0500

    Add Graph2dRange feedback editor

---
+++ app/features/feedback/shared/strategies/datavis/graph2drange/index.js
@@ -0,0 +1,9 @@
+import LabComponent from '../../drawing/lab-component';
+import validations from '../../drawing/validations';
+
+export default {
+  id: 'graph2drange',
+  labComponent: LabComponent,
+  title: 'Graph2dRange',
+  validations
+};
