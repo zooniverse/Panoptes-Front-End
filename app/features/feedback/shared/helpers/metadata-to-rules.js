28e39ccd57bdbefa0584699f8939bcefa26f9386
mcbouslog <mcbouslog@gmail.com>
2019-10-01 14:42:02 -0500

    Edit feedback metadata-to-rules ruleIndex

--- app/features/feedback/shared/helpers/metadata-to-rules.js
+++ app/features/feedback/shared/helpers/metadata-to-rules.js
@@ -1,12 +1,17 @@
 // Converts a subject metadata object into an array of feedback objects
 function metadataToRules(metadata = {}) {
   const metadataKeys = Object.keys(metadata);
-  const rules = metadataKeys.reduce(function (result, key) {
+  const rules = metadataKeys.reduce((result, key) => {
     const [prefix, ruleIndex, propKey] = key.split('_');
     const value = metadata[key];
-    const stringValue = value.toString();
+    const stringValue = (value !== null && value !== undefined) ? value.toString() : '';
 
     if (prefix === '#feedback' && stringValue) {
+      if (isNaN(ruleIndex)) {
+        console.error(
+          `Subject metadata feedback rule index ${ruleIndex} is improperly formatted. The feedback rule index should be an integer.`
+        );
+      }
       const rule = result[ruleIndex] || {};
       rule[propKey] = value;
       result[ruleIndex] = rule;
