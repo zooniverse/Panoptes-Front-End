a80e72dc2fca7bfd20413c02c165afde7f2d5388
Hugh Dickinson <hugh.dickinson@open.ac.uk>
2020-01-30 18:02:55 +0000

    Updated tests for modal feedback message reducer

--- app/features/feedback/classifier/helpers/open-feedback-modal.spec.js
+++ app/features/feedback/classifier/helpers/open-feedback-modal.spec.js
@@ -77,21 +77,21 @@ describe('helpers > open-feedback-modal > getFeedbackMessages', function() {
     it('The counts for both message classes should be > 1.',
       function() {
         const reduced = getFeedbackMessages(MULTI_TRUE_MULTI_FALSE_RULES)
-        expect(reduced).to.deep.equal({Successes: ['hit (3 matches)'], Failures: ['miss (2 matches)']})
+        expect(reduced).to.deep.equal({correct: ['hit (3 matches)'], incorrect: ['miss (2 matches)']})
       })
 
     it('The counts for the success message class should be > 1. ' +
       'The count for the failure message class should be 1.',
       function() {
         const reduced = getFeedbackMessages(MULTI_TRUE_SINGLE_FALSE_RULES)
-        expect(reduced).to.deep.equal({Successes: ['hit (3 matches)'], Failures: ['miss (1 match)']})
+        expect(reduced).to.deep.equal({correct: ['hit (3 matches)'], incorrect: ['miss (1 match)']})
       })
 
     it('The counts for the failure message class should be > 1. ' +
       'The count for the success message class should be 1.',
       function() {
         const reduced = getFeedbackMessages(SINGLE_TRUE_MULTI_FALSE_RULES)
-        expect(reduced).to.deep.equal({Successes : ['hit (1 match)'], Failures: ['miss (3 matches)']})
+        expect(reduced).to.deep.equal({correct : ['hit (1 match)'], incorrect: ['miss (3 matches)']})
       })
 
     it('1. The counts for the success message class should be > 1. ' +
@@ -99,7 +99,7 @@ describe('helpers > open-feedback-modal > getFeedbackMessages', function() {
       'The count for the false positive message class should be 1.',
       function() {
         const reduced = getFeedbackMessages(MULTI_TRUE_MULTI_FALSE_FP_RULES)
-        expect(reduced).to.deep.equal({Successes: ['hit (2 matches)'], Failures: ['miss (1 match)'], "False Positives": ['hit (1 match)']})
+        expect(reduced).to.deep.equal({correct: ['hit (2 matches)'], incorrect: ['miss (1 match)'], falsepos: ['hit (1 match)']})
       })
 
     it('2. The counts for the success message class should be > 1. ' +
@@ -107,7 +107,7 @@ describe('helpers > open-feedback-modal > getFeedbackMessages', function() {
       'The count for the false positive message class should be 1.',
       function() {
         const reduced = getFeedbackMessages(MULTI_TRUE_SINGLE_FALSE_FP_RULES)
-        expect(reduced).to.deep.equal({Successes: ['hit (2 matches)'], Failures: ['miss (1 match)'], "False Positives": ['hit (1 match)']})
+        expect(reduced).to.deep.equal({correct: ['hit (2 matches)'], incorrect: ['miss (1 match)'], falsepos: ['hit (1 match)']})
       })
 
     it('3. The counts for the success message class should be 1. ' +
@@ -115,7 +115,7 @@ describe('helpers > open-feedback-modal > getFeedbackMessages', function() {
       'The count for the false positive message class should be 0.',
       function() {
         const reduced = getFeedbackMessages(SINGLE_TRUE_MULTI_FALSE_FP_RULES)
-        expect(reduced).to.deep.equal({Successes: ['hit (1 match)'], Failures: ['miss (1 match)']})
+        expect(reduced).to.deep.equal({correct: ['hit (1 match)'], incorrect: ['miss (1 match)']})
       })
   })
 })
