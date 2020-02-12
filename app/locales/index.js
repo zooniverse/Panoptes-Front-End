07e9c89fe4154dca8d199ed35bd85f3393407d82
Jim O'Donnell <jim@zooniverse.org>
2020-02-12 15:25:59 +0000

    Register the Polish locale (#5636)

--- app/locales/index.js
+++ app/locales/index.js
@@ -9,6 +9,7 @@ export default {
   it: require('./it').default,
   ja: require('./ja').default,
   nl: require('./nl').default,
+  pl: require('./pl').default,
   pt: require('./pt').default,
   'zh-cn': require('./zh-cn').default,
   'zh-tw': require('./zh-tw').default
