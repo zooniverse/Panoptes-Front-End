d5a83ab17a2695bfdb72b2e6d3c188e4cec1a715
Jim O'Donnell <jim@zooniverse.org>
2020-09-17 15:26:16 +0100

    Expand user admin page (#5798)
    * User admin: add general email preferences
    Add a short form to the user admin page which lists checkboxes for general and beta testing email preferences.
    
    * Convert handleInputChange to JS
    
    * Add valid_email and created_at
    
    * Make valid_email editable
    Allow admins to invalidate an email address.

--- app/pages/admin/user-settings/details.jsx
+++ app/pages/admin/user-settings/details.jsx
@@ -5,7 +5,13 @@ import AutoSave from '../../../components/auto-save';
 import handleInputChange from '../../../lib/handle-input-change';
 
 const UserDetails = (props) => {
-  const handleChange = handleInputChange.bind(props.user);
+  const handleUserChange = handleInputChange.bind(props.user);
+  const created = new Date(props.user.created_at)
+
+  function onChange(e) {
+    handleUserChange(e);
+    props.user.save();
+  }
 
   return (
     <div className="user-details">
@@ -14,7 +20,49 @@ const UserDetails = (props) => {
         <li>Login: {props.user.login}</li>
         <li>Display name: {props.user.display_name}</li>
         <li>Email address: {props.user.email}</li>
+        <li>Signed up: {created.toString()}</li>
+        <li>
+          <label>
+            Valid email:{' '}
+            <input
+              type="checkbox"
+              name="valid_email"
+              checked={props.user.valid_email}
+              onChange={onChange}
+            />
+          </label>
+        </li>
       </ul>
+
+      <form>
+        <fieldset>
+          <legend>Email preferences</legend>
+          <ul>
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  name="global_email_communication"
+                  checked={props.user.global_email_communication}
+                  onChange={onChange}
+                />{' '}
+                Zooniverse general emails
+              </label>
+            </li>
+              <label>
+                <input
+                  type="checkbox"
+                  name="beta_email_communication"
+                  checked={props.user.beta_email_communication}
+                  onChange={onChange}
+                />{' '}
+                Beta test emails
+              </label>
+            <li>
+            </li>
+          </ul>
+        </fieldset>
+      </form>
 
     </div>
   );
--- app/pages/admin/user-settings/details.spec.js
+++ app/pages/admin/user-settings/details.spec.js
@@ -1,6 +1,9 @@
+import apiClient from 'panoptes-client/lib/api-client';
 import React from 'react';
 import assert from 'assert';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import UserDetails from './details';
 
 describe('UserDetails', function () {
@@ -8,6 +11,14 @@ describe('UserDetails', function () {
     let user = {};
 
     before(function () {
+        user = apiClient.type('users').create({
+          login: 'test-account',
+          display_name: 'Test Account',
+          email: 'test@zooniverse.org',
+          global_email_communication: false,
+          beta_email_communication: true
+        })
+        sinon.stub(user, 'save');
         wrapper = shallow(<UserDetails user={user} />);
     });
 
@@ -15,4 +26,64 @@ describe('UserDetails', function () {
         const container = wrapper.find('div.user-details');
         assert.equal(container.length, 1);
     });
+
+    describe('email preferences', function () {
+      it('should list general email preferences', function () {
+        const generalPrefs = wrapper.find('input[name="global_email_communication"]');
+        expect(generalPrefs.props().checked).to.be.false;
+      })
+
+      it('should list beta email preferences', function () {
+        const betaPrefs = wrapper.find('input[name="beta_email_communication"]');
+        expect(betaPrefs.props().checked).to.be.true;
+      })
+
+      describe('global email preference change', function () {
+        before(function () {
+          const generalPrefs = wrapper.find('input[name="global_email_communication"]');
+          const target = {
+            checked: true,
+            name: 'global_email_preferences',
+            type: 'checkbox'
+          }
+          generalPrefs.simulate('change', { target });
+        })
+
+        after(function () {
+          user.save.resetHistory()
+        })
+
+        it('should update general email preferences', function () {
+          expect(!!user.global_email_preferences).to.be.true;
+        })
+
+        it('should save general email preferences', function () {
+          expect(user.save).to.have.been.calledOnce;
+        })
+      })
+
+      describe('beta email preference change', function () {
+        before(function () {
+          const betaPrefs = wrapper.find('input[name="beta_email_communication"]');
+          const target = {
+            checked: false,
+            name: 'beta_email_preferences',
+            type: 'checkbox'
+          }
+          betaPrefs.simulate('change', { target });
+        })
+
+        after(function () {
+          user.save.resetHistory()
+        })
+
+        it('should update beta email preferences', function () {
+          expect(!!user.beta_email_preferences).to.be.false;
+        })
+
+        it('should save beta email preferences', function () {
+          expect(user.save).to.have.been.calledOnce;
+        })
+      })
+    })
 });
