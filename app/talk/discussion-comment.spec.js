dcfe198610a8142b8a4073301f83bfecd86f1cc1
Jim O'Donnell <jim@eatyourgreens.org.uk>
2020-10-30 12:52:30 +0000

    Revert "Talk: require a valid email address (#5799)"
    This reverts commit 119f365dd27aad16f0ae947cdebb4505b518cf88.

--- app/talk/discussion-comment.spec.js
+++ app/talk/discussion-comment.spec.js
@@ -1,8 +1,5 @@
-import apiClient from 'panoptes-client/lib/api-client';
 import React from 'react';
-import { Link } from 'react-router';
 import assert from 'assert';
-import { expect } from 'chai';
 import DiscussionComment from './discussion-comment';
 import { shallow } from 'enzyme';
 
@@ -10,23 +7,10 @@ const discussion = {
   section: 1
 };
 
-const validUser = apiClient.type('users').create({
-  login: 'test-account',
-  display_name: 'Test Account',
-  email: 'test@zooniverse.org',
-  global_email_communication: false,
-  beta_email_communication: true,
-  valid_email: true
-});
-
-const invalidUser = apiClient.type('users').create({
-  login: 'test-account',
-  display_name: 'Test Account',
-  email: 'test@zooniverse',
-  global_email_communication: false,
-  beta_email_communication: true,
-  valid_email: false
-});
+const user = {
+  id: 1,
+  display_name: 'Test User'
+};
 
 describe('DiscussionComment', function() {
   let wrapper;
@@ -49,32 +33,15 @@ describe('DiscussionComment', function() {
 
   describe('logged in', function() {
     beforeEach(function() {
-      wrapper = shallow(<DiscussionComment discussion={discussion} user={validUser} />);
+      wrapper = shallow(<DiscussionComment discussion={discussion} user={user} />);
     });
 
     it('will show a user avatar', function() {
       assert.equal(wrapper.find('.talk-comment-author').length, 1);
     });
-
-    describe('with a valid email', function() {
-      it('will show a comment box', function() {
-        assert.equal(wrapper.find('Commentbox').props().user, validUser);
-      });
-    })
-
-    describe('with an invalid email', function() {
-      beforeEach(function () {
-        wrapper.setProps({ user: invalidUser });
-        wrapper.update();
-      });
-
-      it('will not show a comment box', function() {
-        expect(wrapper.find('Commentbox').length).to.equal(0);
-      });
-      it('will show a link to update your email address', function () {
-        expect(wrapper.find(Link).props().to).to.equal('/settings/email');
-      })
-    })
+    it('will show a comment box', function() {
+      assert.equal(wrapper.find('Commentbox').props().user, user);
+    });
   });
 
   describe('comment validation', function(){
