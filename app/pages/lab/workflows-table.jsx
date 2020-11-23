94448e83aa711a91ba61ef4ff6ec877ba837ae67
Jim O'Donnell <jim@zooniverse.org>
2020-11-23 11:47:03 +0000

    Project Builder: Align workflow completeness radio buttons (#5834)
    * Align workflow completeness radio buttons
    Align the workflow completeness radio buttons, in the lab, so that they are above one another.
    
    * Switch to BEM

--- app/pages/lab/workflows-table.jsx
+++ app/pages/lab/workflows-table.jsx
@@ -58,7 +58,7 @@ const WorkflowsTable = ({
                 </label>
               </td>
               <td>
-                <label>
+                <label class="standard-table__label__completeness">
                   <input
                     checked={statsCompletenessType === 'classification'}
                     name={`stats_completeness_type.${workflow.id}`}
@@ -69,7 +69,7 @@ const WorkflowsTable = ({
                   Classification Count
                 </label>
                 {' '}
-                <label>
+                <label class="standard-table__label__completeness">
                   <input
                     checked={statsCompletenessType === 'retirement'}
                     name={`stats_completeness_type.${workflow.id}`}
