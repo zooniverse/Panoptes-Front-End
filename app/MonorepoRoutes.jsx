07158242b56f2569cefdd5603467d6d49453f7d3
Jim O'Donnell <jim@zooniverse.org>
2021-10-26 10:27:05 +0100

    Refactor slugs into monorepoSlugs.js

--- app/MonorepoRoutes.jsx
+++ app/MonorepoRoutes.jsx
@@ -2,25 +2,8 @@ import React from 'react';
 import { Route } from 'react-router';
 import { createRoutesFromReactChildren } from 'react-router/lib//RouteUtils';
 import MonorepoRoute from './MonorepoRoute';
+import SLUGS from './monorepoSlugs'
 
-/*
-  Edit this list to add new project slugs.
-*/
-const SLUGS = [
-  "nora-dot-eisner/planet-hunters-tess",
-  "adamamiller/zwickys-stellar-sleuths",
-  "msalmon/hms-nhs-the-nautical-health-service",
-  "blicksam/transcription-task-testing",
-  "humphrydavy/davy-notebooks-project",
-  "mainehistory/beyond-borders-transcribing-historic-maine-land-documents",
-  "zookeeper/galaxy-zoo-weird-and-wonderful",
-  "hughdickinson/superwasp-black-hole-hunters",
-  "bogden/scarlets-and-blues",
-  "kmc35/peoples-contest-digital-archive",
-  "rachaelsking/corresponding-with-quakers",
-  "mariaedgeworthletters/maria-edgeworth-letters",
-  "pmlogan/poets-and-lovers"
-]
 /*
   This is a very useful article explaining how to write custom Route components for react-router v3.
   https://marmelab.com/blog/2016/09/20/custom-react-router-component.html
---
+++ app/monorepoSlugs.js
@@ -0,0 +1,15 @@
+export default [
+  'nora-dot-eisner/planet-hunters-tess',
+  'adamamiller/zwickys-stellar-sleuths',
+  'msalmon/hms-nhs-the-nautical-health-service',
+  'blicksam/transcription-task-testing',
+  'humphrydavy/davy-notebooks-project',
+  'mainehistory/beyond-borders-transcribing-historic-maine-land-documents',
+  'zookeeper/galaxy-zoo-weird-and-wonderful',
+  'hughdickinson/superwasp-black-hole-hunters',
+  'bogden/scarlets-and-blues',
+  'kmc35/peoples-contest-digital-archive',
+  'rachaelsking/corresponding-with-quakers',
+  'mariaedgeworthletters/maria-edgeworth-letters',
+  'pmlogan/poets-and-lovers'
+];
