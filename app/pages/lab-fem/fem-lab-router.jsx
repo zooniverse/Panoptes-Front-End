0f34ca50861b0eb70dcb990c1b31cc633b2d9302
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-11-01 19:00:03 +0000

    Add FEMLabRouter logic

--- app/pages/lab-fem/fem-lab-router.jsx
+++ app/pages/lab-fem/fem-lab-router.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
 import Workflow from '../lab/workflow'
+import FEMWorkflow from './workflow'
 
 /**
 The Front End Monorepo Lab (Project Builder) Router returns a different
-component, depending on the path, AND whether or not the associated Zooniverse
+component, depending on the path AND whether or not the associated Zooniverse
 Project is FEM-compatible.
  */
 export default function FEMLabRouter (props) {
-  console.log('+++ props: ', props)
-  console.log('+++ Workflow: ', Workflow)
-  return <h2>'HELLO WORLD'</h2>
+  const thisProjectUsesFEM = props.project?.id === '1873'  // DEBUG
+  const thisRoute = props?.routes?.map(r=>r.path).join('/').replace('//', '/') || ''
+
+  switch (thisRoute) {
+    case '/lab/:projectID/workflows/:workflowID':  // WARNING: assumes there are no child routes.
+      return (thisProjectUsesFEM)
+        ? <FEMWorkflow {...props} />
+        : <Workflow {...props} />
+  }
+
+  return <div>ERROR: This path is not registered in the FEMLabRouter.</div>
 }
