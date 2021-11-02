00b8f0ad89b45a5bb58b0456b80f193e5a67cf69
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-11-02 13:35:49 +0000

    Add functional FEM-lab checks

--- app/pages/lab-fem/fem-lab-router.jsx
+++ app/pages/lab-fem/fem-lab-router.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Workflow from '../lab/workflow'
 import FEMWorkflow from './workflow'
+import { usesMonorepo } from '../../monorepoUtils';
 
 /**
 The Front End Monorepo Lab (Project Builder) Router returns a different
@@ -9,9 +10,15 @@ component, depending on the path AND whether or not the associated Zooniverse
 Project is FEM-compatible.
  */
 export default function FEMLabRouter (props) {
-  const thisProjectUsesFEM = props.project?.id === '1873'  // DEBUG
-  const thisRoute = props?.routes?.map(r=>r.path).join('/').replace('//', '/') || ''
+  // TODO: the "is it FEM-compatible?" check is pretty arbitrary, as of
+  // 2 Nov 2011. We need to come back to this once we have a better foundational
+  // solution to figuring out whether a project is FEM-compatible.
+  const thisProjectUsesFEM = (  // Use FEM-compatible pages if...
+    usesMonorepo(props?.project?.slug)  // ...the project slug is registered in the PFE code
+    || !!props?.location?.query?.femLab // ...or ?femLab=true flag is set
+  ) && !props?.location?.query?.pfeLab  // ...UNLESS ?pfeLab=true flag is set
 
+  const thisRoute = props?.routes?.map(r=>r.path).join('/').replace('//', '/') || ''
   switch (thisRoute) {
     case '/lab/:projectID/workflows/:workflowID':  // WARNING: assumes there are no child routes.
       return (thisProjectUsesFEM)
