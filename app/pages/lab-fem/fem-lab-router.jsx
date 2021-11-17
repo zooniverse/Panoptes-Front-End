eb3fc179d5e31550cc66b233c063bd8d880e4743
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2021-11-18 02:09:24 +0800

    FEM-compatible Project Builder (aka Lab 2 aka /lab-fem) (#6037)
    * Add /lab-fem route
    
    * Add app/pages/lab-fem folder with README
    
    * Add lab-fem Workflows editor. URLs links are still wonky though
    
    * Apply changes from #6306
    
    * WIP remove /lab-fem route. Add FEMLabSplitter component
    
    * Fix import typo
    
    * Propagate changes from PR #6040
    
    * Update FEMLabSplitter into FEMLabRouter
    
    * Add FEMLabRouter logic
    
    * Update README
    
    * Add functional FEM-lab checks
    
    * Update README. Add option to toggle FEM-lab via experimental tools
    
    * Synch changes from #6042
    
    * Add deprecated Shortcut Editor option.
    
    * Remove 'Project is already using the FEM Classifier' clause in FEMLabRouter
    
    * Remove ShortcutEditor completely from FEM-lab
    
    * Change checks for ?pfeLab and ?femLab
    
    Co-authored-by: Jim O'Donnell <jim@zooniverse.org>

---
+++ app/pages/lab-fem/fem-lab-router.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Workflow from '../lab/workflow'
+import FEMWorkflow from './workflow'
+
+/**
+The Front End Monorepo Lab (Project Builder) Router returns a different
+component, depending on the path AND whether or not the associated Zooniverse
+Project is FEM-compatible.
+ */
+export default function FEMLabRouter (props) {
+  // TODO: the "is it FEM-compatible?" check is pretty arbitrary, as of
+  // 2 Nov 2011. We need to come back to this once we have a better foundational
+  // solution to figuring out whether a project is FEM-compatible.
+  const thisProjectUsesFEM = (  // Use FEM-compatible pages if...
+    props?.project?.experimental_tools?.includes('femLab')  // ...the project has the femLab experimental tool
+    || props?.location?.query?.femLab === 'true' // ...OR ?femLab=true query param is set
+  ) && props?.location?.query?.pfeLab !== 'true'  // ...UNLESS ?pfeLab=true query param is set
+
+  const thisRoute = props?.routes?.map(r=>r.path).join('/').replace('//', '/') || ''
+  switch (thisRoute) {
+    case '/lab/:projectID/workflows/:workflowID':  // WARNING: assumes there are no child routes.
+      return (thisProjectUsesFEM)
+        ? <FEMWorkflow {...props} />
+        : <Workflow {...props} />
+  }
+
+  return <div>ERROR: This path is not registered in the FEMLabRouter.</div>
+}
