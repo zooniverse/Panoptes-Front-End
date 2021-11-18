0d555031a4d152390f49502865f736c7498528b0
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-11-18 20:47:40 +0800

    Add documentation for 'use FEM or PFE?' checks

--- app/monorepoUtils.js
+++ app/monorepoUtils.js
@@ -1,3 +1,14 @@
+/**
+These utilities are used to determine if a Zooniverse project should be using
+the FEM (Front-End-Monorepo) Classifier + project page.
+
+Context: this code was created during the 2020/2021 transitory period where
+we're migrating _some_ (but not _all_ ) projects to use FEM.
+
+Please see the related /app/pages/lab-fem folder, which use separate checks
+to see if the project should use the **FEM-compatible Project Builder.**
+ */
+
 export const SLUGS = [
   'nora-dot-eisner/planet-hunters-tess',
   'adamamiller/zwickys-stellar-sleuths',
---
+++ app/pages/lab-fem/is-this-project-using-fem-lab.js
@@ -0,0 +1,16 @@
+/**
+This function is used to determine if a Zooniverse project should be using
+the FEM-compatible (Front-End-Monorepo) version of the Project Builder (Lab).
+
+More FEM-lab code can be found in app/pages/lab-fem
+
+Context: this code was created during the 2020/2021 transitory period where
+we're migrating _some_ (but not _all_ ) projects to use FEM.
+
+Please see the associated /app/monorepoUtils.js , /app/MonorepoRoute.jsx,
+and /app/MonorepoRoutes.jsx code, which use separate checks to determine if the
+project should use the **FEM Classifier + project page.**
+ */
+export default function isThisProjectUsingFEMLab() {
+
+}
