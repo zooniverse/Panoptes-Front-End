1885332f28ffcf7818d0bbf90f2b5605665d110c
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-12-06 14:13:20 +0800

    Fix tests

--- app/pages/about/index.spec.js
+++ app/pages/about/index.spec.js
@@ -13,6 +13,6 @@ describe('AboutPage', function () {
   it('renders Translate elements', function () {
     const wrapper = shallow(<AboutPage />);
     const translateElements = wrapper.find('Translate');
-    assert.equal(translateElements.length, 10);
+    assert.equal(translateElements.length, 11);
   });
 });
