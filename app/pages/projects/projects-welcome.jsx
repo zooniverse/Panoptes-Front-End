3dba6d4e2bca19c8d15132b56a0e6bac053cd7e5
Campbell Allen <campbell.allen@gmail.com>
2022-01-12 17:51:46 +0000

    integrate project recommendation system to projects page

--- app/pages/projects/projects-welcome.jsx
+++ app/pages/projects/projects-welcome.jsx
@@ -7,15 +7,32 @@ import FeaturedProjectEditor from './featured-project-editor';
 import ProjectCard from '../../partials/project-card';
 
 class ProjectsWelcome extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      featuredProjects: []
+      featuredProjects: [],
+      recommendedProjects: [],
+      user: null
     };
   }
 
   componentDidMount() {
     this.getFeaturedProjects();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.user !== this.props.user) {
+      console.log('I have a user now :) setting it as state');
+      console.log(nextProps.user);
+      this.setState({ user: nextProps.user });
+    }
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    const { user } = this.state;
+    if (user !== prevState.user) {
+      this.getRecommendedProjects();
+    }
   }
 
   getFeaturedProjects() {
@@ -25,6 +42,40 @@ class ProjectsWelcome extends Component {
         this.setState({ featuredProjects });
       });
   }
+
+  getRecommendedProjects() {
+    // TODO: query the recommender end point
+    const { user } = this.state;
+    if (user) {
+      // only return 4 projects right now
+      // alternatively fetch more but shuffle so they change on page refresh
+      const recommenderUrl = `http://127.0.0.1:8000/recommend?user_id=${user.id}&page_size=4`;
+      fetch(recommenderUrl)
+        .then((response) => {
+          console.log(response.statusText);
+          if (!response.ok) {
+            console.error('recommender service: ERROR');
+            throw Error(response.statusText);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // console.log('recommender service: Received data');
+
+          // recommender system is built on production data so project ids won't match :(
+          const projectIds = data.map(recommendation => recommendation.project_id);
+          const query = { ids: projectIds.join(','), cards: true };
+          return apiClient.type('projects').get(query)
+            .then((recommendedProjects) => {
+              this.setState({ recommendedProjects });
+            });
+        })
+        .catch((err) => {
+          console.error(`recommender service: No status data from ${err}`);
+        });
+    }
+  }
+
   render() {
     return (
       <div className="resources-container welcome-banner">
@@ -33,6 +84,14 @@ class ProjectsWelcome extends Component {
         <Markdown>{counterpart('projects.welcome.talk')}</Markdown>
         <div className="project-card-list">
           {this.state.featuredProjects.map(project =>
+            <FeaturedProjectEditor key={project.id} project={project}>
+              <ProjectCard project={project} />
+            </FeaturedProjectEditor>
+          )}
+        </div>
+        <Translate content="projects.welcome.recommended" component="p" />
+        <div className="project-card-list">
+          {this.state.recommendedProjects.map(project =>
             <FeaturedProjectEditor key={project.id} project={project}>
               <ProjectCard project={project} />
             </FeaturedProjectEditor>
