7c4522ad79af838d4a46e4d7c958eda70e1a8d35
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-01-19 14:54:50 +0000

    Remove 'Audio Projects' block from the 'Call for Projects' page

--- app/pages/get-involved/call-for-projects.jsx
+++ app/pages/get-involved/call-for-projects.jsx
@@ -6,25 +6,6 @@ class CallForProjects extends React.Component {
   render() {
     return (
       <div>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.header')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.intro')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.seekingProposals')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.projectSelection')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.requirements')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.audioCompatibility')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.selectionCriteriaTitle')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.selectionCriteriaOne')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.selectionCriteriaTwo')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.selectionCriteriaThree')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.selectionCriteriaFour')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.selectionCriteriaFive')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.selectionCriteriaSix')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.furtherNotes')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.imlsGrantInfo')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.deadline')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.contact')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.submissionLink')}</Markdown>
-        <Markdown>{counterpart('getInvolved.callForProjects.audio.break')}</Markdown>
         <Markdown>{counterpart('getInvolved.callForProjects.bio.header')}</Markdown>
         <Markdown>{counterpart('getInvolved.callForProjects.bio.wouldResearchBenefit')}</Markdown>
         <Markdown>{counterpart('getInvolved.callForProjects.bio.projectBuilder')}</Markdown>
