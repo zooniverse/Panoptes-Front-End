4b734a588d34248a19d044867f5f148dd36d200e
Jim O'Donnell <jim@zooniverse.org>
2022-02-17 15:24:45 +0000

    Add a link to the new set

--- app/pages/lab/components/UploadButton.jsx
+++ app/pages/lab/components/UploadButton.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router';
 
-import { createSubject, createSubjectSet } from './helpers'
+import { createSubject, createSubjectSet } from './helpers';
 
 export default function UploadButton({
   manifest,
@@ -8,43 +9,46 @@ export default function UploadButton({
   project,
   subjects
 }) {
+  const [subjectSet, setSubjectSet] = useState(null);
   const [uploading, setUploading] = useState(false);
   const [uploadCount, setUploadCount] = useState(0);
 
+  async function uploadSubject(subject) {
+    const { priority, ...subjectMetadata } = subject.metadata;
+    const { locations } = subject;
+    const panoptesSubject = await createSubject({
+      locations,
+      metadata: {
+        ['#priority']: priority,
+        ...metadata,
+        ...subjectMetadata
+      },
+      links:
+        { project: project.id }
+    });
+    setUploadCount(count => count + 1);
+    return panoptesSubject;
+  }
+
   async function createSet() {
     setUploading(true);
-    const subjectSet = await createSubjectSet({
+    const _subjectSet = await createSubjectSet({
       display_name: manifest.label,
       metadata,
       links:
         { project: project.id }
     });
 
-    async function uploadSubject(subject) {
-      const { priority, ...subjectMetadata } = subject.metadata;
-      const { locations } = subject;
-      const panoptesSubject = await createSubject({
-        locations,
-        metadata: {
-          ['#priority']: priority,
-          ...metadata,
-          ...subjectMetadata
-        },
-        links:
-          { project: project.id }
-      });
-      setUploadCount(count => count + 1);
-      return panoptesSubject;
-    }
-
     const _subjects = await Promise.all(subjects.map(uploadSubject));
-    subjectSet.addLink('subjects', _subjects.map(subject => subject.id));
+    _subjectSet.addLink('subjects', _subjects.map(subject => subject.id));
+    setSubjectSet(_subjectSet);
   }
 
   return (
     <>
       {subjects && !uploading && <button onClick={createSet}>Create a subject set</button>}
       {uploading && <p>Uploading {uploadCount}/{subjects.length} subjects.</p>}
+      {subjectSet?.id && <Link to={`/lab/${project.id}/subject-sets/${subjectSet.id}`}>{subjectSet.display_name}</Link>}
     </>
   )
 }
