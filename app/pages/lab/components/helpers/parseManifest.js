8ec246699ec5cfb8b3af8cd3159d043efeba4909
Jim O'Donnell <jim@zooniverse.org>
2022-03-07 09:36:23 +0000

    Add manifest and canvas identifiers Record the manifest and canvas identifiers in subject metadata, as `iiif:manifest` and `iiif:canvas`.

--- app/pages/lab/components/helpers/parseManifest.js
+++ app/pages/lab/components/helpers/parseManifest.js
@@ -1,18 +1,19 @@
 function parseCanvasImage(image) {
   const { resource } = image;
-  const id = resource.service[`@id`]
-  const src = `${id}/full/!1400,2000/0/default.jpg`
-  return { 'image/jpeg': src }
+  const id = resource.service['@id'];
+  const src = `${id}/full/!1400,2000/0/default.jpg`;
+  return { 'image/jpeg': src };
 }
 
 function parseCanvas(canvas, index) {
-  const locations = canvas.images.map(parseCanvasImage)
-  const id = canvas.images[0].resource.service['@id'];
-  const thumb = `${id}/full/!400,400/0/default.jpg`
+  const locations = canvas.images.map(parseCanvasImage);
+  const thumbId = canvas.images[0].resource.service['@id'];
+  const thumb = `${thumbId}/full/!400,400/0/default.jpg`;
   const metadata = {
+    "iiif:canvas": canvas['@id'],
     priority: index + 1
-  }
-  return { id, thumb, locations, metadata }
+  };
+  return { thumb, locations, metadata };
 }
 
 export default function parseManifest(manifest) {
@@ -20,14 +21,16 @@ export default function parseManifest(manifest) {
   const [sequence] = sequences;
   const subjects = sequence.canvases.map((canvas, index) => {
     const alt = structures?.[index].label;
-    const { id, thumb, locations, metadata } = parseCanvas(canvas, index)
-    return { id, alt, locations, metadata, thumb };
+    const { thumb, locations, metadata } = parseCanvas(canvas, index);
+    return { alt, locations, metadata, thumb };
   });
-  const metadata = {};
+  const metadata = {
+    "iiif:manifest": manifest['@id']
+  };
   manifest.metadata.forEach(({ label, value }) => {
     metadata[label] = value;
   });
   const thumb = subjects[0].thumb;
-  const label = subjects[0].alt
+  const label = subjects[0].alt;
   return { label, metadata, subjects, thumb };
 }
