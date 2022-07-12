496d8c28281d1211353551f65ecfe799ac96e139
Cliff Johnson <cliff@zooniverse.org>
2022-07-12 10:37:13 -0500

    Edit Call for Projects Page (#6159)
    * removed untranslated callForProjects text
    
    * edit Get Involved tab translations
    
    * edit CallForProjects content in en locale file
    
    * edit callForProjects page code and test
    
    * minor edit to CfP text
    
    Co-authored-by: Delilah C <23665803+goplayoutside3@users.noreply.github.com>

--- app/locales/fr.js
+++ app/locales/fr.js
@@ -730,22 +730,6 @@ export default {
         favorites: 'Favoris'
       }
     },
-    callForProjects: {
-      bio: {
-        header: '## Call for Biomedical Project Proposals',
-        wouldResearchBenefit: 'Would your research benefit from the involvement of thousands of volunteers? We are currently seeking proposals for biomedical projects to be developed as part of the Zooniverse platform. The Zooniverse is the world’s largest and most successful online platform for crowd-sourced research; we currently have over 1.5 million registered volunteers working in collaboration with professional researchers on more than 50 research projects across a range of disciplines, from physics to biology.',
-        projectBuilder: 'Using our unique [Project Builder](/lab) you can create your own Zooniverse project for free with a set of tried and tested tools, including multiple-choice questions and region marking or drawing tools. If we don’t yet offer the tools you need, please propose your project below; we are particularly interested in developing novel projects that extend the functionality of our platform.',
-        projectSelection: '### Project Selection',
-        expandFunctionality: 'We are looking for biomedical projects that will help us expand the functionality of the Zooniverse and build on the selection of tools available to researchers via our platform. Projects may involve a processing task applied to images, graphs, videos or another data format, data collection, or a combination of the two. Successful projects will be developed and hosted by the Zooniverse team, in close collaboration with the applicants.',
-        examples: 'Examples of our current biomedical projects include [Microscopy Masters](https://www.zooniverse.org/projects/jbrugg/microscopy-masters), where volunteers classify cryo-electron microscopy images to advance understanding of protein and virus structure, and [Worm Watch Lab](https://www.wormwatchlab.org/), which aims to improve understanding of the relationship between genes and behaviour.',
-        selectionCriteriaTitle: '### Selection Criteria:',
-        selectionCriteriaOne: '1. Projects extending the capability of the Zooniverse platform or serving as case studies for crowdsourcing in new areas are encouraged.',
-        selectionCriteriaTwo: '2. Alignment with biomedical research (long-term aim of research is to improve human health outcomes).',
-        selectionCriteriaThree: '3. Merit and usefulness of the data expected to result from the project.',
-        deadline: '### Deadline\nProject proposals are accepted on a rolling basis. Applications will be reviewed at the beginning of each month.',
-        submissionLink: '[SUBMIT A BIOMEDICAL PROPOSAL](https://goo.gl/forms/uUGdO5CpWDNFE5Uz2)'
-      }
-    },
     education: {
       title: '## L\'éducation avec Zooniverse',
       becomeCitizenScientist: 'En tant que volontaire sur les projets Zooniverse, vous et vos élèves pouvez devenir des citoyens  scientifiques et des citoyens chercheurs, participant à de vraies recherches scientifiques et dans d\’autres domaines. Si vous ou vos élèves pensez que vous avez fais une erreur, ne vous inquietez pas, même les chercheurs font des erreurs. Ces projets sont fait de façon à ce que plusieurs volontaires classifient chaque image, vidéo ou graphique du projet, ce qui permet d\’éliminer la grande majorité des erreurs humaines. Les erreurs font parti du jeu et peuvent même nous aider à évaluer la difficulté de la tâche demandée. Tant que toutes les personnes font de leur mieux,  ils aident !',
