9ba5a7b63b3fedbdac38e3086f10a2221cfe3511
Jim O'Donnell <jim@zooniverse.org>
2022-09-26 10:03:53 +0100

    Home page: Fix ‘Show all publications link’ (#6195)
    Publications is an external page now, so link to it with a HTML link, not React Router.

--- app/pages/home-common/social.jsx
+++ app/pages/home-common/social.jsx
@@ -127,7 +127,7 @@ export default class HomePageSocial extends React.Component {
             <Translate className="tertiary-kicker" component="h3" content="socialHomePage.recentPublications" />
             <Translate className="regular-body" component="span" content="socialHomePage.recentPublicationsBlurb" />
             <br/>
-            <Link to="/about/publications" className="primary-button primary-button--light">See All Publications</Link>
+            <a href="/about/publications" className="primary-button primary-button--light">See All Publications</a>
           </div>
 
           <div className="home-social__content--vertical-line"></div>
