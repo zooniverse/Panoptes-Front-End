3604b26537e832695bffd1325f346692fee67898
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-10-20 16:06:21 +0100

    Use full paths (ie include file extension) for ES imports

--- app/pages/lab/workflow.jsx
+++ app/pages/lab/workflow.jsx
@@ -1,25 +1,25 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import handleInputChange from '../../lib/handle-input-change';
-import PromiseRenderer from '../../components/promise-renderer';
+import handleInputChange from '../../lib/handle-input-change.js';
+import PromiseRenderer from '../../components/promise-renderer.cjsx';
 import TriggeredModalForm from 'modal-form/triggered';
 import ModalFormDialog from 'modal-form/dialog';
 import apiClient from 'panoptes-client/lib/api-client';
-import ChangeListener from '../../components/change-listener';
-import RetirementRulesEditor from '../../components/retirement-rules-editor';
+import ChangeListener from '../../components/change-listener.cjsx';
+import RetirementRulesEditor from '../../components/retirement-rules-editor.cjsx';
 import {Link} from 'react-router';
-import MultiImageSubjectOptionsEditor from '../../components/multi-image-subject-options-editor';
+import MultiImageSubjectOptionsEditor from '../../components/multi-image-subject-options-editor.cjsx';
 import taskComponents from '../../classifier/tasks';
-import AutoSave from '../../components/auto-save';
-import FileButton from '../../components/file-button';
-import WorkflowCreateForm from './workflow-create-form';
-import workflowActions from './actions/workflow';
+import AutoSave from '../../components/auto-save.coffee';
+import FileButton from '../../components/file-button.cjsx';
+import WorkflowCreateForm from './workflow-create-form.cjsx';
+import workflowActions from './actions/workflow.js';
 import classnames from 'classnames';
-import ShortcutEditor from '../../classifier/tasks/shortcut/editor';
+import ShortcutEditor from '../../classifier/tasks/shortcut/editor.jsx';
 import FeedbackSection from '../../features/feedback/lab';
 import MobileSection from './mobile';
-import SubjectGroupViewerEditor from './workflow-components/subject-group-viewer-editor';
-import SubjectSetLinker from './workflow-components/subject-set-linker';
+import SubjectGroupViewerEditor from './workflow-components/subject-group-viewer-editor.jsx';
+import SubjectSetLinker from './workflow-components/subject-set-linker.jsx';
 
 const DEMO_SUBJECT_SET_ID = process.env.NODE_ENV === 'production'
 ? '6' // Cats
