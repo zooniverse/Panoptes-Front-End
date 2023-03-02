54822837ef5aedf223744a12055d8d711adc3a0d
Delilah Clement <delilahclement@gmail.com>
2023-03-02 12:50:46 -0600

    add layout-options component

---
+++ app/pages/lab-fem/components/layout-options.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function LayoutOptions({ workflow = null }) {
+  const handleNaturalSubjectDimesions = (e) => {
+    return workflow.update({
+      "configuration.use_natural_subject_dimensions": e.target.checked,
+    });
+  };
+
+  const useNaturalSubjectDimensions = !!workflow?.configuration?.use_natural_subject_dimensions
+
+  return (
+    <div>
+      <span className="form-label">Subject image display</span>
+      <br />
+      <small className="form-help">
+        By default, subject images stretch to fit the width of the volunteer's
+        browser. Check this option if you want the subject's maximum dimensions
+        to be the image's natural width and natural height.{" "}
+      </small>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          checked={useNaturalSubjectDimensions}
+          onChange={handleNaturalSubjectDimesions}
+        />
+        Display subject image using natural width and height
+      </label>
+    </div>
+  );
+}
