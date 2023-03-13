831a7ce3ea09c7954529fce12f6f8391d2af9721
Jim O'Donnell <jim@zooniverse.org>
2023-03-13 17:27:54 +0000

    Custom sanitiser options Allow SVG, SVG filters, the `<animate>` and `<use>` tags, and the `calcMode` attribute.

--- app/lib/put-file.js
+++ app/lib/put-file.js
@@ -1,6 +1,20 @@
 import createDOMPurify from 'dompurify';
 
 const DOMPurify = createDOMPurify(window);
+const sanitiseOptions = {
+  USE_PROFILES: {
+    svg: true,
+    svgFilters: true
+  },
+  ADD_TAGS: [
+    'animate',
+    'use'
+  ],
+  ADD_ATTR: [
+    'calcmode'
+  ]
+}
+
 const ALLOWED_MEDIA_TYPES = ['image', 'audio', 'video'];
 const ALLOWED_FILE_TYPES = ['text/plain', 'application/json', 'application/pdf'];
 
@@ -13,7 +27,7 @@ export default async function putFile(url, file, headers = {}) {
     let sanitisedFile = file
     if (file.type === 'image/svg+xml') {
       sanitisedFile = await file.text();
-      sanitisedFile = DOMPurify.sanitize(sanitisedFile)
+      sanitisedFile = DOMPurify.sanitize(sanitisedFile, sanitiseOptions);
     }
     const options = {
       method: 'PUT',
