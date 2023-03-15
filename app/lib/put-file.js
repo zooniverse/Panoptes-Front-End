bf7629bb545a441c5abd084740a9afa65d8169f2
Jim O'Donnell <jim@eatyourgreens.org.uk>
2023-03-15 10:57:02 +0000

    Revert "Sanitise SVG file uploads (#6479)"
    This reverts commit 0df86cbd414854e7231436c4393a50ccd6aea1dc.

--- app/lib/put-file.js
+++ app/lib/put-file.js
@@ -1,20 +1,3 @@
-import createDOMPurify from 'dompurify';
-
-const DOMPurify = createDOMPurify(window);
-const sanitiseOptions = {
-  USE_PROFILES: {
-    svg: true,
-    svgFilters: true
-  },
-  ADD_TAGS: [
-    'animate',
-    'use'
-  ],
-  ADD_ATTR: [
-    'calcmode'
-  ]
-}
-
 const ALLOWED_MEDIA_TYPES = ['image', 'audio', 'video'];
 const ALLOWED_FILE_TYPES = ['text/plain', 'application/json', 'application/pdf'];
 
@@ -24,18 +7,13 @@ export default async function putFile(url, file, headers = {}) {
     ALLOWED_MEDIA_TYPES.includes(type) ||
     ALLOWED_FILE_TYPES.includes(file.type);
   if (allowedUpload) {
-    let sanitisedFile = file
-    if (file.type === 'image/svg+xml') {
-      sanitisedFile = await file.text();
-      sanitisedFile = DOMPurify.sanitize(sanitisedFile, sanitiseOptions);
-    }
     const options = {
       method: 'PUT',
       headers: {
         ...headers,
         'x-ms-blob-type': 'BlockBlob'
       },
-      body: sanitisedFile
+      body: file
     };
     const response = await fetch(url, options);
     if (response.ok) {
