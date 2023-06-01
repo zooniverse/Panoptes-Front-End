350abd1c14cae0007302561b800170a431818e83
Jim O'Donnell <jim@zooniverse.org>
2023-06-01 21:20:25 +0100

    Fix home page social feeds (#6629)
    * Fix home page social feeds
    Fetch the home page blog feeds direct from `wordpress.com`.
    
    * Parse HTML in the browser
    
    * Show 3 posts each from both blogs

--- app/lib/get-social-data.js
+++ app/lib/get-social-data.js
@@ -2,6 +2,43 @@ import apiClient from 'panoptes-client/lib/api-client';
 import talkClient from 'panoptes-client/lib/talk-client';
 import Publications from './publications';
 
+const BLOG_FEEDS = [
+  // daily.zooniverse.org
+  'https://public-api.wordpress.com/rest/v1.1/sites/57182749/posts',
+  // blog.zooniverse.org
+  'https://public-api.wordpress.com/rest/v1.1/sites/36711287/posts'
+]
+
+function removeEntities(htmlString) {
+  const container = document.createElement('div');
+  container.innerHTML = htmlString;
+  return container.textContent;
+}
+
+function parseFeedPost(post) {
+  return {
+    id: post.ID,
+    title: removeEntities(post.title),
+    excerpt: removeEntities(post.excerpt),
+    created_at: post.date,
+    link: post.URL,
+    image: post.featured_image
+  };
+}
+async function fetchBlogFeed(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const feed = await response.json();
+      return feed.posts;
+    }
+    return [];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
 function getNewestProject() {
   return apiClient.type('projects').get({
     cards: true,
@@ -18,11 +55,11 @@ function getNewestProject() {
 async function getBlogPosts() {
   let posts = []
   try {
-    const response = await fetch(`${talkClient.root}/social`);
-    if (response.ok) {
-      const data = await response.json();
-      posts = data.posts;
-    }
+    const feeds = await Promise.all(BLOG_FEEDS.map(fetchBlogFeed));
+    feeds.forEach(feed => {
+      posts = posts.concat(feed.slice(0,3))
+    });
+    return posts.map(parseFeedPost);
   } catch (error) {
     console.error(error);
   }
