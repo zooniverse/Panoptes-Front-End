40a3e9ebeebe7e429ddbb517f53b4a5bc431e3fb
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-08-31 18:32:18 +0800

    PagesEditor: add StrictMode for better testing

--- app/pages/lab-pages-editor/index.jsx
+++ app/pages/lab-pages-editor/index.jsx
@@ -3,6 +3,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/require-default-props */
 
+import { StrictMode } from 'react';
 import PropTypes from 'prop-types';
 import DataManager from './DataManager.jsx';
 import Tester from './Tester.jsx';
@@ -11,20 +12,22 @@ function PagesEditor(props) {
   const { params } = props;
 
   return (
-    <div>
-      <h5>Pages Editor</h5>
-      <DataManager
-        key={params?.workflowId || '-'} //
-        workflowId={params?.workflowId}
-      >
-        <h6>
-          Workflow
-          {' '}
-          {params?.workflowId}
-        </h6>
-        <Tester />
-      </DataManager>
-    </div>
+    <StrictMode>
+      <div>
+        <h5>Pages Editor</h5>
+        <DataManager
+          key={params?.workflowId || '-'} //
+          workflowId={params?.workflowId}
+        >
+          <h6>
+            Workflow
+            {' '}
+            {params?.workflowId}
+          </h6>
+          <Tester />
+        </DataManager>
+      </div>
+    </StrictMode>
   );
 }
 
