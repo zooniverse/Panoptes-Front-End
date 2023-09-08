5bb53fad0fe002322290462784e092cc0808b3a6
Mark Bouslog <mark@zooniverse.org>
2023-09-08 17:16:27 -0500

    Refactor markAsRead and markAllRead tests

--- app/pages/notifications/notification-section.spec.js
+++ app/pages/notifications/notification-section.spec.js
@@ -4,6 +4,8 @@ import React from 'react';
 import assert from 'assert';
 import sinon from 'sinon';
 import { shallow } from 'enzyme';
+import talkClient from 'panoptes-client/lib/talk-client';
+
 import NotificationSection from './notification-section';
 
 const notificationComment = {
@@ -11,45 +13,59 @@ const notificationComment = {
   source_type: 'Comment',
   delivered: true,
   source: {
-    discussion_id: '456'
-  }
+    discussion_id: '789'
+  },
+  update: sinon.stub().returnsThis(),
+  save: sinon.stub().resolves({})
 };
 
 const notificationDataRequest = {
   id: '124',
   source_type: 'DataRequest',
-  delivered: true
-};
-
-const notificationDiscussion = {
-  id: '456',
-  source_type: 'Discussion',
-  delivered: true
+  delivered: true,
+  update: sinon.stub().returnsThis(),
+  save: sinon.stub().resolves({})
 };
 
 const notificationModeration = {
-  id: '457',
+  id: '126',
   source_type: 'Moderation',
-  delivered: true
+  delivered: true,
+  update: sinon.stub().returnsThis(),
+  save: sinon.stub().resolves({})
 };
 
-const notificationCommentUnread = {
-  id: '125',
+const notificationCommentUnread1 = {
+  id: '127',
   source_type: 'Comment',
   delivered: false,
   source: {
-    discussion_id: '456'
-  }
+    discussion_id: '790'
+  },
+  update: sinon.stub().returnsThis(),
+  save: sinon.stub().resolves({})
+};
+
+const notificationCommentUnread2 = {
+  id: '128',
+  source_type: 'Comment',
+  delivered: false,
+  source: {
+    discussion_id: '790'
+  },
+  update: sinon.stub().returnsThis(),
+  save: sinon.stub().resolves({})
 };
 
 const notifications = [
   notificationComment,
   notificationDataRequest,
-  notificationDiscussion,
   notificationModeration
 ];
 
-const notificationData = notifications.map((notification) => {
+const notificationsWithUnread = notifications.concat([notificationCommentUnread1, notificationCommentUnread2]);
+
+function notificationDataItem(notification) {
   const dataType = notification.source_type.toLowerCase();
   return {
     notification,
@@ -59,7 +75,11 @@ const notificationData = notifications.map((notification) => {
       }
     }
   };
-});
+}
+
+const notificationData = notifications.map((notification) => notificationDataItem(notification));
+
+const notificationDataUnread = notificationsWithUnread.map((notification) => notificationDataItem(notification));
 
 describe('NotificationSection', function () {
   describe('it can display a Zooniverse section', function () {
@@ -119,8 +139,6 @@ describe('NotificationSection', function () {
 
   describe('it can display a section with unread notifications', function () {
     let wrapper;
-    const notificationsWithUnread = Array.from(notifications);
-    notificationsWithUnread.push(notificationCommentUnread);
 
     before(function () {
       wrapper = shallow(
@@ -139,7 +157,7 @@ describe('NotificationSection', function () {
       });
     });
 
-    it('should show an unread notification in place of an avatar', function () {
+    it('should show an unread notification circle in place of an avatar', function () {
       assert.equal(wrapper.find('circle').length, 1);
     });
   });
@@ -166,7 +184,7 @@ describe('NotificationSection', function () {
     });
 
     it('should display the correct number of notifications', function () {
-      assert.equal(wrapper.find('Notification').length, 4);
+      assert.equal(wrapper.find('Notification').length, 3);
     });
 
     it('should show close icon', function () {
@@ -174,10 +192,8 @@ describe('NotificationSection', function () {
     });
   });
 
-  describe.skip('will update notifications as read', function () {
+  describe('will update notification as read', function () {
     let wrapper;
-    const notificationsWithUnread = Array.from(notifications);
-    notificationsWithUnread.push(notificationCommentUnread);
 
     const notificationsCounterStub = {
       update: sinon.stub()
@@ -201,21 +217,87 @@ describe('NotificationSection', function () {
       );
       wrapper.setState({
         loading: false,
-        name: 'Zooniverse'
+        name: 'Zooniverse',
+        notificationData: notificationDataUnread
       });
-      // wrapper.instance().markAsRead();
+      wrapper.instance().markAsRead(notificationDataUnread[4].notification);
     });
 
-    it('should update read notification as read (delivered)', function () {
-      assert.equal(true, false);
+    it('should update unread notification as read (delivered)', function () {
+      assert.equal(notificationCommentUnread2.update.calledWith({ delivered: true }), true);
+      assert.equal(notificationCommentUnread2.save.called, true);
     });
 
-    it('should update related notifications as read (delivered)', function () {
-      assert.equal(true, false);
+    it('should update related unread notifications as read (delivered)', function () {
+      assert.equal(notificationCommentUnread1.update.calledWith({ delivered: true }), true);
+      assert.equal(notificationCommentUnread1.save.called, true);
+    });
+
+    it('should not update unrelated notifications', function () {
+      assert.equal(notificationComment.update.called, false);
+      assert.equal(notificationDataRequest.update.called, false);
+      assert.equal(notificationModeration.update.called, false);
     });
 
     it('should update the notifications counter', function () {
-      assert.equal(true, false);
+      assert.equal(notificationsCounterStub.update.called, true);
+    });
+  });
+
+  describe('will update all notifications as read', function () {
+    let wrapper;
+    let toggleSectionSpy;
+
+    const notificationsCounterStub = {
+      setUnread: sinon.stub()
+    };
+
+    before(function () {
+      sinon.stub(talkClient, 'request').callsFake(() => Promise.resolve([]));
+      toggleSectionSpy = sinon.spy();
+
+      wrapper = shallow(
+        <NotificationSection
+          expanded={true}
+          notifications={notificationsWithUnread}
+          projectID="zooniverse"
+          section="zooniverse"
+          toggleSection={toggleSectionSpy}
+          user={{ id: '1' }}
+        />,
+        {
+          context: {
+            notificationsCounter: notificationsCounterStub
+          },
+          disableLifecycleMethods: true
+        }
+      );
+      wrapper.setState({
+        loading: false,
+        name: 'Zooniverse',
+        notificationData: notificationDataUnread
+      });
+      wrapper.instance().markAllRead();
+    });
+
+    after(function () {
+      talkClient.request.restore();
+    });
+
+    it('should update all notifications as read (delivered)', function () {
+      assert.equal(notificationComment.update.calledWith({ delivered: true }), true);
+      assert.equal(notificationDataRequest.update.calledWith({ delivered: true }), true);
+      assert.equal(notificationModeration.update.calledWith({ delivered: true }), true);
+      assert.equal(notificationCommentUnread1.update.calledWith({ delivered: true }), true);
+      assert.equal(notificationCommentUnread2.update.calledWith({ delivered: true }), true);
+    });
+
+    it('should call toggleSection', function () {
+      assert.equal(toggleSectionSpy.calledWith(false), true);
+    });
+
+    it('should update the notifications counter', function () {
+      assert.equal(notificationsCounterStub.setUnread.calledWith(0), true);
     });
   });
 });
