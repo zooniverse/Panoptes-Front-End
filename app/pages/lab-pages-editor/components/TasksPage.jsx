80f589360cbdd2e7370560723cf91335a1d38ef4
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-10-03 18:17:05 +0100

    Tasks Page: style up

--- app/pages/lab-pages-editor/components/TasksPage.jsx
+++ app/pages/lab-pages-editor/components/TasksPage.jsx
@@ -8,10 +8,28 @@ import strings from '../strings.json'; // TODO: move all text into strings
 
 export default function TasksPage() {
   const { workflow, update } = useWorkflowContext();
+  const isActive = true; // TODO
+
+  if (!workflow) return null;
 
   return (
-    <div>
-      Tasks Page
+    <div className="tasks-page">
+      <div className="workflow-title flex-row">
+        <h2 className="flex-item">{workflow?.display_name}</h2>
+        <span className="workflow-id">{`#${workflow?.id}`}</span>
+        {(isActive) ? <span className="status-active">Active</span> : <span className="status-inactive">Inactive</span>}
+      </div>
+      {/* TODO: the Tasks thing might need to be a section? */}
+      <div>
+        <div>Tasks</div>
+        <div className="flex-row">
+          <button className="flex-item" type="button">Add a new Task +</button>
+          <select className="flex-item">
+            <option>T0</option>
+            <option>T1</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
