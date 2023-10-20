05a5f3417a55666c8a9672ee7ca3d1343c8ed7a6
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2023-10-20 21:52:32 +0100

    createStep: add missing stepKey
    Co-authored-by: Jim O'Donnell <jim@zooniverse.org>

--- app/pages/lab-pages-editor/helpers/createStep.js
+++ app/pages/lab-pages-editor/helpers/createStep.js
@@ -3,5 +3,5 @@ Creates a Step, with tasks, that's ready to be inserted into a Workflow.
  */
 
 export default function createStep(stepKey, taskKeys = []) {
-  return [stepKey, { taskKeys }];
+  return [stepKey, { stepKey, taskKeys }];
 }
