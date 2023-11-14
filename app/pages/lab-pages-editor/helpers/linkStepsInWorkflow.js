115e12a1a54914c3052b5ade150cf0f449d2d7ff
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-11-14 14:08:32 +0000

    TasksPage: when linking Steps, check if it CAN branch, not if it does

--- app/pages/lab-pages-editor/helpers/linkStepsInWorkflow.js
+++ app/pages/lab-pages-editor/helpers/linkStepsInWorkflow.js
@@ -5,7 +5,7 @@ Links Steps in a Workflow together.
 - Output: COPY of array of Steps, with their `step.next`s updated.
 
 Rules for linking steps:
-- If a Step is branching, then step.next = undefined.
+- If a Step *can* branch, then step.next = undefined.
 - If a Step is the final step in the array, then step.next = undefined. 
 - Otherwise, each Step's step.next will be the ID of the next Step in the array.
  */
@@ -15,8 +15,8 @@ export default function linkStepsInWorkflow(steps = [], tasks = {}) {
     const [stepId, stepBody] = step;
 
     const isFinalStep = index >= (steps.length - 1);
-    const isBranching = isStepBranching(step, tasks);
-    const nextStep = (isFinalStep || isBranching) ? undefined : steps[index+1]?.[0];
+    const canBranch = canStepBranch(step, tasks);
+    const nextStep = (isFinalStep || canBranch) ? undefined : steps[index+1]?.[0];
 
     const newStepBody = {
       ...stepBody,
@@ -30,14 +30,25 @@ export default function linkStepsInWorkflow(steps = [], tasks = {}) {
 }
 
 /*
-Checks if a step is branching.
+Checks if a step can branch. We're not asking if the step IS branching, but if it *can.*
 - If yes, returns Task ID of the Task that's causing the branch.
+  - If multiple Tasks qualify, only return the FIRST.
 - If no, returns false.
  */
-function isStepBranching(step, tasks) {
-  // TODO: actually implement the darn check
-  // FEM's convertWorkflowToUseSteps has a method for checking if a Task is branching (isThereBranching())
-  // https://github.com/zooniverse/front-end-monorepo/blob/master/packages/lib-classifier/src/store/helpers/convertWorkflowToUseSteps/convertWorkflowToUseSteps.js
-  // QUESTION: what happens when a Step has multiple branching Questions?
-  return false;
+function canStepBranch(step = [], tasks = {}) {
+  // TODO/QUESTION: what happens when a Step has multiple branching Questions?
+  // TODO/CHECK: if a Question Task has 0 answers, is it still considered 'branching'? Check what happens with FEM Classifier
+  const [stepId, stepBody] = step;
+  const tasksInStep = stepBody?.taskKeys?.map(taskKey => tasks[taskKey] ? [taskKey, tasks[taskKey]]: undefined).filter(task => !!task) || []
+  return tasksInStep.find(canTaskBranch)?.[0] || false;
+}
+
+/*
+Checks if a task can branch.
+- Returns true or false.
+- At the moment, only "Single Question" tasks can branch. 
+- Compare with "isTaskBranching" in https://github.com/zooniverse/front-end-monorepo/blob/master/packages/lib-classifier/src/store/helpers/convertWorkflowToUseSteps/convertWorkflowToUseSteps.js
+ */
+function canTaskBranch([taskKey, task]) {
+  return task?.type === 'single';
 }
