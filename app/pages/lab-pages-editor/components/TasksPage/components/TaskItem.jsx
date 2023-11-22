7ace391011f1006651a71056afff8394bdfca8c6
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-11-22 22:58:51 +0000

    TaskItem: add missing alt/aria-label for task icons

--- app/pages/lab-pages-editor/components/TasksPage/components/TaskItem.jsx
+++ app/pages/lab-pages-editor/components/TasksPage/components/TaskItem.jsx
@@ -10,6 +10,12 @@ import PropTypes from 'prop-types';
 // import strings from '../../../strings.json'; // TODO: move all text into strings
 import TaskIcon from '../../../icons/TaskIcon.jsx';
 
+const TaskLabels = {
+  'drawing': 'Drawing Task',
+  'single': 'Question Task',  // Single question
+  'text': 'Text Task'
+};
+
 function TaskItem({
   task,
   taskKey
@@ -26,8 +32,10 @@ function TaskItem({
       <div className="flex-row spacing-bottom-M">
         <span className="task-key">{taskKey}</span>
         <span className="task-icon">
-          {/* TODO: change icon and aria label depending on task type */}
-          <TaskIcon type={task.type} />
+          <TaskIcon
+            alt={TaskLabels[task.type]}
+            type={task.type}
+          />
         </span>
         <span className="task-text flex-item">{taskText}</span>
       </div>
