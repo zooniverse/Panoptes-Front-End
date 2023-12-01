5532ffae472f951274bf636522274789a8f4f356
Mark Bouslog <mark@zooniverse.org>
2023-12-01 09:38:32 -0600

    Refactor admin for user_groups list and search

--- app/pages/admin/user-group-search-selector.jsx
+++ app/pages/admin/user-group-search-selector.jsx
@@ -1,65 +1,53 @@
 import { debounce } from 'lodash';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { browserHistory } from 'react-router';
 import apiClient from 'panoptes-client/lib/api-client';
 import Select from 'react-select';
 
-class SearchSelector extends Component {
+class UserGroupSearchSelector extends Component {
   constructor(props) {
     super(props);
-    this.navigateToUserGroup = this.navigateToUserGroup.bind(this);
-    this.searchByName = this.searchByName.bind(this);
-  }
 
-  navigateToUserGroup(userGroupOption) {
-    const userGroupID = userGroupOption.value;
-    browserHistory.push(['/admin/user-group-status', userGroupID].join('/'));
+    this.searchByName = this.searchByName.bind(this);
   }
 
   searchByName = debounce((value) => {
     const query = {
-      search: `%${value}%`
+      search: `%${value}%`,
+      page_size: 5
     };
-    if ((value != null ? value.trim().length : undefined) > 3) {
-      return apiClient.type('user_groups').get(query, {
-        page_size: 10
-      }).then((userGroups) => {
-        const opts = userGroups.map(userGroup => ({
-          value: userGroup.id,
-          label: userGroup.display_name
-        }));
-        return { options: opts };
-      });
-    } else {
-      return Promise.resolve({ options: [] });
+    
+    if (value.trim().length > 3) {
+      return apiClient.type('user_groups')
+        .get(query)
+        .then((userGroups) => {
+          const opts = userGroups.map(userGroup => ({
+            value: userGroup.id,
+            label: userGroup.display_name
+          }));
+          return ({ options: opts });
+        })
+        .catch((error) => {
+          console.log('error', error);
+        });
     }
-  }, 500); // 500ms delay
+    return Promise.resolve({ options: [] });
+  }, 300); // 300ms delay
 
   render() {
-    const { className } = this.props;
-
     return (
       <Select.Async
-        multi={false}
-        name="resourcesid"
-        placeholder="Display Name:"
-        value=""
-        searchPromptText="Search by display name..."
         loadOptions={this.searchByName}
-        onChange={this.navigateToUserGroup}
-        className={`search card-search standard-input ${className}`}
+        multi={false}
+        name="user_groups_search"
+        placeholder="Display Name:"
+        searchPromptText="Search by display name..."
+        value={this.props.userGroup}
+        onChange={this.props.handleUserGroupSearchChange}
+        className="search standard-input"
       />
     );
   }
 }
 
-SearchSelector.propTypes = {
-  className: PropTypes.string
-};
-
-SearchSelector.defaultProps = {
-  className: ''
-};
-
-export default SearchSelector;
+export default UserGroupSearchSelector;
--- app/pages/admin/user-group-status-list.jsx
+++ app/pages/admin/user-group-status-list.jsx
@@ -1,77 +1,51 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Link } from 'react-router';
-import apiClient from 'panoptes-client/lib/api-client';
+import { browserHistory } from 'react-router';
 
-import SearchSelector from './user-group-search-selector';
-import LoadingIndicator from '../../components/loading-indicator';
-import Paginator from '../../talk/lib/paginator';
+import UserGroupSearchSelector from './user-group-search-selector';
 
 class UserGroupStatusList extends Component {
   constructor(props) {
     super(props);
-    this.getUserGroups = this.getUserGroups.bind(this);
-    this.renderUserGroupList = this.renderUserGroupList.bind(this);
-    this.renderUserGroupListItem = this.renderUserGroupListItem.bind(this);
+
     this.state = {
-      loading: false,
-      userGroups: [],
-      error: null
+      userGroup: null
     };
+
+    this.handleUserGroupSearchChange = this.handleUserGroupSearchChange.bind(this);
+    this.navigateToUserGroup = this.navigateToUserGroup.bind(this);
   }
 
-  componentDidMount() {
-    this.getUserGroups();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.location.query !== this.props.location.query) {
-      this.getUserGroups();
+  handleUserGroupSearchChange = (userGroupOption) => {
+    if (userGroupOption) {
+      this.setState({ userGroup: userGroupOption });
+    } else {
+      this.setState({ userGroup: null });
     }
   }
 
-  getUserGroups() {
-    const { query } = this.props.location;
+  navigateToUserGroup(event) {
+    event.preventDefault();
 
-    this.setState({ loading: true, error: null });
-    return apiClient.type('user_groups').get(query)
-      .then((userGroups) => { this.setState({ userGroups, loading: false }); })
-      .catch((error) => { this.setState({ error: `Error requesting userGroups:, ${error}`, loading: false }); });
-  }
-
-  renderUserGroupList() {
-    const { userGroups } = this.state;
-    let meta = {};
-    if (userGroups.length > 0) {
-      meta = userGroups[0].getMeta();
-    }
-
-    return (userGroups.length === 0) ?
-      <div className="project-status-list">No user groups found for this filter</div> :
-      <div>
-        <h4 style={{ marginLeft: '20px' }}>Display Name - ID</h4>
-        <menu>
-          {userGroups.map(userGroup => this.renderUserGroupListItem(userGroup))}
-        </menu>
-        <Paginator page={meta.page} pageCount={meta.page_count} />
-      </div>;
-  }
-
-  renderUserGroupListItem(userGroup) {
-    return (
-      <li key={userGroup.id}>
-        <Link to={`/admin/user-group-status/${userGroup.id}`}>
-          <span>{userGroup.display_name} - {userGroup.id}</span>
-        </Link>
-      </li>
-    )
+    const userGroupID = this.state.userGroup.value;
+    browserHistory.push(['/admin/user-group-status', userGroupID].join('/'));
   }
 
   render() {
     return (
-      <div className="project-status-page">
-        <SearchSelector className="project-status-search" />
-        {(this.state.error) ? <p>{this.state.error}</p> : null}
-        {(this.state.loading) ? <LoadingIndicator /> : this.renderUserGroupList()}
+      <div className="columns-container">
+        <div className="column">
+          <UserGroupSearchSelector
+            handleUserGroupSearchChange={this.handleUserGroupSearchChange}
+            userGroup={this.state.userGroup}
+          />
+        </div>
+        <button
+          type="button"
+          onClick={this.navigateToUserGroup}
+        >
+          Edit user_group
+        </button>
       </div>
     );
   }
