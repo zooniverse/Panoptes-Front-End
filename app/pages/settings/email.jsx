360aa9a9c318a23c5bfe1a7287a2f38e60c938e8
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-04-24 02:19:55 +0100

    Email Settings: add requestConfirmationEmail link. Refactor for readability.

--- app/pages/settings/email.jsx
+++ app/pages/settings/email.jsx
@@ -151,6 +151,7 @@ class EmailSettingsPage extends React.Component {
     };
     this.handleProjectPreferenceChange = this.handleProjectPreferenceChange.bind(this);
     this.handleTalkPreferenceChange = this.handleTalkPreferenceChange.bind(this);
+    this.requestConfirmationEmail = this.requestConfirmationEmail.bind(this);
     this.getProjectForPreferences(props.user);
     this.getTalkPreferences();
   }
@@ -197,6 +198,10 @@ class EmailSettingsPage extends React.Component {
     });
   }
 
+  requestConfirmationEmail() {
+    console.log('+++ Send confirmation email!')
+  }
+
   handleProjectPreferenceChange(index, event) {
     const { projectPreferences } = this.state;
     projectPreferences[index].update({
@@ -235,8 +240,8 @@ class EmailSettingsPage extends React.Component {
   }
 
   render() {
-    const isEmailValid = !!this.props.user?.valid_email;
-    const isEmailVerfied = !!this.props.user?.confirmed_at;
+    const isEmailValid = false && !!this.props.user?.valid_email;
+    const isEmailVerfied = true || !!this.props.user?.confirmed_at;
   
     return (
       <div className="content-container">
@@ -256,12 +261,33 @@ class EmailSettingsPage extends React.Component {
           </AutoSave>
           <div>
             {(isEmailValid)
-              ? <div><i className='fa fa-check-circle' style={{ color: '#51db72' }} /> <Translate content="emailSettings.general.emailValid" /></div>
-              : <div><i className='fa fa-times-circle' style={{ color: '#e35950' }} /> <Translate content="emailSettings.general.emailInvalid" /> | <Translate content="emailSettings.general.emailInvalidPrompt" /></div>
+              ? <div>
+                  <i className='fa fa-check-circle' style={{ color: '#51db72' }} />
+                  {' '}
+                  <Translate content="emailSettings.general.emailValid" />
+                </div>
+              : <div>
+                  <i className='fa fa-times-circle' style={{ color: '#e35950' }} />
+                  {' '}
+                  <Translate content="emailSettings.general.emailInvalid" />
+                  {' | '}
+                  <Translate content="emailSettings.general.emailInvalidPrompt" />
+                </div>
             }
             {(isEmailVerfied)
-              ? <div><i className='fa fa-check-circle' style={{ color: '#51db72' }} /> <Translate content="emailSettings.general.emailVerified" /></div>
-              : <div><i className='fa fa-times-circle' style={{ color: '#e35950' }} /> <Translate content="emailSettings.general.emailUnverified" /> | <Translate content="emailSettings.general.emailUnverifiedPrompt" /></div>
+              ? <div>
+                  <i className='fa fa-check-circle' style={{ color: '#51db72' }} />
+                  {' '}
+                  <Translate content="emailSettings.general.emailVerified" /></div>
+              : <div>
+                  <i className='fa fa-times-circle' style={{ color: '#e35950' }} />
+                  {' '}
+                  <Translate content="emailSettings.general.emailUnverified" />
+                  {' | '}
+                  <a href="#" onClick={requestConfirmationEmail}>
+                    <Translate content="emailSettings.general.emailUnverifiedPrompt" />
+                  </a>
+                </div>
             }
           </div>
         </p>
