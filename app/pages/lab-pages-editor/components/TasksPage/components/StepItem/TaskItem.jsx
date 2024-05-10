ede1d82392f07f9d5c6a3d4f9964c69081839493
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-05-10 22:23:45 +0100

    TaskItem: add placeholders for DrawingTask

--- app/pages/lab-pages-editor/components/TasksPage/components/StepItem/TaskItem.jsx
+++ app/pages/lab-pages-editor/components/TasksPage/components/StepItem/TaskItem.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 
 // import strings from '../../../strings.json'; // TODO: move all text into strings
+import DrawingToolIcon from '../../../../icons/DrawingToolIcon.jsx';
 import TaskIcon from '../../../../icons/TaskIcon.jsx';
 import BranchingNextControls from './BranchingNextControls.jsx';
 
@@ -89,6 +90,27 @@ function PlaceholderAnswers({
       <div>
         <div className="fake-text-input">Participant text here</div>
       </div>
+    );
+  }
+
+  if (task.type === 'drawing') {
+    const tools = task.tools || [];
+
+    return (
+      <ul className="fake-drawing-tools">
+        {tools.map((tool, index) => (
+          <li
+            key={`placeholder-answer-${taskKey}-${index}`}
+            className="flex-row"
+          >
+            <span className="icon-wrapper">
+              <DrawingToolIcon type={tool?.type} color={tool?.color} />
+            </span>
+            <span className="flex-item">{tool?.label}</span>
+            <span>0 drawn</span>
+          </li>
+        ))}
+      </ul>
     );
   }
 
