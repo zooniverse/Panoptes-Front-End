9518fec88c22171515d9dd27d91bfe4845364544
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-05-31 14:43:43 +0100

    Allow for a local production test on local.zooniverse.org

--- static-server.js
+++ static-server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3735;
 
+const DEVELOPMENT_HOSTS = ['localhost', 'local.zooniverse.org'];
+
 // serve up compiled static assets if we're in production mode
 app.use(express.static(__dirname + '/dist'));
 
@@ -11,10 +13,13 @@ app.get('*', function response(req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(port, 'localhost', function onStart(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.info('==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
-  }
+
+DEVELOPMENT_HOSTS.forEach((host) => {
+  app.listen(port, host, function onStart(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.info('==> 🌎 Listening on port %s. Open up http://%s:%s/ in your browser.', port, host, port);
+    }
+  });
 });
