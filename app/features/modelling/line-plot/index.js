cdf064dd9274d41ccf771de3e9ed068d2ca7660b
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-06-06 12:36:28 +0100

    refactor(subject previews): allow for single series JSON data subjects
    Refactor previews for JSON data subjects to include subjects with a single `x,y` data series. `getDatasets(data)` reflects on the type of data, then calls an appropriate helper function to convert `data` into an array of `chart.js` datasets.

--- app/features/modelling/line-plot/index.js
+++ app/features/modelling/line-plot/index.js
@@ -1,5 +1,37 @@
 import { Chart } from 'chart.js/auto';
 
+function getXYData(data) {
+  return {
+    data: data.x.map((x, index) => ({ x, y: data.y[index] })),
+    pointStyle: 'circle',
+    backgroundColor: '#ffffff',
+    borderColor: '#000000'
+  }
+}
+
+function getSeriesData(series, index) {
+  const { seriesData, seriesOptions } = series;
+  return {
+    data: seriesData,
+    label: seriesOptions.label || `Series ${index + 1}`,
+    pointStyle: seriesOptions.glyph,
+    backgroundColor: seriesOptions.color,
+    borderColor: '#000000'
+  };
+}
+
+function getDatasets(data) {
+  if (data?.datasets) {
+    return data.datasets;
+  }
+  if (data?.map) {
+    return data.map(getSeriesData);
+  }
+  if (data.x && data.y) {
+    return [getXYData(data)];
+  }
+}
+
 // Help at http://www.chartjs.org/docs/latest
 class LinePlotModel {
   constructor(canvas, { frame, src, jsonData }, { onLoad, modelDidError }) {
@@ -24,65 +56,65 @@ class LinePlotModel {
   }
   
   processData(data, canvas, onLoad) {
+    if(data.x && data.y) {
+      data = {
+        data: {
+          x: data.x,
+          y: data.y
+        },
+        chartOptions: {
+          xAxisLabel: 'Days',
+          yAxisLabel: 'Brightness'
+        }
+      };
+    }
     let datasets;
     const { chartOptions, data: FEMdata, ...rest } = data;
     if (FEMdata) {
-      datasets = FEMdata.map( (data, index) => {
-        const { seriesData, seriesOptions } = data
-        return {
-          data: seriesData,
-          label: seriesOptions.label || `Series ${index + 1}`,
-          pointStyle: seriesOptions.glyph,
-          backgroundColor: seriesOptions.color,
-          borderColor: '#000000'
-        }
-      });
-    }
-    if (data.datasets) {
-      datasets = data.datasets
-    }
-    console.log({
-      type: 'scatter',
-      data: {
-        datasets,
-        ...rest
-      }
-    });
-    this.lineChart = new Chart(
-      this.ctx,
-      {
+      datasets = getDatasets(FEMdata);
+      console.log({
         type: 'scatter',
         data: {
           datasets,
           ...rest
-        },
-        options: {
-          plugins: {
-            legend: {
-              labels: {
-                usePointStyle: true
-              }
-            }
+        }
+      });
+      this.lineChart = new Chart(
+        this.ctx,
+        {
+          type: 'scatter',
+          data: {
+            datasets,
+            ...rest
           },
-          scales: {
-            x: {
-              reverse: chartOptions?.invertAxes?.x || false,
-              title: {
-                display: true,
-                text: chartOptions?.xAxisLabel
+          options: {
+            plugins: {
+              legend: {
+                labels: {
+                  usePointStyle: true
+                }
               }
             },
-            y: {
-              reverse: chartOptions?.invertAxes?.y || false,
-              title: {
-                display: true,
-                text: chartOptions?.yAxisLabel
+            scales: {
+              x: {
+                reverse: chartOptions?.invertAxes?.x || false,
+                title: {
+                  display: true,
+                  text: chartOptions?.xAxisLabel
+                }
+              },
+              y: {
+                reverse: chartOptions?.invertAxes?.y || false,
+                title: {
+                  display: true,
+                  text: chartOptions?.yAxisLabel
+                }
               }
             }
           }
         }
-      }
-    );
+      );
+    }
     const { height, width } = canvas.getBoundingClientRect()
     onLoad({ height, width })
   }
