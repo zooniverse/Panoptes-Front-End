5be4d25225dc3ea64492a8bd28403b1f04e1972e
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-06-08 19:27:32 +0100

    make MIN_SEARCH_LENGTH a configurable parameter

--- app/collections/searchCollections.js
+++ app/collections/searchCollections.js
@@ -1,5 +1,8 @@
 import apiClient from 'panoptes-client/lib/api-client';
 
+// Tune this value to determine when a search term is long enough to use Panoptes full-text search.
+const MIN_SEARCH_LENGTH = 3;
+
 function panoptesCollectionsSearch(text) {
   const query = {
     page_size: 100,
@@ -14,11 +17,11 @@ function panoptesCollectionsSearch(text) {
 
 export async function collectionsSearch(text = '') {
   const search = text.trim().toLowerCase();
-  // Search terms of more than 4 characters can use Panoptes full-text search.
-  if (search.length > 3) {
+  // Search terms of more than MIN_SEARCH_LENGTH characters can use Panoptes full-text search.
+  if (search.length > MIN_SEARCH_LENGTH) {
     return panoptesCollectionsSearch(search);
   }
-  // Otherwise, filter your collection names by the search term.
+  // Otherwise, filter all your collection names by the search term.
   const allCollections = await panoptesCollectionsSearch();
   return allCollections.filter(
     collection => collection.display_name.toLowerCase().includes(search)
