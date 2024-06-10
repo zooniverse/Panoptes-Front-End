5a3bedc2db7382019a2bf8444ea07f98085369c2
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-06-10 10:04:09 +0100

    Add some documentation

--- app/collections/searchCollections.js
+++ app/collections/searchCollections.js
@@ -3,6 +3,11 @@ import apiClient from 'panoptes-client/lib/api-client';
 // Tune this value to determine when a search term is long enough to use Panoptes full-text search.
 const MIN_SEARCH_LENGTH = 3;
 
+/**
+ * Query the Panoptes collections search API
+ * @param {string} text - The search term.
+ * @returns {Promise<Array>} - A promise that resolves to an array of collections.
+ */
 function panoptesCollectionsSearch(text) {
   const query = {
     page_size: 100,
@@ -15,6 +20,12 @@ function panoptesCollectionsSearch(text) {
   return apiClient.type('collections').get(query);
 }
 
+/**
+ * Search Panoptes collections by name. Use full-text search for long search terms.
+ * Otherwise, request all collections and filter by name.
+ * @param {string} text - The search term.
+ * @returns {Promise<Array>} - A promise that resolves to an array of collections.
+ */
 export async function collectionsSearch(text = '') {
   const search = text.trim().toLowerCase();
   // Search terms of more than MIN_SEARCH_LENGTH characters can use Panoptes full-text search.
