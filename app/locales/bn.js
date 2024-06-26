5150a5ea4ecaee19bcf74327321a3bd659d5d391
Cliff Johnson <cliff@zooniverse.org>
2024-06-26 09:41:22 -0500

    Add Bengali (bn) Locale (#7122)
    * add bn locale file
    
    * register bn locale
    
    * add bn (w/o dialect) option in longer language list
    
    * edit nav label for Talk

---
+++ app/locales/bn.js
@@ -0,0 +1,294 @@
+
+export default {
+  loading: 'লোডিং',
+  aboutPages: {
+    missingContent: {
+      education: 'এই প্রকল্পের এখনও কোন শিক্ষাগত সম্পদ নেই.',
+      faq: 'এই প্রকল্পে এখনও কোন সাধারণভাবে জিজ্ঞাসিত প্রশ্ন নেই।',
+      research: 'এই প্রকল্পের এখনও কোন বৈজ্ঞানিক উদাহরণ নেই',
+      results: 'এই প্রকল্পের এখনও রিপোর্ট করার জন্য কোন ফলাফল নেই',
+      team: 'এই প্রকল্পের কোন টিমের তথ্য নেই'
+    }
+  },
+  projectRoles: {
+    title: 'টীম',
+    owner: 'স্বত্বাধিকারী',
+    collaborator: 'সহযোগী',
+    translator: 'অনুবাদক',
+    scientist: 'গবেষক',
+    moderator: 'মডারেটর',
+    tester: 'টেস্টার',
+    expert: 'বিশেষজ্ঞ',
+    museum: 'মিউসিয়াম'
+  },
+  classifier: {
+    back: 'ফিরে যান',
+    backButtonWarning: 'ফিরে গেলে আপনার কাজের প্রগতি হারিয়ে যাবে',
+    close: 'বন্ধ করুন',
+    continue: 'চালিয়ে যান',
+    detailsSubTaskFormSubmitButton: 'ওকে',
+    done: 'সম্পন্ন',
+    doneAndTalk: 'সম্পন্ন এবং আলোচনা',
+    dontShowMinicourse: 'ভবিষ্যতে মিনি-কোর্স দেখাবেন না',
+    letsGo: 'শুরু করুন',
+    next: 'পরবর্তী',
+    optOut: 'অপ্ট আউট (করতে চাই না)',
+    taskTabs: {
+      taskTab: 'টাস্ক',
+      tutorialTab: 'টিউটোরিয়াল'
+    },
+    recents: 'আপনার সাম্প্রতিক শ্রেণীবিভাজন',
+    talk: 'আলোচনা',
+    taskHelpButton: 'আপনার কি এই কাজে সাহায্যের প্রয়োজন?',
+    miniCourseButton: 'প্রজেক্ট মিনি-কোর্স রিস্টার্ট করুন ',
+    workflowAssignmentDialog: {
+      promotionMessage: 'অভিনন্দন! আপনি পরবর্তী ওয়ার্কফ্লো আনলক করেছেন। আপনি চাইলে এই ওয়ার্কফ্লোতে থাকতে পারেন।',
+      acceptButton: 'পরবর্তী স্তরে যেতে চাই',
+      declineButton: 'না'
+    },
+    interventions: {
+      optOut: 'আর বার্তা দেখাবেন না।',
+      studyInfo: 'আমি এই আলোচনায় ভাগ নিতে চাই না [অধ্যয়ন](+tab+https://docs.google.com/document/d/1gLyN6Dgff8dOCOC88f47OD6TtFrfSJltsLgJMKkYMso/preview).'
+    }
+  },
+  projects: {
+    welcome: {
+      heading: 'স্বাগতম। আপনার সহযোগিতায় আমরা আনন্দিত',
+      thanks: 'প্রকৃত গবেষণায় সাহায্য করায় আপনার আগ্রহরের জন্য ধন্যবাদ। আমাদের কাছে কিছু প্রকল্প আছে যাতে আমাদের আপনার সাহায্য দরকার। আমাদের সমস্ত সক্রিয় প্রকল্পগুলি ব্রাউজ করতে নীচে স্ক্রোল করুন।',
+      talk: 'এছাড়াও আপনি অন্যান্য সমমনা স্বেচ্ছাসেবকদের সাথে আলোচনা করতে [টক্](/talk) ব্যবহার করতে পারেন',
+      scrollDown: 'আরো দেখতে নিচে স্ক্রল করুন'
+    }
+  },
+  project: {
+    language: 'ভাষা',
+    loading: 'লোডিং প্রজেক্ট',
+    disclaimer: 'এই প্রকল্পটি Zooniverse প্রজেক্ট বিল্ডার ব্যবহার করে তৈরি করা হয়েছে কিন্তু এটি এখনও একটি অফিসিয়াল Zooniverse প্রকল্প নয়। এই প্রজেক্ট সম্পর্কিত প্রশ্ন বা সমস্যার উত্তর দিতে Zooniverse এর টীম বাধিত নয়',
+    fieldGuide: 'ফিল্ড গাইড',
+    about: {
+      header: 'প্রজেক্ট এর বিষয়ে',
+      nav: {
+        research: 'গবেষণা',
+        results: 'রেজাল্টস',
+        faq: 'সাধারণভাবে জিজ্ঞাসিত প্রশ্ন',
+        education: 'শিক্ষা',
+        team: 'টীম'
+      }
+    },
+    nav: {
+      about: 'প্রজেক্ট এর বিষয়ে',
+      adminPage: 'অ্যাডমিন পেজ',
+      classify: 'শ্রেণীবিভাজন করুন',
+      collections: 'সংগ্রহ করুন',
+      exploreProject: 'প্রকল্প অন্বেষণ করুন',
+      lab: 'ল্যাব',
+      recents: 'সাম্প্রতিক',
+      talk: 'কথা বলুন',
+      underReview: 'পর্যালোচনার অধীনে',
+      zooniverseApproved: 'Zooniverse অনুমোদিত'
+    },
+    classifyPage: {
+      dark: 'ডার্ক (অন্ধকার)',
+      light: 'লাইট (উজ্জ্বল)',
+      title: 'শ্রেণীবিভাজন করুন',
+      themeToggle: '%(theme)s থিমে স্যুইচ করুন'
+    },
+    home: {
+      organization: 'সংগঠন',
+      researcher: 'গবেষকদের বক্তব্য',
+      about: '%(title)s এর বিষয়ে',
+      metadata: {
+        statistics: '%(title)s এর পরিসংখ্যান',
+        classifications: 'শ্রেণীবিভাগ',
+        volunteers: 'স্বেচ্ছাসেবক',
+        completedSubjects: 'সমাপ্ত বিষয়',
+        subjects: 'বিষয়'
+      },
+      talk: {
+        zero: 'এই মুহূর্তে কেউ **%(title)s** নিয়ে কথা বলছে না।',
+        one: '**1** ব্যক্তি এই মুহূর্তে **%(title)s** নিয়ে কথা বলছে।',
+        other: '**%(count)s** লোকে এখন **%(title)s** সম্পর্কে কথা বলছে৷'
+      },
+      joinIn: 'যোগদান করুন',
+      learnMore: 'আরও জানুন',
+      getStarted: 'শুরু করুন',
+      workflowAssignment: 'আপনি আনলক করেছেন %(workflowDisplayName)s',
+      visitLink: 'প্রকল্পটি দেখুন',
+      links: 'বহিরাগত প্রকল্প লিঙ্ক'
+    }
+  },
+  organization: {
+    error: 'সংস্থার তথ্য সংগ্রহ করতে সমস্যা দেখা দিচ্ছে',
+    home: {
+      about: '%(title)s এর বিষয়ে',
+      introduction: '%(title)s এর ভূমিকা',
+      learn: '%(title)s সম্পর্কে আরও জানুন',
+      links: '%(title)s এর সাথে সংযোগ করুন',
+      metadata: {
+        complete: 'শতাংশ সম্পূর্ণ',
+        heading: 'সংস্থার পরিসংখ্যান',
+        numbers: 'সংখ্যা দ্বারা',
+        projects: 'প্রকল্প',
+        subtitle: 'আপনি এবং আপনার সহকর্মী স্বেচ্ছাসেবকরা এই প্রকল্পে যে অগ্রগতি করেছেন তার উপর নজর রাখুন।',
+        text: 'প্রতিটি ক্লিক গুরুত্বপূর্ণ! এই প্রকল্পটি সম্পূর্ণ করতে %(title)s এর সম্প্রদায়ে যোগ দিন এবং গবেষকদের গুরুত্বপূর্ণ ফলাফল তৈরি করতে সহায়তা করুন৷'
+      },
+      projects: {
+        active: 'সক্রিয় প্রকল্প',
+        all: 'সমস্ত',
+        error: 'সংস্থার প্রকল্পগুলি লোড করার সময় একটি সমস্যা দেখা দিচ্ছে',
+        finished: 'সমাপ্ত প্রকল্প',
+        hideSection: 'লুকানো অংশ',
+        loading: 'সংস্থার প্রকল্পগুলি লোড হচ্ছে...',
+        none: 'এই সংস্থার সাথে যুক্ত কোনো %(state)s %(category)s প্রকল্প নেই।',
+        paused: 'বিরাম দেওয়া প্রকল্পগুলি৷',
+        projectCategory: 'প্রকল্প বিভাগ',
+        showSection: 'বিভাগ দেখান'
+      },
+      researcher: 'গবেষকের বার্তা',
+      viewToggle: 'স্বেচ্ছাসেবক হিসাবে দেখুন'
+    },
+    loading: 'সংস্থা লোড হচ্ছে',
+    notFound: 'সংস্থা খুঁজে পাওয়া যায়নি।',
+    notPermission: 'আপনি যদি নিশ্চিত হন যে URLটি সঠিক, তাহলে আপনার কাছে এই সংস্থাটি দেখার অনুমতি নাও থাকতে পারে৷',
+    pleaseWait: 'অনুগ্রহপূর্বক অপেক্ষা করুন...',
+    stats: {
+      adjustParameters: 'প্যারামিটার নিয়ন্ত্রন করুন',
+      byTheNumbers: 'সংখ্যা দ্বারা',
+      byTheNumbersContent: {
+        classifications: 'শ্রেণীবিভাজন',
+        firstProject: 'প্রথম প্রকল্প',
+        firstProjectLaunch: 'প্রথম প্রজেক্ট লঞ্চের তারিখ',
+        liveProjects: 'লাইভ প্রকল্প',
+        pausedProjects: 'বিরাম দেওয়া প্রকল্পগুলি৷',
+        retiredProjects: 'অবসরপ্রাপ্ত প্রকল্প',
+        retiredSubjects: 'সমাপ্ত বিষয়',
+        subjects: 'বিষয়'
+      },
+      classification: 'শ্রেণীবিভাজন',
+      comment: '“টক” মন্তব্য',
+      dateRange: 'তারিখের পরিসীমা',
+      expandWorkflowStats: 'ওয়ার্কফ্লো পরিসংখ্যান প্রসারিত করুন',
+      for: 'কারণ',
+      hidden: 'পরিসংখ্যান লুকানো',
+      hourly: 'প্রতি ঘণ্টার ডেটা শুধুমাত্র সাম্প্রতিক ২ সপ্তাহের জন্য উপলব্ধ।',
+      organizationStatistics: '%(title)s এর পরিসংখ্যান',
+      perclassification: 'প্রতি শ্রেণীবিভাজন',
+      percomment: 'প্রতি মন্তব্য',
+      projectStats: 'লাইভ প্রকল্প (%(count)s)',
+      reset: 'ভিউ রিসেট করুন'
+    }
+  },
+  tasks: {
+    hidePreviousMarks: '%(count)s টি পূর্ব চিহ্ন প্রচ্ছন্ন',
+    less: 'কম',
+    more: 'বেশী',
+    shortcut: {
+      noAnswer: 'কোনো উত্তর নয়'
+    },
+    survey: {
+      clear: 'ক্লিয়ার (খালি করতে চাই)',
+      clearFilters: 'ফিল্টার খালি করতে চাই',
+      makeSelection: 'একটি নির্বাচন করুন',
+      showing: '%(max)s এর %(count)s দেখানো হচ্ছে',
+      confused: 'প্রায়শই গুলিয়ে যায়',
+      dismiss: 'খারিজ',
+      itsThis: 'আমার ধারণায় এটি',
+      cancel: 'বাতিল করুন',
+      identify: 'শনাক্ত করুন',
+      surveyOf: '%(count)s এর  জরিপ',
+      identifications: {
+        zero: 'সনাক্ত করা যায়নি এমন',
+        one: 'একটি সনাক্তিকরণ',
+        other: '%(count)s টি সনাক্তিকরণ'
+      }
+    }
+  },
+  signIn: {
+    title: 'সাইন ইন/রেজিস্টার করুন',
+    withZooniverse: 'আপনার Zooniverse অ্যাকাউন্ট দিয়ে সাইন ইন করুন',
+    whyHaveAccount: 'সাইন-ইন করা স্বেচ্ছাসেবকরা তাদের কাজের ট্র্যাক রাখতে পারেন এবং এর দ্বারা স্বেচ্ছাসেবকদের তাদের অবদানের জন্য প্রকাশনায় সহজে ক্রেডিট দেয়া যাবে',
+    signIn: 'সাইন ইন',
+    register: 'রেজিস্টার করুন',
+    orThirdParty: 'অথবা অন্য পরিষেবা দিয়ে সাইন ইন করুন',
+    withFacebook: 'ফেসবুক দিয়ে সাইন ইন করুন',
+    withGoogle: 'গুগল দিয়ে সাইন ইন করুন'
+  },
+  notFoundPage: {
+    message: 'পাওয়া যায় নি'
+  },
+  resetPassword: {
+    heading: 'পাসওয়ার্ড রিসেট করুন',
+    newPasswordFormDialog: 'একই পাসওয়ার্ড দুবার লিখুন যাতে আপনি কিছু গবেষণা করতে ফিরে যেতে পারেন। পাসওয়ার্ড কমপক্ষে ৮ অক্ষর দীর্ঘ হতে হবে।',
+    newPasswordFormLabel: 'নতুন পাসওয়ার্ড',
+    newPasswordConfirmationLabel: 'নিশ্চিত করতে আপনার পাসওয়ার্ড পুনরাবৃত্তি করুন:',
+    enterEmailLabel: 'অনুগ্রহ করে এখানে আপনার ইমেল এড্রেসটি লিখুন এবং আমরা আপনাকে একটি লিঙ্ক পাঠাব যা দিয়ে আপনি এটি পুনরায় সেট করতে পারবেন',
+    emailSuccess: 'আপনার পাসওয়ার্ড রিসেট করার জন্য আমরা আপনাকে একটি লিঙ্ক সহ ইমেল পাঠিয়েছি।',
+    emailError: 'আপনার পাসওয়ার্ড রিসেট করার সময় একটি ত্রুটি হয়েছে৷',
+    passwordsDoNotMatch: 'পাসওয়ার্ডদুটি মিলছে না, আবার চেষ্টা করুন.',
+    loggedInDialog: 'আপনি বর্তমানে লগ ইন করেছেন৷ আপনি যদি আপনার পাসওয়ার্ড পুনরায় সেট করতে চান তাহলে অনুগ্রহ করে লগ আউট করুন৷',
+    missingEmailsSpamNote: 'আপনি যদি রিসেট ইমেল না পেয়ে থাকেন তাহলে অনুগ্রহ করে আপনার স্প্যাম ফোল্ডার চেক করুন।',
+    missingEmailsAlternateNote: 'আপনি যদি এখনও ইমেলটি না পেয়ে থাকেন, তাহলে অনুগ্রহ করে অন্য কোনো ইমেল (যা দিয়ে সাইন আপ করে থাকতে পারেন) দিয়ে চেষ্টা করুন।'
+  },
+  workflowToggle: {
+    label: 'সক্রিয়'
+  },
+  collections: {
+    createForm: {
+      private: 'ব্যক্তিগত',
+      submit: 'সংগ্রহে যোগ করুন'
+    }
+  },
+  about: {
+    index: {
+      header: 'বিষয়ে',
+      title: 'বিষয়ে',
+      nav: {
+        about: 'বিষয়ে',
+        publications: 'প্রকাশনা',
+        ourTeam: 'আমাদের টীম',
+        acknowledgements: 'স্বীকৃতি',
+        contact: 'যোগাযোগ করুন',
+        faq: 'সাধারণভাবে জিজ্ঞাসিত প্রশ্ন',
+        resources: 'সম্পদ',
+        highlights: 'হাইলাইটস বুক',
+        mobileApp: 'মোবাইল এপ',
+        donate: 'অনুদান'
+      }
+    }
+  },
+  userSettings: {
+    account: {
+      displayName: 'প্রদর্শিত নাম (প্রয়োজনীয়)',
+      displayNameHelp: 'কিভাবে আপনার নাম টক এবং আপনার প্রোফাইল পৃষ্ঠায় অন্যান্য ব্যবহারকারীদের কাছে প্রদর্শিত হবে',
+      realName: 'আসল নাম (ঐচ্ছিক)',
+      realNameHelp: 'পাবলিক; আমরা কাগজপত্রে, পোস্টারে, ইত্যাদিতে স্বীকৃতি দেওয়ার জন্য এটি ব্যবহার করব।',
+      interventions: 'প্রকল্প হস্তক্ষেপ বিজ্ঞপ্তি দেখান.',
+      interventionsHelp: 'শ্রেণীবদ্ধ করার সময় প্রকল্পগুলিকে বার্তা প্রদর্শনের অনুমতি দিন।',
+      interventionsPreferences: 'বিজ্ঞপ্তি পছন্দসমূহ',
+      changePassword: {
+        heading: 'আপনার পাসওয়ার্ড পরিবর্তন করুন',
+        currentPassword: 'বর্তমান পাসওয়ার্ড (প্রয়োজনীয়)',
+        newPassword: 'নতুন পাসওয়ার্ড (প্রয়োজনীয়)',
+        tooShort: 'এটা খুবই সংক্ষিপ্ত',
+        confirmNewPassword: 'নতুন পাসওয়ার্ড নিশ্চিত করুন (প্রয়োজনীয়)',
+        doesntMatch: 'এগুলি মিলছে না',
+        change: 'বদলে দিন'
+      }
+    },
+    profile: {
+      dropImage: 'এখানে ছবি ড্রপ করুন (বা নির্বাচন করতে ক্লিক করুন)।',
+      changeAvatar: 'অবতার পরিবর্তন করুন',
+      avatarImageHelp: 'এখানে ছবি ড্রপ করুন (স্কোয়ার আকৃতির, %(size)s KB-র চেয়ে ছোট)',
+      clearAvatar: 'অবতার সরিয়ে দিন',
+      changeProfileHeader: 'প্রোফাইল হেডার পরিবর্তন করুন',
+      profileHeaderImageHelp: 'এখানে ছবি ড্র্যাগ এবং ড্রপ করুন (যে কোনো আকৃতির, %(size)s KB-র চেয়ে ছোট)',
+      clearHeader: 'শিরোনাম পরিষ্কার করুন'
+    }
+  },
+  feedback: {
+    categories: {
+      correct: 'সফল সনাক্তিকরণ',
+      incorrect: 'অসফল সনাক্তিকরণ',
+      falsepos: 'ভুল সনাক্তিকরণ'
+    }
+  }
+};
