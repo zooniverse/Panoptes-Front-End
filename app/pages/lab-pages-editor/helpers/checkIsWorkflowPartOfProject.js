4bcf4e9fa5c8a1a8005c900d98b70838bcb029b3
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-08-21 17:39:55 +0100

    checkIsWorkflowPartOfProject: correct checking logic. We're looking at WF ID, not Subject Sets.

--- app/pages/lab-pages-editor/helpers/checkIsWorkflowPartOfProject.js
+++ app/pages/lab-pages-editor/helpers/checkIsWorkflowPartOfProject.js
@@ -4,5 +4,5 @@ Checks if a workflow belongs to a project.
 
 export default function checkIsWorkflowPartOfProject(workflow, project) {
   if (!workflow || !project) return false;
-  return !!workflow.links?.subject_sets?.some(sset => project.links?.subject_sets?.includes(sset));
+  return !!project.links?.workflows?.includes(workflow.id);
 }
