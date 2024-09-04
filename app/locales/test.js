6bc8904da976f6d8a31d3085b6919c917db42770
Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
2024-09-03 22:16:28 -0400

    A test connection from Lokalise

---
+++ app/locales/test.js
@@ -0,0 +1,167 @@
+var translations = {
+  "aboutPages": {
+    "missingContent": {
+      "education": "Translated",
+      "faq": "Translated",
+      "research": "Translated",
+      "results": "Translated",
+      "team": "Translated"
+    }
+  },
+  "classifier": {
+    "back": "Translated",
+    "backButtonWarning": "Translated",
+    "close": "Translated",
+    "continue": "Translated",
+    "detailsSubTaskFormSubmitButton": "Translated",
+    "done": "Translated",
+    "doneAndTalk": "Translated",
+    "dontShowMinicourse": "Translated",
+    "interventions": {
+      "optOut": "Translated",
+      "studyInfo": "Translated [study](+tab+https:\/\/docs.google.com\/document\/d\/1gLyN6Dgff8dOCOC88f47OD6TtFrfSJltsLgJMKkYMso\/preview)."
+    },
+    "letsGo": "Translated",
+    "miniCourseButton": "Translated",
+    "next": "Translated",
+    "optOut": "Translated",
+    "recents": "Translated",
+    "talk": "Translated",
+    "taskHelpButton": "Translated",
+    "taskTabs": {
+      "taskTab": "Translated",
+      "tutorialTab": "Translated"
+    },
+    "workflowAssignmentDialog": {
+      "acceptButton": "Translated",
+      "declineButton": "Translated",
+      "promotionMessage": "Translated"
+    }
+  },
+  "collections": {
+    "createForm": {
+      "private": "Translated",
+      "submit": "Translated"
+    }
+  },
+  "loading": "Translated",
+  "notFoundPage": {
+    "message": "Translated"
+  },
+  "project": {
+    "about": {
+      "header": "Translated",
+      "nav": {
+        "education": "Translated",
+        "faq": "Translated",
+        "research": "Translated",
+        "results": "Translated",
+        "team": "Translated"
+      }
+    },
+    "classifyPage": {
+      "dark": "Translated",
+      "light": "Translated",
+      "themeToggle": "Translated %(theme)s",
+      "title": "Translated"
+    },
+    "disclaimer": "Translated",
+    "fieldGuide": "Translated",
+    "home": {
+      "about": "Translated %(title)s",
+      "getStarted": "Translated",
+      "joinIn": "Translated",
+      "learnMore": "Translated",
+      "links": "Translated",
+      "metadata": {
+        "classifications": "Translated",
+        "completedSubjects": "Translated",
+        "statistics": "Translated %(title)s",
+        "subjects": "Translated",
+        "volunteers": "Translated"
+      },
+      "organization": "Translated",
+      "researcher": "Translated",
+      "talk": "{\"one\":\"Translated %(title)s\",\"other\":\"%(count)s Translated %(title)s\"}",
+      "visitLink": "Translated",
+      "workflowAssignment": "Translated %(workflowDisplayName)s"
+    },
+    "language": "Translated",
+    "loading": "Translated",
+    "nav": {
+      "about": "Translated",
+      "adminPage": "Translated",
+      "classify": "Translated",
+      "collections": "Translated",
+      "exploreProject": "Translated",
+      "lab": "Translated",
+      "recents": "Translated",
+      "talk": "Translated",
+      "underReview": "Translated",
+      "zooniverseApproved": "Translated"
+    }
+  },
+  "projectRoles": {
+    "collaborator": "Translated",
+    "expert": "Translated",
+    "moderator": "Translated",
+    "museum": "Translated",
+    "owner": "Translated",
+    "scientist": "Translated",
+    "tester": "Translated",
+    "title": "Translated",
+    "translator": "Translated"
+  },
+  "projects": {
+    "welcome": {
+      "heading": "Translated",
+      "scrollDown": "Translated",
+      "talk": "Translated",
+      "thanks": "Translated"
+    }
+  },
+  "resetPassword": {
+    "emailError": "Translated",
+    "emailSuccess": "Translated",
+    "enterEmailLabel": "Translated",
+    "heading": "Translated",
+    "loggedInDialog": "Translated",
+    "missingEmailsAlternateNote": "Translated",
+    "missingEmailsSpamNote": "Translated",
+    "newPasswordConfirmationLabel": "Translated",
+    "newPasswordFormDialog": "Translated",
+    "newPasswordFormLabel": "Translated",
+    "passwordsDoNotMatch": "Translated"
+  },
+  "signIn": {
+    "register": "Translated",
+    "signIn": "Translated",
+    "title": "Translated",
+    "whyHaveAccount": "Translated",
+    "withZooniverse": "Translated"
+  },
+  "tasks": {
+    "hidePreviousMarks": "Translated %(count)s",
+    "less": "Translated",
+    "more": "Translated",
+    "shortcut": {
+      "noAnswer": "Translated"
+    },
+    "survey": {
+      "cancel": "Translated",
+      "clear": "Translated",
+      "clearFilters": "Translated",
+      "confused": "Translated",
+      "dismiss": "Translated",
+      "identifications": "{\"one\":\"Translated\",\"other\":\"%(count)s Translated\"}",
+      "identify": "Translated",
+      "itsThis": "Translated",
+      "makeSelection": "Translated",
+      "showing": "Translated %(count)s %(max)s",
+      "surveyOf": "Translated %(count)s"
+    }
+  },
+  "workflowToggle": {
+    "label": "Translated"
+  }
+};
