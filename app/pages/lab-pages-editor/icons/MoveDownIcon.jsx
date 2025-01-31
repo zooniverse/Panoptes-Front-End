50154c296e5069083bbdc512c0ab0c9620bbb15b
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-01-31 21:53:42 +0000

    PagesEditor: refactor ..control-button styling. Update EditIcon.

--- app/pages/lab-pages-editor/icons/MoveDownIcon.jsx
+++ app/pages/lab-pages-editor/icons/MoveDownIcon.jsx
@@ -12,7 +12,7 @@ export default function MoveDownIcon({ alt, className = '', width = 16, height =
       <path
         d="M13.9194 3C14.7579 3 15.2241 3.96993 14.7002 4.6247L8.78087 12.0239C8.38054 12.5243 7.61946 12.5243 7.21913 12.0239L1.29976 4.6247C0.775946 3.96993 1.24212 3 2.08062 3H13.9194Z"
         stroke="currentColor"
-        stroke-width="1.5"
+        strokeWidth="1.5"
       />
     </svg>
   );
--- app/pages/lab-pages-editor/icons/MoveUpIcon.jsx
+++ app/pages/lab-pages-editor/icons/MoveUpIcon.jsx
@@ -9,11 +9,11 @@ export default function MoveUpIcon({ alt, className = '', width = 16, height = 1
       viewBox="0 0 16 16"
       fill="none"
     >
-      <g clip-path="url(#clip0_1767_3606)">
+      <g clipPath="url(#clip0_1767_3606)">
         <path
           d="M13.9194 13C14.7579 13 15.2241 12.0301 14.7003 11.3753L8.78088 3.97609C8.38056 3.47568 7.61947 3.47568 7.21915 3.97609L1.29977 11.3753C0.775961 12.0301 1.24213 13 2.08064 13H13.9194Z"
           stroke="currentColor"
-          stroke-width="1.5"
+          strokeWidth="1.5"
         />
       </g>
       <defs>
