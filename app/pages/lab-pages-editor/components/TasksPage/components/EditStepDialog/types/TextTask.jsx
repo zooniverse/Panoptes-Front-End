4985dfebb98094850969e2273d29895f2a7ebd3d
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-03-18 18:02:49 +0000

    TextTask, DrawingTask: implement TaskHelpField

--- app/pages/lab-pages-editor/components/TasksPage/components/EditStepDialog/types/TextTask.jsx
+++ app/pages/lab-pages-editor/components/TasksPage/components/EditStepDialog/types/TextTask.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 
-import CollapseIcon from '../../../../../icons/CollapseIcon.jsx';
-import DeleteIcon from '../../../../../icons/DeleteIcon.jsx';
-import ExpandIcon from '../../../../../icons/ExpandIcon.jsx';
+import DeleteIcon from '../../../../../icons/DeleteIcon.jsx'
+import TaskHelpField from '../TaskHelpField.jsx'
 
 const DEFAULT_HANDLER = () => {};
 
@@ -88,36 +87,14 @@ function TextTask({
           </label>
         </span>
       </div>
-      <div className="input-row">
-        <div className="flex-row spacing-bottom-S">
-          <label
-            className="medium"
-            htmlFor={`task-${taskKey}-help`}
-          >
-            Help Text
-          </label>
-          <button
-            aria-label={showHelpField ? 'Hide Help field' : 'Show Help field'}
-            aria-controls={`task-${taskKey}-help`}
-            aria-expanded={showHelpField ? 'true' : 'false'}
-            className="plain"
-            onClick={toggleShowHelpField}
-            type="button"
-          >
-            {showHelpField
-              ? <CollapseIcon />
-              : <ExpandIcon />
-            }
-          </button>
-        </div>
-        <textarea
-          id={`task-${taskKey}-help`}
-          hidden={!showHelpField}
-          value={help}
-          onBlur={update}
-          onChange={(e) => { setHelp(e?.target?.value) }}
-        />
-      </div>
+      <TaskHelpField
+        help={help}
+        setHelp={setHelp}
+        showHelpField={showHelpField}
+        taskKey={taskKey}
+        toggleShowHelpField={toggleShowHelpField}
+        update={update}
+      />
     </div>
   );
 }
