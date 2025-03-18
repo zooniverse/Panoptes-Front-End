cf6993e57613441a07818c71cee0e2949e73880f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-03-18 17:40:04 +0000

    Extract TaskHelpField subcomponent

---
+++ app/pages/lab-pages-editor/components/TasksPage/components/EditStepDialog/TaskHelpField.jsx
@@ -0,0 +1,46 @@
+import CollapseIcon from '../../../../icons/CollapseIcon.jsx';
+import ExpandIcon from '../../../../icons/ExpandIcon.jsx';
+
+const DEFAULT_HANDLER = () => {}
+
+export default function TaskHelpField ({
+  help = '',  // Help text written by user
+  setHelp = DEFAULT_HANDLER,
+  showHelpField = false,
+  taskKey = '',
+  toggleShowHelpField = DEFAULT_HANDLER,
+  update = DEFAULT_HANDLER,  // Update the workflow resource
+}) {
+  return (
+    <div className="input-row">
+      <div className="flex-row spacing-bottom-S">
+        <label
+          className="medium"
+          htmlFor={`task-${taskKey}-help`}
+        >
+          Help Text
+        </label>
+        <button
+          aria-label={showHelpField ? 'Hide Help field' : 'Show Help field'}
+          aria-controls={`task-${taskKey}-help`}
+          aria-expanded={showHelpField ? 'true' : 'false'}
+          className="plain"
+          onClick={toggleShowHelpField}
+          type="button"
+        >
+          {showHelpField
+            ? <CollapseIcon />
+            : <ExpandIcon />
+          }
+        </button>
+      </div>
+      <textarea
+        id={`task-${taskKey}-help`}
+        hidden={!showHelpField}
+        value={help}
+        onBlur={update}
+        onChange={(e) => { setHelp(e?.target?.value) }}
+      />
+    </div>
+  )
+}
