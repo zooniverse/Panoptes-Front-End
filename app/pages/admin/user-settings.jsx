11a36246acda8576bb3098ad2b1893d97d61ca83
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-04-17 19:17:48 +0100

    Admin page: add warnings about unreliability of Most Recent Classifications list

--- app/pages/admin/user-settings.jsx
+++ app/pages/admin/user-settings.jsx
@@ -112,7 +112,8 @@ class UserSettings extends Component {
           </ul>
         </details>
         <details>
-          <summary>Recent classifications {this.state.classifications.length}</summary>
+          <summary>Semi-recent classifications {this.state.classifications.length}</summary>
+          <p>⚠️ WARNING: classifications are <b>not correctly sorted</b> by most recently created. Order of classifications is semi-random.</p>
           <form onSubmit={this.updateSubjectID}>
             <label for="subjectId">Filter by subject ID: </label>
             <input id="subjectId" name="subjectID" type="text" defaultValue='' pattern='\d+' />
--- app/pages/admin/user-settings/stats.js
+++ app/pages/admin/user-settings/stats.js
@@ -84,7 +84,7 @@ export function getUserProjects(user, callback, _page = 1) {
 export function getUserClassifications(user, subject_id, _page = 1) {
   return user.get('classifications', {
     subject_id: subject_id ? subject_id : undefined,
-    sort: '-created_at',
+    // sort: '-created_at',  // WARNING: sorting doesn't work for Classifications
     page: _page
   })
 }
