e9b964ac2f63fe2431f467662b81ba12fae96b4e
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2025-06-05 22:26:34 +0100

    RubinPage: ensure Success Message only shows if there's a logged-in user
    Co-authored-by: Mark Bouslog <mark@zooniverse.org>

--- app/rubin-2025/rubin-page.jsx
+++ app/rubin-2025/rubin-page.jsx
@@ -43,7 +43,7 @@ function RubinPage ({
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut dapibus augue. Aliquam varius, lorem id tempor lacinia, nibh mauris ultrices odio, id aliquam purus metus et eros. Maecenas in felis nulla. Quisque nec urna orci. Integer odio lectus, vehicula ac faucibus eget, blandit eget libero. Morbi sed nunc interdum, aliquam orci sed, euismod orci. Vivamus in porttitor nunc. Proin vestibulum tempor aliquam. Nullam leo risus, posuere et venenatis sit amet, tincidunt pharetra neque. Duis aliquam mauris vel posuere fermentum. In nec euismod velit. Phasellus rutrum nulla at lorem facilisis blandit. Vestibulum eget massa leo. Fusce lobortis tortor id consequat aliquam. In eget turpis dignissim, fringilla ligula sed, porta magna.</p>
       <p>Quisque tempus, ante condimentum tempus auctor, massa erat convallis quam, sit amet bibendum dui quam id leo. Vivamus hendrerit nibh ipsum, ut dapibus ante dapibus ut. Sed non sapien sit amet sapien cursus varius. Vestibulum faucibus sed enim ac placerat. Ut nec ex ac tortor auctor lobortis. Etiam a pulvinar nisl, vel pharetra enim. Donec vitae neque in sapien faucibus porttitor. Aenean tincidunt tellus ut nisl tristique pulvinar. Suspendisse ultricies nisl a diam imperdiet, sit amet pellentesque purus imperdiet.</p>
 
-      {successMessage && (
+      {user && successMessage && (
         <div className="successMessage">
           <Translate content={`newAccountsPage.${successMessage}`} />
         </div>
