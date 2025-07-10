f043abf9ed9b107985549af53c1b25bec04ea2d8
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-07-10 23:40:49 +0100

    getAPIEnv: add docs. Change default env to staging, which makes a bit more sense.

--- app/pages/lab/data-exports-batch-aggregations/helpers/getAPIEnv.js
+++ app/pages/lab/data-exports-batch-aggregations/helpers/getAPIEnv.js
@@ -1,4 +1,16 @@
-const DEFAULT_ENV = 'development'
+/*
+Determines which environment (production or staging) should be used when
+contacting our Zooniverse API endpoints.
+
+- Returns either "staging" or "production"
+- Tries to (mostly) match PJC's "env"-deciding logic:
+  https://github.com/zooniverse/panoptes-javascript-client/blob/721b696/lib/config.js#L60-L69 
+- This means that getAPIEnv() will respect PJC overrides, i.e. adding
+  ?env=production to the URL will force both PJC and this function to use the
+  'production' environment.
+*/
+
+const DEFAULT_ENV = 'staging'
 
 export default function getAPIEnv() {
   const params = new URLSearchParams(window?.location?.search);
