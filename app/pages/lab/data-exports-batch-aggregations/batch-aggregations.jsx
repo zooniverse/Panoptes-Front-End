1a3e2e491d48c534d9f79dd105a75ade4725a611
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-07-11 23:56:27 +0100

    batch-aggregations-pt2: add stylesheet

--- app/pages/lab/data-exports-batch-aggregations/batch-aggregations.jsx
+++ app/pages/lab/data-exports-batch-aggregations/batch-aggregations.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import WorkflowsList from './components/workflows-list.jsx';
 import AggregationsChecker from './components/aggregations-checker.jsx';
 
+// TODO: find a better place to put shared items
+import CloseIcon from '../../lab-pages-editor/icons/CloseIcon.jsx';
+
 const DEFAULT_HANDLER = () => {};
 
 function BatchAggregations ({
@@ -15,7 +18,20 @@ function BatchAggregations ({
 
   return (
     <div className="batch-aggregations">
-      <h4>Batch Aggregations</h4>
+      <div className="batch-aggregations-header">
+        <h2>Batch Aggregations</h2>
+        <button
+          aria-label="Close dialog"
+          className="button close-button"
+          onClick={closeModal}
+        >
+          <CloseIcon />
+        </button>
+      </div>
+
+      <div className="batch-aggregations-info">
+        <p>(TODO: info)</p>
+      </div>
 
       <WorkflowsList
         project={project}
@@ -35,12 +51,6 @@ function BatchAggregations ({
         user={user}
         workflow={workflow}
       />
-
-      <hr/>
-
-      <div>
-        <button onClick={closeModal}>Close</button>
-      </div>
     </div>
   );
 }
