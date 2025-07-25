fb9639be7058195cfb3eefcf5a97c8ada07167b0
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-07-25 18:22:16 +0100

    AggregationsSummary: fully implement logic

--- app/pages/lab/data-exports-batch-aggregations/components/aggregation-summary.jsx
+++ app/pages/lab/data-exports-batch-aggregations/components/aggregation-summary.jsx
@@ -31,19 +31,150 @@ Possible states:
 Component Props:
 - workflow: the selected workflow. (Panoptes Workflow Resource)
 - workflowsExport: the Workflows Export data. (Panoptes Data Export Resource)
+- user: currently logged-in user. (Panoptes User Resource)
  */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import apiClient from 'panoptes-client/lib/api-client';
+import AggregationItem from './aggregation-item.jsx';
 
 function AggregationSummary ({
   workflow,
-  workflowsExport
+  workflowsExport,
+  user
 }) {
+  const [apiData, setApiData] = useState({
+    aggregation: null,
+    status: 'ready',
+    statusMessage: '',
+  });
+
+  function reset () {
+    setApiData({
+      aggregation: null,
+      status: 'ready',
+      statusMessage: ''
+    });
+  }
+
+  function onError (err) {
+    console.error('AggregationSummary', err);
+    setApiData({
+      aggregation: null,
+      status: 'error',
+      statusMessage: err
+    });
+  }
+
+  async function fetchAggregations () {
+    // Sanity check: if there's no workflow, reset everything and then do nothing.
+    if (!workflow) return reset();
+
+    try {
+      // Initialise fetching state, then fetch.
+      setApiData({
+        aggregation: null,
+        status: 'fetching'
+      });
+      const aggregationsResourcesArray = await apiClient.type('aggregations').get({ workflow_id: workflow.id  });
+      const aggregation = aggregationsResourcesArray?.[0];
+      
+      // On success, save the results.
+      setApiData({
+        aggregation: aggregation,
+        status: 'success'
+      });
+    
+    } catch (err) {
+      onError(err);
+    }
+  }
+
+  useEffect(fetchAggregations, [workflow]);
+
+  async function requestNewAggregation () {
+    // Sanity check: if there's no workflow (or user), reset everything and then do nothing.
+    if (!user || !workflow) return reset();
+
+    try {
+      // Initialise requesting state, then post.
+      setApiData({
+        aggregation: null,
+        status: 'requesting'
+      });
+      const url = '/aggregations';
+      const payload = {
+        'aggregations': {
+          'links': {
+            'user': `${user.id}`,
+            'workflow': `${workflow.id}`
+          }
+        }
+      };
+      const aggregationsResourcesArray = await apiClient.post(url, payload);
+      const aggregation = aggregationsResourcesArray?.[0];
+      
+      setApiData({
+        aggregation: aggregation,
+        status: 'success'
+      });
+    
+    } catch (err) {
+      onError(err);
+    }
+  }
+
+  const isWorkflowValid = true;
+
+  const showExistingAggregation = apiData.status === 'success' && apiData.aggregation;
+
+  const requestEnabled = (  // For the "request new batch aggregation data export" button to be enabled...
+    workflow && isWorkflowValid  // ...a valid workflow must be selected.
+    && workflowsExport  // ...the project must have a workflows export.
+    && (apiData.status === 'success' && !apiData.aggregation)  // ...the selected workflow must have no existing aggregation.
+  );
+
+  const workflowsExportUpdatedAt = workflowsExport ? new Date(workflowsExport.updated_at) : null;
 
   return (
     <div className="aggregation-summary">
       <p>Aggregation Summary</p>
-      <button>Generate</button>
+      {!workflow && (
+        <p>❌ No workflow selected</p>
+      )}
+      {workflow && !isWorkflowValid && (
+        <p>❌ Workflow {workflow.id} is invalid</p>
+      )}
+      {workflow && isWorkflowValid && (
+        <p>✅ Workflow {workflow.id} is valid</p>
+      )}
+
+      {workflowsExport && (
+        <p>✅ Workflow export date: {workflowsExportUpdatedAt?.toLocaleString()}</p>
+      )}
+      {!workflowsExport && (
+        <p>❌ No workflow export has been generated.</p>
+      )}
+
+      {!showExistingAggregation && (
+        <button
+          disabled={!requestEnabled}
+          onClick={requestNewAggregation}
+        >
+          Generate
+        </button>
+      )}
+
+      {showExistingAggregation && (
+        <>
+          <p>Aggregation already exists</p>
+          <ul className="single-aggregation">
+            <AggregationItem
+              aggregation={apiData.aggregation}
+            />
+          </ul>
+        </>
+      )}
     </div>
   );
 }
