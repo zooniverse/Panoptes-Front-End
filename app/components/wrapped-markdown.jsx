49d9207990f111ea51aadd961230fb8dfc146443
Jim O'Donnell <jim@eatyourgreens.org.uk>
2026-07-16 08:04:17 +0100

    fix: typo in relNofollow attribute
    - Closes #7500.
    
    Search engines should now ignore spam links posted to Talk.

--- app/components/wrapped-markdown.jsx
+++ app/components/wrapped-markdown.jsx
@@ -29,7 +29,7 @@ class WrappedMarkdown extends React.Component {
     return (
       <div onClick={this.onClick}>
         <Markdown
-          relNofollow
+          relNoFollow
           content={this.props.content}
           project={this.props.project}
           header={this.props.header}
